23c245ec33459f37b8b27eb3e41a75ef8b3176f9
Cayle Sharrock <CjS77@users.noreply.github.com>
2019-01-18 12:16:05 +0200

    DRY up SecretKey and PublicKey (#69)
    * Refactor key traits to DRY
    
    * Re-add deleted trait documentation

--- infrastructure/crypto/src/keys.rs
+++ infrastructure/crypto/src/keys.rs
@@ -3,22 +3,7 @@
 //! implementation of ECC curve). The idea being that we can swap out the underlying
 //! implementation without worrying too much about the impact on upstream code.
 
-use crate::hex::HexError;
-use derive_error::Error;
 use rand::{CryptoRng, Rng};
-use std::marker::Sized;
-
-//----------------------------------------   Key Errors   ----------------------------------------//
-#[derive(Debug, Error)]
-pub enum KeyError {
-    // Could not create a valid key when converting from a different format
-    #[error(msg_embedded, non_std, no_from)]
-    ConversionError(String),
-    // Invalid hex representation for key
-    HexConversionError(HexError),
-}
-
-//----------------------------------------   Secret Keys  ----------------------------------------//
 
 /// A secret key factory trait. The `random` function is pulled out into a separate Trait because
 /// we can't know _a priori_ whether the default implementation (uniform random characters over the
@@ -33,7 +18,7 @@ pub trait SecretKeyFactory {
 ///
 /// ## Example
 ///
-/// Assuming there is a Curve25519EdDSA implementation,
+/// Assuming there is a Curve25519 implementation,
 /// ```edition2018
 /// # use crypto::curve25519::{ Curve25519SecretKey, Curve25519PublicKey };
 /// # use crypto::keys::{ SecretKeyFactory, SecretKey, PublicKey };
@@ -42,33 +27,7 @@ pub trait SecretKeyFactory {
 /// let k = Curve25519SecretKey::random(&mut rng);
 /// let p = Curve25519PublicKey::from_secret_key(&k);
 /// ```
-pub trait SecretKey {
-    /// Return the hexadecimal string representation of the secret key
-    fn to_hex(&self) -> String;
-
-    /// Try and convert the given hexadecimal string to a secret key. Any failures (incorrect
-    /// string length, non hex characters, etc) return a
-    /// [KeyError](enum.KeyError.html) with an explanatory note.
-    fn from_hex(hex: &str) -> Result<Self, KeyError>
-    where Self: Sized;
-
-    /// Try and convert the given byte array to a secret key. Any failures (incorrect
-    /// array length, implementation-specific checks, etc) return a
-    /// [KeyError](enum.KeyError.html) with an explanatory note.
-    fn from_bytes(bytes: &[u8]) -> Result<Self, KeyError>
-    where Self: Sized;
-
-    /// Return the secret key as a byte array
-    fn to_bytes(&self) -> &[u8];
-
-    /// Return the secret key as a byte vector
-    fn to_vec(&self) -> Vec<u8>;
-
-    /// Try and convert the given byte vector to a secret key. Any failures (incorrect
-    /// string length etc) return a [KeyError](enum.KeyError.html) with an explanatory note.
-    fn from_vec(v: &[u8]) -> Result<Self, KeyError>
-    where Self: Sized;
-}
+pub trait SecretKey {}
 
 //----------------------------------------   Public Keys  ----------------------------------------//
 
@@ -78,31 +37,7 @@ pub trait SecretKey {
 /// See [SecretKey](trait.SecretKey.html) for an example.
 pub trait PublicKey {
     type K: SecretKey;
-
     /// Calculate the public key associated with the given secret key. This should not fail; if a
-    /// failure does occur (implementatio error?), the function will panic.
+    /// failure does occur (implementation error?), the function will panic.
     fn from_secret_key(k: &Self::K) -> Self;
-
-    /// Return the hexadecimal string representation of the public key
-    fn to_hex(&self) -> String;
-
-    /// Try and convert the given byte array to a public key. Any failures (incorrect
-    /// array length, implementation-specific checks, etc) return a
-    /// [KeyError](enum.KeyError.html) with an explanatory note.
-    fn from_hex(hex: &str) -> Result<Self, KeyError>
-    where Self: Sized;
-
-    /// Try and convert the given byte array to a public key. Any failures (incorrect
-    /// array length, implementation-specific checks, etc) return a
-    /// [KeyError](enum.KeyError.html) with an explanatory note.
-    fn from_bytes(bytes: &[u8]) -> Result<Self, KeyError>
-    where Self: Sized;
-
-    /// Return the public key as a byte array
-    fn to_bytes(&self) -> &[u8];
-
-    fn to_vec(&self) -> Vec<u8>;
-
-    fn from_vec(v: &[u8]) -> Result<Self, KeyError>
-    where Self: Sized;
 }
