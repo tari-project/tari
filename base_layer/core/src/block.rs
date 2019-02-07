c4a9d0f6d4b205910fd94a291dd2b91c52cbcd26
SW van Heerden <swvheerden@gmail.com>
2019-02-07 14:56:33 +0200

    Sw move block header (#114)
    * move blockheader to its own file
    
    * cargo fmt

--- base_layer/core/src/block.rs
+++ base_layer/core/src/block.rs
@@ -24,9 +24,11 @@
 // Version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0.
 
 use crate::{
+    blockheader::BlockHeader,
     pow::ProofOfWork,
     transaction::{BlindingFactor, TransactionInput, TransactionKernel, TransactionOutput},
 };
+
 use chrono::{DateTime, Utc};
 
 type BlockHash = [u8; 32];
@@ -35,25 +37,6 @@ type BlockHash = [u8; 32];
 pub struct Block {
     pub header: BlockHeader,
     pub body: AggregateBody,
-}
-
-/// The BlockHeader contains all the metadata for the block, including proof of work, a link to the previous block
-/// and the transaction kernels.
-#[derive(Clone, Debug, PartialEq)]
-pub struct BlockHeader {
-    /// Version of the block
-    pub version: u16,
-    /// Height of this block since the genesis block (height 0)
-    pub height: u64,
-    /// Hash of the block previous to this in the chain.
-    pub prev_hash: BlockHash,
-    /// Timestamp at which the block was built.
-    pub timestamp: DateTime<Utc>,
-    /// Total accumulated sum of kernel offsets since genesis block. We can derive the kernel offset sum for *this*
-    /// block from the total kernel offset of the previous block header.
-    pub total_kernel_offset: BlindingFactor,
-    /// Proof of work summary
-    pub pow: ProofOfWork,
 }
 
 /// The components of the block or transaction. The same struct can be used for either, since in Mimblewimble,
--- base_layer/core/src/blockheader.rs
+++ base_layer/core/src/blockheader.rs
@@ -19,13 +19,45 @@
 // SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Portions of this file were originally copyrighted (c) 2018 The Grin Developers, issued under the Apache License,
+// Version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0.
 
-#[macro_use]
-extern crate bitflags;
+use crate::{
+    pow::ProofOfWork,
+    transaction::{BlindingFactor, TransactionInput, TransactionKernel, TransactionOutput},
+};
+use chrono::{DateTime, Utc};
 
-pub mod block;
-pub mod blockheader;
-pub mod pow;
-pub mod range_proof;
-pub mod transaction;
-pub mod types;
+type BlockHash = [u8; 32];
+
+/// The BlockHeader contains all the metadata for the block, including proof of work, a link to the previous block
+/// and the transaction kernels.
+#[derive(Clone, Debug, PartialEq)]
+pub struct BlockHeader {
+    /// Version of the block
+    pub version: u16,
+    /// Height of this block since the genesis block (height 0)
+    pub height: u64,
+    /// Hash of the block previous to this in the chain.
+    pub prev_hash: BlockHash,
+    /// Timestamp at which the block was built.
+    pub timestamp: DateTime<Utc>,
+    /// This is the MMR root of the outputs
+    pub output_mmr: BlockHash,
+    /// This is the MMR root of the kernels
+    pub kernel_mmr: BlockHash,
+    /// Total accumulated sum of kernel offsets since genesis block. We can derive the kernel offset sum for *this*
+    /// block from the total kernel offset of the previous block header.
+    pub total_kernel_offset: BlindingFactor,
+    /// Nonce used
+    /// Proof of work summary
+    pub pow: ProofOfWork,
+}
+
+impl BlockHeader {
+    /// This function will validate the proof of work in the header
+    pub fn validate_pow(&self) -> bool {
+        unimplemented!();
+    }
+}
