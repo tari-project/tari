1149b7ac5244fc02fd07fe13a920713e7e83e574
Stan Bondi <sdbondi@users.noreply.github.com>
2019-03-08 10:08:55 +0200

    Added comms and peer manager scaffolding (#142)
    * Added initial comms layout
    
    * NodeAddress -> NetAddress (as enum), using derive_error for errors
    
    * Using most recent rust nightly 2019-03-08 for circle ci
    
    * Use tari rust builder image for circleci
    
    * Fixed mdbook already installed error
    
    * Missing toolchain in rustup command

--- infrastructure/comms/src/lib.rs
+++ infrastructure/comms/src/lib.rs
@@ -1,1 +1,10 @@
+#![feature(custom_attribute)]
+extern crate crypto;
+extern crate derive_error;
+extern crate zmq;
 
+pub mod connection;
+pub mod peer_manager;
+pub mod pubsub;
+pub mod router;
+pub mod rpc;
