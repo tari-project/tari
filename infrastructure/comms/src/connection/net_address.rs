9f855b557ab4395c8e81d0b2c8e4f2e0e662a182
Stan Bondi <sdbondi@users.noreply.github.com>
2019-03-11 06:53:47 +0200

    2019 Copyright fixes, added edition 2018, build badge in README (#148)
    * copyright 2018->2019
    
    * Added build badge to readme

--- infrastructure/comms/src/connection/net_address.rs
+++ infrastructure/comms/src/connection/net_address.rs
@@ -1,4 +1,4 @@
-//  Copyright 2018 The Tari Project
+//  Copyright 2019 The Tari Project
 //
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 //  following conditions are met:
@@ -23,7 +23,7 @@
 use derive_error::Error;
 use std::str::FromStr;
 
-use connection::{i2p::I2PAddress, onion::OnionAddress, p2p::SocketAddress};
+use crate::connection::{i2p::I2PAddress, onion::OnionAddress, p2p::SocketAddress};
 
 #[derive(Debug, Error)]
 pub enum NetAddressError {
