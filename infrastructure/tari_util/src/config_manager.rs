acba98f9cd8598046b5dc9d630896cd984d2ad97
SW van Heerden <swvheerden@gmail.com>
2019-05-07 11:32:58 +0200

    Added a config manager

--- infrastructure/tari_util/src/config_manager.rs
+++ infrastructure/tari_util/src/config_manager.rs
@@ -19,20 +19,48 @@
 // SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-pub mod bit;
-pub mod byte_array;
-pub mod chacha20;
-pub mod cipher;
 
-#[cfg(feature = "confg")]
-pub mod config_manager;
-pub mod extend_bytes;
-pub mod hash;
-pub mod hex;
+use config::*;
+use derive_error::Error;
+use serde::de::Deserialize;
+use std::fs;
 
-pub use self::extend_bytes::ExtendBytes;
+// TODO add clap
 
-pub use self::{
-    byte_array::{ByteArray, ByteArrayError},
-    hash::Hashable,
-};
+struct ConfigManager {
+    fileconfig: Config,
+}
+
+#[derive(Debug, Error)]
+pub enum ConfigManagerError {
+    // Could not load file
+    FileLoadError(ConfigError),
+    // Could not find the file
+    FileNotFound,
+    // Could not be translated into object
+    FileDeseriliseError,
+}
+
+impl ConfigManager {
+    /// This creates a new ConfigManager
+    pub fn new() -> ConfigManager {
+        let c = Config::default();
+        ConfigManager { fileconfig: c }
+    }
+
+    /// This function adds a configuration file be loaded and searched for settings
+    pub fn add_file(&mut self, filename: String) -> Result<(), ConfigManagerError> {
+        if fs::metadata(&filename).is_err() {
+            return Err(ConfigManagerError::FileNotFound);
+        }
+        self.fileconfig
+            .merge(File::new(&filename, FileFormat::Toml))
+            .map_err(|e| ConfigManagerError::FileLoadError(e))?;
+        Ok(())
+    }
+
+    /// Attempt to deserialize the entire configuration into the requested type.
+    pub fn try_into<'de, T: Deserialize<'de>>(self) -> Result<T, ConfigManagerError> {
+        T::deserialize(self.fileconfig).map_err(|_| ConfigManagerError::FileDeseriliseError)
+    }
+}
