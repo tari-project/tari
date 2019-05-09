f2a086268e48fe7c5d8a5dd710c32ae156886be3
SW van Heerden <swvheerden@gmail.com>
2019-05-09 11:49:00 +0200

    Add serialize header struct to message
    remove unused file
    
    cleaned up and removed some files
    Added unit test
    
    Updated tests
    
    fixed spelling mistakes
    
    adjusted spacing in ascii graph
    
    Update RFC/src/RFC-0172_PeerToPeerMessagingProtocol.md
    
    Co-Authored-By: Stan Bondi <sdbondi@users.noreply.github.com>
    Update RFC/src/RFC-0172_PeerToPeerMessagingProtocol.md
    
    Co-Authored-By: Stan Bondi <sdbondi@users.noreply.github.com>
    addressed review comments

--- infrastructure/comms/src/connection/message.rs
+++ infrastructure/comms/src/connection/message.rs
@@ -20,9 +20,12 @@
 //  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 //  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-use std::convert::TryFrom;
-
+use crate::peer_manager::node_id::*;
+use bitflags::*;
 use derive_error::Error;
+use serde_derive::{Deserialize, Serialize};
+use std::convert::TryFrom;
+use tari_crypto::keys::PublicKey;
 
 #[derive(Error, Debug)]
 pub enum MessageError {
@@ -30,6 +33,37 @@ pub enum MessageError {
     MalformedMultipart,
     /// Failed to deserialize message
     DeserializeFailed,
+    // An error occurred serialising an object into binary
+    BinarySerializeError,
+    // An error occurred deserialising binary data into an object
+    BinaryDeserializeError,
+}
+
+bitflags! {
+    #[derive(Deserialize, Serialize)]
+    struct IdentityFlags: u8 {
+        const ENCRYPTED = 0b00000001;
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub enum NodeDestination<PubKey>
+where PubKey: PublicKey
+{
+    Unknown,
+    PublicKey(PubKey),
+    NodeId(NodeId),
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub struct MessageEnvelopeHeader<PubKey>
+where PubKey: PublicKey
+{
+    version: u8,
+    source: PubKey,
+    dest: NodeDestination<PubKey>,
+    signature: Vec<u8>,
+    flags: IdentityFlags,
 }
 
 const FRAMES_PER_MESSAGE: usize = 4;
@@ -40,14 +74,14 @@ pub type Frame = Vec<u8>;
 pub type FrameSet = Vec<Frame>;
 
 /// Represents a message which is about to go on or has just come off the wire.
-pub struct RawDataMessage {
+pub struct MessageEnvelope {
     frames: FrameSet,
 }
 
-impl RawDataMessage {
-    /// Create a new RawDataMessage from four frames
+impl MessageEnvelope {
+    /// Create a new MessageEnvelope from four frames
     pub fn new(identity: Frame, version: Frame, header: Frame, body: Frame) -> Self {
-        RawDataMessage {
+        MessageEnvelope {
             frames: vec![identity, version, header, body],
         }
     }
@@ -73,29 +107,36 @@ impl RawDataMessage {
     }
 }
 
-impl TryFrom<FrameSet> for RawDataMessage {
+impl TryFrom<FrameSet> for MessageEnvelope {
     type Error = MessageError;
 
-    /// Returns a RawDataMessage from a FrameSet
+    /// Returns a MessageEnvelope from a FrameSet
     fn try_from(frames: FrameSet) -> Result<Self, Self::Error> {
         if frames.len() != FRAMES_PER_MESSAGE {
             return Err(MessageError::MalformedMultipart);
         }
 
-        Ok(RawDataMessage { frames })
+        Ok(MessageEnvelope { frames })
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use std::convert::TryInto;
+    use rand;
+    use rmp_serde;
+    use serde::{Deserialize, Serialize};
+    use tari_crypto::{
+        keys::SecretKey,
+        ristretto::{RistrettoPublicKey, RistrettoSecretKey},
+    };
 
+    use std::convert::TryInto;
     #[test]
     fn try_from_valid() {
         let example = vec![vec![0u8], vec![1u8], vec![2u8], vec![3u8]];
 
-        let raw_message: Result<RawDataMessage, MessageError> = example.try_into();
+        let raw_message: Result<MessageEnvelope, MessageError> = example.try_into();
 
         assert!(raw_message.is_ok());
         let raw_message = raw_message.unwrap();
@@ -109,7 +150,7 @@ mod test {
     fn try_from_invalid() {
         let example = vec![vec![0u8], vec![1u8], vec![2u8]];
 
-        let raw_message: Result<RawDataMessage, MessageError> = example.try_into();
+        let raw_message: Result<MessageEnvelope, MessageError> = example.try_into();
 
         assert!(raw_message.is_err());
         let error = raw_message.err().unwrap();
@@ -117,5 +158,31 @@ mod test {
             MessageError::MalformedMultipart => {},
             _ => panic!("Unexpected MessageError {:?}", error),
         }
+    }
+
+    #[test]
+    fn test_ser_des() {
+        let version = 0;
+        let mut rng = rand::OsRng::new().unwrap();
+        let k = RistrettoSecretKey::random(&mut rng);
+        let p = RistrettoPublicKey::from_secret_key(&k);
+        let source = p;
+        let dest: NodeDestination<RistrettoPublicKey> = NodeDestination::Unknown;
+        let signature = vec![0];
+        let flags = IdentityFlags::ENCRYPTED;
+        let header: MessageEnvelopeHeader<RistrettoPublicKey> = MessageEnvelopeHeader {
+            version,
+            source,
+            dest,
+            signature,
+            flags,
+        };
+
+        let mut buf = Vec::new();
+        header.serialize(&mut rmp_serde::Serializer::new(&mut buf)).unwrap();
+        let serialized = buf.to_vec();
+        let mut de = rmp_serde::Deserializer::new(serialized.as_slice());
+        let deserialized: MessageEnvelopeHeader<RistrettoPublicKey> = Deserialize::deserialize(&mut de).unwrap();
+        assert_eq!(deserialized, header);
     }
 }
