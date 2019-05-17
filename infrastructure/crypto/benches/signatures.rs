9d707fea15de91589f55fadb70933b920387293f
CjS77 <CjS77@users.noreply.github.com>
2019-05-17 08:32:40 +0200

    Split Schnorr signature sign and verify benchmarks

--- infrastructure/crypto/benches/signatures.rs
+++ infrastructure/crypto/benches/signatures.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::Criterion;
+use criterion::{BatchSize, Criterion};
 use rand::{OsRng, RngCore};
 use std::time::Duration;
 use tari_crypto::{
@@ -26,24 +26,54 @@ fn native_keypair(c: &mut Criterion) {
     });
 }
 
-fn sign_and_verify_message(c: &mut Criterion) {
-    c.bench_function("Sign and verify", |b| {
-        let mut rng = OsRng::new().unwrap();
-        b.iter(|| {
-            let mut msg = [0u8; 32];
-            rng.fill_bytes(&mut msg);
-            let (k, p) = RistrettoPublicKey::random_keypair(&mut rng);
-            let r = RistrettoSecretKey::random(&mut rng);
-            let msg_key = RistrettoSecretKey::from_bytes(&msg).unwrap();
-            let sig = RistrettoSchnorr::sign(k, r, &msg).unwrap();
-            assert!(sig.verify(&p, &msg_key));
-        });
+struct SigningData {
+    k: RistrettoSecretKey,
+    p: RistrettoPublicKey,
+    r: RistrettoSecretKey,
+    m: RistrettoSecretKey,
+}
+
+fn gen_keypair() -> SigningData {
+    let mut rng = OsRng::new().unwrap();
+    let mut msg = [0u8; 32];
+    rng.fill_bytes(&mut msg);
+    let (k, p) = RistrettoPublicKey::random_keypair(&mut rng);
+    let r = RistrettoSecretKey::random(&mut rng);
+    let m = RistrettoSecretKey::from_bytes(&msg).unwrap();
+    SigningData { k, p, r, m }
+}
+
+fn sign_message(c: &mut Criterion) {
+    c.bench_function("Create RistrettoSchnorr", move |b| {
+        b.iter_batched(
+            || gen_keypair(),
+            |d| {
+                let _ = RistrettoSchnorr::sign(d.k, d.r, &d.m.to_vec()).unwrap();
+            },
+            BatchSize::SmallInput,
+        );
+    });
+
+    //    assert!(sig.verify(&p, &msg_key));
+}
+
+fn verify_message(c: &mut Criterion) {
+    c.bench_function("Verify RistrettoSchnorr", move |b| {
+        b.iter_batched(
+            || {
+                let d = gen_keypair();
+                let s = RistrettoSchnorr::sign(d.k.clone(), d.r.clone(), &d.m.to_vec()).unwrap();
+                (d, s)
+            },
+            |(d, s)| assert!(s.verify(&d.p, &d.m)),
+            BatchSize::SmallInput,
+        );
     });
 }
 
 criterion_group!(
-    name = signatures;
-    config = Criterion::default().warm_up_time(Duration::from_millis(500));
-    targets = generate_secret_key, native_keypair, sign_and_verify_message
-    );
+name = signatures;
+config = Criterion::default().warm_up_time(Duration::from_millis(500));
+targets = generate_secret_key, native_keypair, sign_message, verify_message
+);
 criterion_main!(signatures);
