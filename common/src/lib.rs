d0e3c9e631f3d7731b3acc31c74bfef8fc972b0a
Cayle Sharrock <CjS77@users.noreply.github.com>
2019-07-01 07:39:17 +0200

    Remove redundant logging module and provide logging convention guidance (#434)
    The logging module doesn't add much value and can be confusing.
    This PR replaces it with some well-documented sample configuration
    files, guidance on how to set up logging for Tari applications
    and a convenience function for deriving a logfile configuration path.

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -20,4 +20,48 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-pub mod logging;
+use dirs;
+use std::{env, path::PathBuf};
+
+/// Determine the path to a log configuration file using the following precedence rules:
+/// 1. Use the provided path (usually pulled from a CLI argument)
+/// 2. Use the value in the `TARI_LOG_CONFIGURATION` envar
+/// 3. The default path (OS-dependent), "~/.tari/log4rs.toml`
+/// 4. The current directory
+pub fn get_log_configuration_path(cli_path: Option<PathBuf>) -> PathBuf {
+    cli_path
+        .or_else(|| {
+            env::var_os("TARI_LOG_CONFIGURATION")
+                .filter(|s| !s.is_empty())
+                .map(|s| PathBuf::from(s))
+        })
+        .or_else(|| dirs::home_dir().map(|path| path.join(".tari/log4rs.toml")))
+        .or_else(|| {
+            Some(env::current_dir().expect(
+                "Could find a suitable path to the log configuration file. Consider setting the \
+                 TARI_LOG_CONFIGURATION envar, or check that the current directory exists and that you have \
+                 permission to read it",
+            ))
+        })
+        .unwrap()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::env;
+
+    #[test]
+    fn get_log_configuration_path_cli() {
+        let path = get_log_configuration_path(Some(PathBuf::from("~/my-tari")));
+        assert_eq!(path.to_str().unwrap(), "~/my-tari");
+    }
+
+    #[test]
+    fn get_log_configuration_path_by_env_var() {
+        env::set_var("TARI_LOG_CONFIGURATION", "~/fake-example");
+        let path = get_log_configuration_path(None);
+        assert_eq!(path.to_str().unwrap(), "~/fake-example");
+        env::set_var("TARI_LOG_CONFIGURATION", "");
+    }
+}
