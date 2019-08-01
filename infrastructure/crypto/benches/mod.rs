7a082353de82bfcf5a4ced3bfd1dac77d19810ce
Cayle Sharrock <CjS77@users.noreply.github.com>
2019-08-01 09:45:03 +0200

    Update infrastructure/crypto/benches/mod.rs
    Co-Authored-By: SW van Heerden <swvheerden@gmail.com>

--- infrastructure/crypto/benches/mod.rs
+++ infrastructure/crypto/benches/mod.rs
@@ -23,7 +23,7 @@
 // Portions of this file were originally copyrighted (c) 2018 The Grin Developers, issued under the Apache License,
 // Version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0.
 
-extern crate criterion;
+use criterion::criterion_main;
 
 pub mod range_proof;
 pub mod signatures;
