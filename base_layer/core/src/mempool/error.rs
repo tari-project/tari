283976f17b551ced10e993ba48731a75f70f092f
Yuko Roodt <neonknight@tari.com>
2019-09-03 10:39:55 +0200

    - Added implementation of Mempool with tests

--- base_layer/core/src/mempool/error.rs
+++ base_layer/core/src/mempool/error.rs
@@ -20,11 +20,15 @@
 //  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 //  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-use crate::mempool::{
-    orphan_pool::OrphanPoolError,
-    pending_pool::PendingPoolError,
-    reorg_pool::ReorgPoolError,
-    unconfirmed_pool::UnconfirmedPoolError,
+use crate::{
+    chain_storage::ChainStorageError,
+    mempool::{
+        orphan_pool::OrphanPoolError,
+        pending_pool::PendingPoolError,
+        reorg_pool::ReorgPoolError,
+        unconfirmed_pool::UnconfirmedPoolError,
+    },
+    transaction::TransactionError,
 };
 use derive_error::Error;
 
@@ -34,4 +38,8 @@ pub enum MempoolError {
     OrphanPoolError(OrphanPoolError),
     PendingPoolError(PendingPoolError),
     ReorgPoolError(ReorgPoolError),
+    TransactionError(TransactionError),
+    ChainStorageError(ChainStorageError),
+    /// The Blockchain height is undefined
+    ChainHeightUndefined,
 }
