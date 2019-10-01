3cd8559cfbbcf2b2087ab3853b3813e3dedb1e23
Stanimal <sdbondi@users.noreply.github.com>
2019-10-01 14:46:35 +0200

    Add path helpers to tari_test_utils (DRYer tests)
    These are useful when creating temporary database paths which are (still) needed for peer manager.
    
    - cargo_path returns the path to the cargo manifest dir
    - database_path returns the test data path
    - create_random_database_path creates a random path in the database_path and return it

--- infrastructure/test_utils/src/lib.rs
+++ infrastructure/test_utils/src/lib.rs
@@ -5,5 +5,8 @@
 //! ## Modules
 //!
 //! - `futures` - Contains utilities which make testing future-based code easier
+//! - `paths` - Contains utilities which return and create paths which are useful for tests involving files
+//! - `random` - Contains utilities to making generating random values easier
 pub mod futures;
+pub mod paths;
 pub mod random;
