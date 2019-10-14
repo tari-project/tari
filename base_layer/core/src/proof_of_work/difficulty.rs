16c6e35b482fb2dd55113e3e86e06a0da6a83516
SW van Heerden <swvheerden@gmail.com>
2019-10-14 10:33:09 +0200

    Add calculate difficulty function to difficulty
    Refactor ProofOfWork trait function name to better differentiate between the two

--- base_layer/core/src/proof_of_work/difficulty.rs
+++ base_layer/core/src/proof_of_work/difficulty.rs
@@ -28,6 +28,10 @@ use std::fmt;
 /// Minimum difficulty, enforced in diff retargetting
 /// avoids getting stuck when trying to increase difficulty subject to dampening
 pub const MIN_DIFFICULTY: u64 = 1;
+/// This is the time in seconds that should be the ideal time between making new blocks
+pub const BLOCK_INTERVAL: u64 = 60;
+/// This is the amount of blocks between difficulty adjustments.
+pub const BLOCKS_PER_ADJUSTMENT: u64 = 2016;
 
 /// The difficulty is defined as the maximum target divided by the block hash.
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Deserialize, Serialize)]
@@ -37,6 +41,23 @@ impl Difficulty {
     /// Difficulty of MIN_DIFFICULTY
     pub fn min() -> Difficulty {
         Difficulty(MIN_DIFFICULTY)
+    }
+
+    /// This function will calculate the required difficulty given the time taken to calculate 2016 blocks
+    /// The interval is the difference in seconds between the two headers
+    pub fn calculate_req_difficulty(interval: i64, difficulty: Difficulty) -> Difficulty {
+        let target_time = (BLOCK_INTERVAL * BLOCKS_PER_ADJUSTMENT) as f32; // 60 seconds per block, 2016 blocks
+        let deviation = (interval as f32) - target_time;
+        let mut difficulty_multiplier = 1.0 - (deviation / target_time);
+        // cap the max adjustment to 50%
+        if difficulty_multiplier >= 1.5 {
+            difficulty_multiplier = 1.5;
+        };
+        if difficulty_multiplier <= 0.5 {
+            difficulty_multiplier = 0.5;
+        };
+        // return a new difficulty that is proportionally larger or smaller depending on the time diff.
+        Difficulty((difficulty.0 as f32 * (difficulty_multiplier)) as u64)
     }
 }
 
@@ -87,5 +108,33 @@ mod test {
         );
         assert_eq!(Difficulty::default() + Difficulty::from(42), Difficulty::from(42));
         assert_eq!(&Difficulty::from(15) + &Difficulty::from(5), Difficulty::from(20));
+    }
+
+    #[test]
+    fn calc_difficulty() {
+        assert_eq!(
+            Difficulty::min(),
+            Difficulty::calculate_req_difficulty(0, Difficulty::min()),
+        );
+        let diff = Difficulty::from(9_000);
+        let new_diff = Difficulty::calculate_req_difficulty(60 * 2016, diff);
+        assert_eq!(diff, new_diff);
+        let new_diff = Difficulty::calculate_req_difficulty(60 * 2016 + 3000, diff);
+        assert_eq!(diff > new_diff, true);
+        let new_diff = Difficulty::calculate_req_difficulty(60 * 2016 - 3000, diff);
+        assert_eq!(diff < new_diff, true);
+    }
+
+    #[test]
+    fn calc_difficulty_max_min() {
+        assert_eq!(
+            Difficulty::min(),
+            Difficulty::calculate_req_difficulty(0, Difficulty::min()),
+        );
+        let diff = Difficulty::from(1000);
+        let new_diff = Difficulty::calculate_req_difficulty(60 * 2016 * 1000, diff);
+        assert_eq!(new_diff, Difficulty::from(500));
+        let new_diff = Difficulty::calculate_req_difficulty(60, diff);
+        assert_eq!(new_diff, Difficulty::from(1500));
     }
 }
