c1ed0cd95a154023bc06d253d6dd16b0d53531a5
SW van Heerden <swvheerden@gmail.com>
2019-10-29 13:58:21 +0200

    Add a basenode to a miner

--- applications/tari_testnet_miner/src/main.rs
+++ applications/tari_testnet_miner/src/main.rs
@@ -20,20 +20,37 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 use clap::{value_t, App, Arg};
-use futures::{future::FutureExt, pin_mut, select};
+use futures::{future, future::FutureExt, pin_mut, select, StreamExt};
 use log::*;
 use serde::Deserialize;
 use std::{
-    sync::atomic::Ordering,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
     time::{Duration, Instant},
 };
+
+use derive_error::Error;
+use tari_common::{load_configuration, GlobalConfig};
 use tari_core::{
     blocks::{Block, BlockBuilder, BlockHeader},
     consensus::ConsensusRules,
 };
-use tari_testnet_miner::miner::Miner;
-use tokio::io::AsyncBufReadExt;
+use tari_testnet_miner::{basenode::*, cli::*, miner::Miner};
+use tari_utilities::hex::Hex;
+use tokio::{io::AsyncBufReadExt, net::signal, runtime::Runtime};
 use tokio_executor::threadpool::ThreadPool;
+
+#[derive(Debug, Error)]
+enum ConfigError {
+    /// The config was broken
+    BrokenConfig,
+    // directory error
+    DirectoryError,
+    // problem running basenode
+    BaseNodeError,
+}
 
 const LOG_TARGET: &str = "applications::testnet_miner";
 
@@ -48,25 +65,67 @@ const LOG_TARGET: &str = "applications::testnet_miner";
 
 //     let response = base_node.get_block(request).await?;
 
-#[derive(Debug, Default, Deserialize)]
-struct Settings {
-    wallet_address: Option<String>,
-    base_node_address: Option<String>,
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let _ = env_logger::init();
+    if let Err(e) = tari_common::dir_utils::create_data_directory() {
+        println!(
+            "We couldn't create a default Tari data directory and have to quit now. This makes us sad :(\n {}",
+            e.to_string()
+        );
+        return Result::Err(Box::new(ConfigError::DirectoryError) as Box<dyn std::error::Error>);
+    }
 
-    info!(target: LOG_TARGET, "Settings loaded");
-
-    info!(target: LOG_TARGET, "Requesting new block");
+    let (global_config, node_id) = read_settings()?;
 
     let mut miner = Miner::new(ConsensusRules::current());
     let mut block = get_block();
     let mining_flag = miner.get_mine_flag();
+    let mut stop_flag = Arc::new(AtomicBool::new(false));
 
     let mut pool = ThreadPool::new();
+    // Set up the Tokio runtime for basenode
+    let rt_base_node = match setup_runtime(&global_config) {
+        Ok(rt_base_node) => rt_base_node,
+        Err(s) => {
+            error!(target: LOG_TARGET, "{}", s);
+            return Result::Err(Box::new(ConfigError::BaseNodeError) as Box<dyn std::error::Error>);
+        },
+    };
+
+    // Build, node, build!
+    let (comms, node) = match configure_and_initialize_node(&global_config, node_id, &rt_base_node) {
+        Ok(n) => n,
+        Err(e) => {
+            error!(target: LOG_TARGET, "Could not instantiate node instance. {}", e);
+            return Result::Err(Box::new(ConfigError::BaseNodeError) as Box<dyn std::error::Error>);
+        },
+    };
+
+    // Configure the shutdown daemon to listen for CTRL-C
+    let flag = node.get_flag();
+    if let Err(e) = handle_ctrl_c(&rt_base_node, flag, mining_flag, stop_flag.clone()) {
+        error!(target: LOG_TARGET, "Could not configure Ctrl-C handling. {}", e);
+        return Result::Err(Box::new(ConfigError::BaseNodeError) as Box<dyn std::error::Error>);
+    };
+
+    // Run, node, run!
+    let main = async move {
+        node.run().await;
+        debug!(
+            target: LOG_TARGET,
+            "The node has finished all it's work. initiating Comms stack shutdown"
+        );
+        match comms.shutdown() {
+            Ok(()) => info!(target: LOG_TARGET, "The comms stack reported a clean shutdown"),
+            Err(e) => warn!(
+                target: LOG_TARGET,
+                "The comms stack did not shut down cleanly: {}",
+                e.to_string()
+            ),
+        }
+    };
+    rt_base_node.spawn(main);
+
     loop {
         let height: u64 = if block.header.height <= 2016 {
             1
@@ -80,29 +139,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         // create threads
 
         let t_miner = mine(&mut miner, header, &mut pool).fuse();
-        let t_cli = stop_mining().fuse();
         let t_tip = check_tip(0).fuse();
         pin_mut!(t_miner);
-        pin_mut!(t_cli);
         pin_mut!(t_tip);
-
-        let mut stop_flag = false;
 
         select! {
         () = t_miner => {info!(target: LOG_TARGET, "Mined a block");
                         send_block();
                         },
-        () = t_cli => {info!(target: LOG_TARGET, "Canceled mining");
-                        mining_flag.store(true, Ordering::Relaxed);
-                        stop_flag = true;
-                        },
         () = t_tip => info!(target: LOG_TARGET, "Canceled mining on current block, tip changed"),}
-        if stop_flag {
+        if stop_flag.load(Ordering::Relaxed) {
             break;
         }
         block = get_block();
     }
     pool.shutdown().await;
+    rt_base_node.shutdown_on_idle();
     Ok(())
 }
 
@@ -134,54 +186,60 @@ async fn check_tip(current_tip: u64) {
 }
 
 /// Function to read in the settings, either from the config file or the cli
-fn read_settings() -> Settings {
-    let mut settings = Settings::default();
-    let matches = App::new("Tari test-net miner")
-        .version("0.1")
-        .arg(
-            Arg::with_name("config")
-                .value_name("FILE")
-                .long("config")
-                .short("c")
-                .help("The relative path of the miner config.toml file")
-                .takes_value(true)
-                .required(false),
-        )
-        .arg(
-            Arg::with_name("wallet_address")
-                .long("wallet_address")
-                .short("w")
-                .help("The address the wallet should use to connect to")
-                .takes_value(true)
-                .required(false),
-        )
-        .arg(
-            Arg::with_name("base_node_address")
-                .long("base_node_address")
-                .short("b")
-                .help("This is the address the server should use to connect to the base_node for blocks")
-                .takes_value(true)
-                .required(false),
-        )
-        .get_matches();
-    if matches.is_present("config") {
-        let mut settings_file = config::Config::default();
-        settings_file
-            .merge(config::File::with_name(matches.value_of("config").unwrap()))
-            .expect("Could not open specified config file");
-        settings = settings_file.try_into().unwrap();
-    }
-    if let Some(_c) = matches.values_of("wallet_address") {
-        if let Ok(v) = value_t!(matches, "wallet_address", String) {
-            settings.wallet_address = Some(v)
-        }
-    }
-    if let Some(_c) = matches.values_of("base_node_address") {
-        if let Ok(v) = value_t!(matches, "base_node_address", String) {
-            settings.base_node_address = Some(v);
-        }
-    }
-    settings
+fn read_settings(
+) -> Result<(GlobalConfig, tari_comms::peer_manager::node_identity::NodeIdentity), Box<dyn std::error::Error>> {
+    print_banner();
+    let arguments = parse_cli_args();
+    // Load and apply configuration file
+    let cfg = match load_configuration(&arguments.bootstrap) {
+        Ok(cfg) => cfg,
+        Err(s) => {
+            error!(target: LOG_TARGET, "{}", s);
+            return Result::Err(Box::new(ConfigError::BrokenConfig));
+        },
+    };
+
+    // Populate the configuration struct
+    let node_config = match GlobalConfig::convert_from(cfg) {
+        Ok(c) => c,
+        Err(e) => {
+            error!(target: LOG_TARGET, "The configuration file has an error. {}", e);
+            return Result::Err(Box::new(ConfigError::BrokenConfig));
+        },
+    };
+
+    // Load or create the Node identity
+    let node_id = match load_identity(&node_config.identity_file, &node_config.address) {
+        Ok(id) => id,
+        Err(e) => {
+            if !arguments.create_id {
+                error!(
+                    target: LOG_TARGET,
+                    "Node identity information not found. {}. You can update the configuration file to point to a \
+                     valid node identity file, or re-run the node with the --create_id flag to create anew identity.",
+                    e
+                );
+                return Result::Err(Box::new(ConfigError::BrokenConfig));
+            }
+            debug!(target: LOG_TARGET, "Node id not found. {}. Creating new ID", e);
+            match create_and_save_id(&node_config.identity_file, &node_config.address) {
+                Ok(id) => {
+                    info!(
+                        target: LOG_TARGET,
+                        "New node identity [{}] with public key {} has been created.",
+                        id.identity.node_id.to_hex(),
+                        id.public_key().to_hex()
+                    );
+                    id
+                },
+                Err(e) => {
+                    error!(target: LOG_TARGET, "Could not create new node id. {}.", e);
+                    return Result::Err(Box::new(ConfigError::BrokenConfig));
+                },
+            }
+        },
+    };
+    Ok((node_config, node_id))
 }
 
 // todo get block here
@@ -204,4 +262,45 @@ fn get_chain_tip_height() -> u64 {
 // todo propagate block
 fn send_block() {
     println!("sending mined block out");
+}
+
+fn setup_runtime(config: &GlobalConfig) -> Result<Runtime, String> {
+    let num_core_threads = config.core_threads;
+    let num_blocking_threads = config.blocking_threads;
+
+    debug!(
+        target: LOG_TARGET,
+        "Configuring the node to run on {} core threads and {} blocking worker threads.",
+        num_core_threads,
+        num_blocking_threads
+    );
+    tokio::runtime::Builder::new()
+        .blocking_threads(num_blocking_threads)
+        .core_threads(num_core_threads)
+        .build()
+        .map_err(|e| format!("There was an error while building the node runtime. {}", e.to_string()))
+}
+
+/// Set the interrupt flag on the node when Ctrl-C is entered
+fn handle_ctrl_c(
+    rt: &Runtime,
+    flag: Arc<AtomicBool>,
+    mining_flag: Arc<AtomicBool>,
+    stop_flag: Arc<AtomicBool>,
+) -> Result<(), String>
+{
+    let ctrl_c = signal::ctrl_c().map_err(|e| e.to_string())?;
+    let s = ctrl_c.take(1).for_each(move |_| {
+        info!(
+            target: LOG_TARGET,
+            "Termination signal received from user. Shutting miner down."
+        );
+        // We need to shutdown the basenode, the mining workers and the mining main thread loop.
+        flag.store(true, Ordering::SeqCst);
+        mining_flag.store(true, Ordering::SeqCst);
+        stop_flag.store(true, Ordering::SeqCst);
+        future::ready(())
+    });
+    rt.spawn(s);
+    Ok(())
 }
