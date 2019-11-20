c544e598191dc7682c8dcd38cadeecd842410af6
Philip Robinson <simian@tari.com>
2019-11-20 14:36:51 +0200

    Add complete error logging to Wallet services
    Errors were not being written to logs in the wallet services but now all service requests that return an error will be logged before returning the error.

--- base_layer/wallet/src/output_manager_service/service.rs
+++ base_layer/wallet/src/output_manager_service/service.rs
@@ -101,7 +101,10 @@ where T: OutputManagerBackend
             futures::select! {
                 request_context = request_stream.select_next_some() => {
                     let (request, reply_tx) = request_context.split();
-                    let _ = reply_tx.send(self.handle_request(request).await).or_else(|resp| {
+                    let _ = reply_tx.send(self.handle_request(request).await.or_else(|resp| {
+                        error!(target: LOG_TARGET, "Error handling request: {:?}", resp);
+                        Err(resp)
+                    })).or_else(|resp| {
                         error!(target: LOG_TARGET, "Failed to send reply");
                         Err(resp)
                     });
