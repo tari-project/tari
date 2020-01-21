642c1298ced732ba1df00274845afbf579133159
SW van Heerden <swvheerden@gmail.com>
2020-01-21 11:35:46 +0200

    remove incorrect  mentions of grin

--- base_layer/core/src/blocks/genesis_block.rs
+++ base_layer/core/src/blocks/genesis_block.rs
@@ -19,9 +19,6 @@
 // SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// Portions of this file were originally copyrighted (c) 2018 The Grin Developers, issued under the Apache License,
-// Version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0.
 
 // This file is used to store the genesis block
 use crate::{
