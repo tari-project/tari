2282c8363e14c8a5b4d48410e11dea3a0d527479
StriderDM <51991544+StriderDM@users.noreply.github.com>
2020-03-09 10:50:28 +0200

    Base Node setup script
    Added setup.sh
    
    Fix
    
    Start tor in seperate window
    
    Update start_tor.sh
    
    Update setup.sh

---
+++ applications/tari_base_node/start_tor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Script to start tor
+#
+tor --allow-missing-torrc --ignore-missing-torrc --clientonly 1 --socksport 9050 --controlport 127.0.0.1:9051 --log "notice stdout" --clientuseipv6 1
