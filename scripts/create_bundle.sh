2ec666fec35d250bae8a54395e6169176afd00df
CjS77 <CjS77@users.noreply.github.com>
2020-03-16 13:23:15 +0200

    Add quick and dirty OsX installer
    This PR is a minimal viable installer for OsX.
    
    The create_bundle script packages the release binary with working
    minimal config files.
    Then install-osx will install dependencies, create an id and run the
    node. It also runs Tor in a separate terminal.
    
    As I said, this is an MVP. There are lots of enhancements that can and
    should be made to this; but this is a start.

---
+++ scripts/create_bundle.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script creates a zip bundle for distribution
+# Right now, it only builds an OsX bundle
+
+print_usage () {
+  echo "Usage:
+  create_bundle [filename]
+"
+}
+
+OUTFILE=$1
+
+if [ "$OUTFILE" = "" ]; then
+  print_usage
+  exit
+fi
+
+BUNDLE='
+target/release/tari_base_node
+scripts/install_tor.sh
+config/presets/rincewind-simple.toml
+common/logging/log4rs-sample.yml
+applications/tari_base_node/install-osx.sh
+applications/tari_base_node/start_tor.sh
+'
+
+# Create a zip file, stripping out paths (-j)
+zip -j - $BUNDLE > $OUTFILE
+
+
