1d97332cc3e4cabc157c11b70ac0a87c04b5f5ae
StriderDM <51991544+StriderDM@users.noreply.github.com>
2020-07-10 11:56:12 +0200

    Tari Conduit Application
    Added application which XMRig can be connected to which forwards requests back and forth between XMRig and MoneroD.
    Added parsing of Monero requests from XMRig and adding of merge mining
    data to response from MoneroD
    Added integration to Tari Base Node for merged mining.
    
    Review comments
    
    Refactor and ReadMe
    
    Updated as per review  comments
    
    Referenced proto file in tari_base_node

--- applications/tari_conduit/src/grpc_conversions/mod.rs
+++ applications/tari_conduit/src/grpc_conversions/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2019. The Tari Project
+// Copyright 2020. The Tari Project
 //
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 // following conditions are met:
@@ -20,44 +20,5 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#[cfg(feature = "base_node")]
-mod blake_pow;
-#[cfg(any(feature = "base_node", feature = "transactions"))]
-mod difficulty;
-#[cfg(any(feature = "base_node", feature = "transactions"))]
-mod error;
-#[cfg(feature = "base_node")]
-mod median_timestamp;
-#[cfg(feature = "base_node")]
-#[allow(clippy::enum_variant_names)]
-pub mod monero_rx;
-#[cfg(feature = "base_node")]
-#[allow(clippy::module_inception)]
-mod proof_of_work;
-#[cfg(any(feature = "base_node", feature = "transactions"))]
-mod proof_of_work_algorithm;
-#[cfg(feature = "base_node")]
-mod target_difficulty;
-
-#[cfg(feature = "base_node")]
-#[cfg(test)]
-pub use blake_pow::test as blake_test;
-#[cfg(feature = "base_node")]
-pub mod lwma_diff;
-#[cfg(feature = "base_node")]
-pub use blake_pow::{blake_difficulty, blake_difficulty_with_hash};
-#[cfg(any(feature = "base_node", feature = "transactions"))]
-pub use difficulty::{Difficulty, DifficultyAdjustment};
-#[cfg(any(feature = "base_node", feature = "transactions"))]
-pub use error::{DifficultyAdjustmentError, PowError};
-#[cfg(feature = "base_node")]
-pub use median_timestamp::get_median_timestamp;
-#[cfg(feature = "base_node")]
-pub use monero_rx::monero_difficulty;
-#[cfg(feature = "base_node")]
-pub use proof_of_work::ProofOfWork;
-#[cfg(feature = "base_node")]
-pub use target_difficulty::get_target_difficulty;
-
-#[cfg(any(feature = "base_node", feature = "transactions"))]
-pub use proof_of_work_algorithm::PowAlgorithm;
+pub mod blocks;
+pub mod transactions;
