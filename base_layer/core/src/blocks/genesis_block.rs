f9ff2c248b75c4efdfd8bee09e39e51b2d379389
SW van Heerden <swvheerden@gmail.com>
2021-01-28 09:36:09 +0200

    Update timestamp for stibbons
    Fix difficulties for stibbons

--- base_layer/core/src/blocks/genesis_block.rs
+++ base_layer/core/src/blocks/genesis_block.rs
@@ -129,7 +129,7 @@ pub fn get_stibbons_genesis_block_raw() -> Block {
             prev_hash: vec![
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             ],
-            timestamp: 1_603_843_200.into(), // 10/28/2020 @ 12:00am (UTC)
+            timestamp: 1_611_835_200.into(), // 28/01/2021 @ 12:00pm (UTC)
             output_mr: from_hex("dcc44f39b65e5e1e526887e7d56f7b85e2ea44bd29bc5bc195e6e015d19e1c06").unwrap(),
             range_proof_mr: from_hex("e4d7dab49a66358379a901b9a36c10f070aa9d7bdc8ae752947b6fc4e55d255f").unwrap(),
             output_mmr_size: 1,
