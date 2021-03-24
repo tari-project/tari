864929330274d36bcbfebba5e5bc6ece874346a9
Byron Hambly <bizzle@tari.com>
2021-03-24 09:53:57 +0200

    [base-node] Update mempool consensus validator to check weight excluding coinbase
    Thanks to @SWvheerden for pointing out that the mempool consensus
    validator needs to check against the max block weight excluding
    space for a coinbase utxo and kernel.

--- base_layer/core/src/validation/transaction_validators.rs
+++ base_layer/core/src/validation/transaction_validators.rs
@@ -74,7 +74,7 @@ impl<B: BlockchainBackend> MempoolTransactionValidation for TxConsensusValidator
     fn validate(&self, tx: &Transaction) -> Result<(), ValidationError> {
         let consensus_constants = self.db.consensus_constants()?;
         // validate maximum tx weight
-        if tx.calculate_weight() > consensus_constants.get_max_block_transaction_weight() {
+        if tx.calculate_weight() > consensus_constants.get_max_block_weight_excluding_coinbase() {
             return Err(ValidationError::MaxTransactionWeightExceeded);
         }
 
