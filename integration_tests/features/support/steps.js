15b6d6c3617f2cbfd2b6071ea8c161872cbcd585
Cifko <gcifko@gmail.com>
2021-06-22 16:58:04 +0200

    Let the test pass if 1% of nodes will fail. The number of nodes that need to succeed is rounded down. Change from promise.all to a custom wait for N promises.

--- integration_tests/features/support/steps.js
+++ integration_tests/features/support/steps.js
@@ -889,21 +889,25 @@ Then(/(.*) has (.*) in (.*) state/, async function (node, txn, pool) {
   expect(this.lastResult.result).to.equal(pool);
 });
 
+// The number is rounded down. E.g. if 1% can fail out of 17, that is 16.83 have to succeed.
+// It's means at least 16 have to succeed.
 Then(
-  /(.*) is in the (.*) of all nodes/,
+  /(.*) is in the (.*) of all nodes(, where (\d+)% can fail)?/,
   { timeout: 1200 * 1000 },
-  async function (txn, pool) {
+  async function (txn, pool, canFail) {
     const sig = this.transactions[txn].body.kernels[0].excess_sig;
-    await this.forEachClientAsync(async (client, name) => {
-      await waitFor(
-        async () => client.transactionStateResult(sig),
-        pool,
-        1200 * 1000
-      );
-      this.lastResult = await client.transactionState(sig);
-      console.log(`Node ${name} response is: ${this.lastResult.result}`);
-      expect(this.lastResult.result).to.equal(pool);
-    });
+    await this.forEachClientAsync(
+      async (client, name) => {
+        await waitFor(
+          async () => client.transactionStateResult(sig),
+          pool,
+          1200 * 1000
+        );
+        this.lastResult = await client.transactionState(sig);
+        console.log(`Node ${name} response is: ${this.lastResult.result}`);
+      },
+      canFail ? parseInt(canFail) : 0
+    );
   }
 );
 
