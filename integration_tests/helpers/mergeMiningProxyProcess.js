beca2b08d834b796b8bc664cad5cc14ee32cafb1
SW van Heerden <swvheerden@gmail.com>
2021-08-06 14:24:34 +0200

    update to use sydney more address

--- integration_tests/helpers/mergeMiningProxyProcess.js
+++ integration_tests/helpers/mergeMiningProxyProcess.js
@@ -65,6 +65,8 @@ class MergeMiningProxyProcess {
       const extraEnvs = {
         TARI_MERGE_MINING_PROXY__LOCALNET__PROXY_SUBMIT_TO_ORIGIN:
           this.submitOrigin,
+        TARI_MERGE_MINING_PROXY__LOCALNET__monerod_url:
+          "http://3.104.4.129:18081",
       };
       const completeEnvs = { ...envs, ...extraEnvs };
       const ps = spawn(cmd, args, {
