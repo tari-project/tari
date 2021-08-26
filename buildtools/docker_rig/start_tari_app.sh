694a4f54b07f149772cb9b36d0ad1c7cb2da072a
Byron Hambly <bizzle@tari.com>
2021-08-26 15:15:29 +0200

    fix: get base node docker working in docker rig

--- buildtools/docker_rig/start_tari_app.sh
+++ buildtools/docker_rig/start_tari_app.sh
@@ -2,7 +2,7 @@
 #
 # Docker Start Script for Tari applications
 # The docker compose environment should set the following envars
-# - APP_NAME - the name of the app to run. This var is used to set the location of log files, and app-soecific config
+# - APP_NAME - the name of the app to run. This var is used to set the location of log files, and app-specific config
 # - APP_EXEC - the name of the application executable. Just the name is enough, since the Dockerfile will put it in /usr/bin
 # - CREATE_CONFIG - set to 1 if we should write a default config file if one is missing.
 # - CREATE_ID - set to 1 if we should create an id file for this application if one is missing. It will be called
@@ -34,22 +34,19 @@ if [[ $WAIT_FOR_TOR == 1 ]]; then
   sleep 30
 fi
 
-cd $TARI_BASE
-
-if [[ $CREATE_CONFIG == 1 && ! -f $CONFIG/config.toml ]]; then
-  echo "I could not find a global Tari configuration file. I can create a default one for you, or you can set this up"
-  echo "yourself and place it in the global config path (usually ~/.tari/config/config.toml, but YMMV)"
-  # TODO what it says on the box
-  exit 1
-fi
-
+cd "$TARI_BASE" || exit 1
 ID_FILENAME=${NETWORK}_${APP_NAME}_id.json
 
 if [[ $CREATE_ID && ! -f $ID_FILENAME ]]; then
+  echo "Creating network identity file ($ID_FILENAME)."
+  $APP_EXEC -b "$TARI_BASE" --create-id || exit 1
+  echo "done"
+fi
 
-  echo "I could not find a network identity file for this node ($ID_FILENAME)."
-  echo "So I'll create one for you real quick."
-  $APP_EXEC -c $CONFIG/config.toml -b $TARI_BASE --create_id
+if [[ $CREATE_CONFIG == 1 && ! -f $CONFIG/config.toml ]]; then
+  echo "Creating config file"
+  $APP_EXEC -b "$TARI_BASE" --init || exit 1
+  echo "done"
 fi
 
 $APP_EXEC "$@"
