fa257cd8186a99afab36aa3451017c3fd24f2641
Stan Bondi <sdbondi@users.noreply.github.com>
2021-09-06 16:32:08 +0400

    refactor: misc minor improvements + add MissTickBehaviour to all timeouts (#3296)
    Description
    ---
    - Adds `MissedTickBehaviour` to every usage of `tokio::time::interval`
    - Removes false error message saying failed to send dial reply, reply to dial is now optional, log error message will only occur if the reply is specified and then dropped
    - Outbound messaging protocol inactivity case was incorrectly handled 
    - Removes erroneous blockchain db error log when database is full and needs a resize
    
    _Apologies for this PR being a bit miscellaneous_
    
    Motivation and Context
    ---
    - MissedTickBehaviour default is Burst. In most cases that we use interval, this is undesirable. `MissedTickBehaviour::Delay` or `Skip` are used as appropriate.
    - When ConnectivityManager calls `dial_peer` it does not require a reply with the `PeerConnection`, changed the internal datastructures to allow this case. Previously the oneshot was created and immediately dropped to satisfy the API which cause the false error to log.
    - Previously, an io::Error was returned when the stream was inactive for a certain length of time. An IO error was used just to satisfy the `stream.forward` combinator. A native MessagingProtocolError is now used, so the inactive case is now handled correctly.
    
    How Has This Been Tested?
    ---
    Existing tests + manually

--- base_layer/core/src/chain_storage/lmdb_db/lmdb.rs
+++ base_layer/core/src/chain_storage/lmdb_db/lmdb.rs
@@ -78,31 +78,28 @@ where
 {
     let val_buf = serialize(val)?;
     trace!(target: LOG_TARGET, "LMDB: {} bytes inserted", val_buf.len());
-    txn.access().put(&db, key, &val_buf, put::NOOVERWRITE).map_err(|e| {
-        error!(
-            target: LOG_TARGET,
-            "Could not insert value into lmdb {} ({}/{:?}): {:?}",
+    match txn.access().put(&db, key, &val_buf, put::NOOVERWRITE) {
+        Ok(_) => Ok(()),
+        Err(lmdb_zero::Error::Code(lmdb_zero::error::KEYEXIST)) => Err(ChainStorageError::KeyExists {
             table_name,
-            to_hex(key.as_lmdb_bytes()),
-            val,
-            e,
-        );
-        if let lmdb_zero::Error::Code(code) = &e {
-            if *code == lmdb_zero::error::KEYEXIST {
-                return ChainStorageError::KeyExists {
-                    table_name,
-                    key: to_hex(key.as_lmdb_bytes()),
-                };
-            }
-            if *code == lmdb_zero::error::MAP_FULL {
-                return ChainStorageError::DbResizeRequired;
-            }
-        }
-        ChainStorageError::InsertError {
-            table: table_name,
-            error: e.to_string(),
-        }
-    })
+            key: to_hex(key.as_lmdb_bytes()),
+        }),
+        Err(lmdb_zero::Error::Code(lmdb_zero::error::MAP_FULL)) => Err(ChainStorageError::DbResizeRequired),
+        Err(e) => {
+            error!(
+                target: LOG_TARGET,
+                "Could not insert value into lmdb {} ({}/{:?}): {:?}",
+                table_name,
+                to_hex(key.as_lmdb_bytes()),
+                val,
+                e,
+            );
+            Err(ChainStorageError::InsertError {
+                table: table_name,
+                error: e.to_string(),
+            })
+        },
+    }
 }
 
 /// Note that calling this on a table that does not allow duplicates will replace it
