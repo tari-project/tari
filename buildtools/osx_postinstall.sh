a6dda915cef97d46c0ec126e12d1a4c1c2a4a5af
Byron Hambly <bizzle@tari.com>
2021-10-22 12:10:43 +0200

    remove scripts after install

--- buildtools/osx_postinstall.sh
+++ buildtools/osx_postinstall.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
+# logs are in /var/log/install.log
 whoami
 echo "$HOME"
 echo "$USER"
 mkdir "$HOME"/.tari/ || exit 1
 cp -R /tmp/tari/ "$HOME"/.tari/ || exit 1
 chown -R "$USER":staff "$HOME"/.tari/ || exit 1
+rm -rf "$HOME"/.tari/scripts/ || exit 1
 
 exit 0
