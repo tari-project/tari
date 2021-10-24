870a9177284b1ae156f36f396c6fd6c18c5f1f63
Stanimal <sdbondi@users.noreply.github.com>
2021-10-24 15:59:30 +0400

    fix: go to manage asset page after asset save

--- applications/tari_collectibles/web-app/src/App.js
+++ applications/tari_collectibles/web-app/src/App.js
@@ -1,4 +1,3 @@
-
 //  Copyright 2021. The Tari Project
 //
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
@@ -22,102 +21,126 @@
 //  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import Dashboard from "./Dashboard";
-import {Route,  BrowserRouter as Router, Switch, Link as RouterLink,} from "react-router-dom";
-import {createTheme} from "@mui/material/styles";
 import {
-    AppBar, Box,
-    CssBaseline,
-    Drawer,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-    Toolbar,
-    Typography
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Link as RouterLink,
+} from "react-router-dom";
+import { createTheme } from "@mui/material/styles";
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
 } from "@mui/material";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import CreateIcon from "@mui/icons-material/Create";
 import AppsIcon from "@mui/icons-material/Apps";
-import {ThemeProvider} from "@emotion/react";
+import { ThemeProvider } from "@emotion/react";
 import Create from "./Create";
-import {deepPurple, pink} from "@mui/material/colors";
+import { deepPurple, pink } from "@mui/material/colors";
 import "./App.css";
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Library from "./Library";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Manage from "./Manage";
 import AssetManager from "./AssetManager";
 
-
 const mdTheme = createTheme({
-    palette: {
-        mode: 'dark',
-        primary: deepPurple,
-        secondary: pink
-    },
+  palette: {
+    mode: "dark",
+    primary: deepPurple,
+    secondary: pink,
+  },
 });
 
-
 function ListItemLink(props) {
-    const { icon, primary, to } = props;
+  const { icon, primary, to } = props;
 
-    const renderLink = React.useMemo(
-        () =>
-            React.forwardRef(function Link(itemProps, ref) {
-                return <RouterLink to={to} ref={ref} {...itemProps} role={undefined} />;
-            }),
-        [to],
-    );
+  const renderLink = React.useMemo(
+    () =>
+      React.forwardRef(function Link(itemProps, ref) {
+        return <RouterLink to={to} ref={ref} {...itemProps} role={undefined} />;
+      }),
+    [to]
+  );
 
-    return (
-        <li>
-            <ListItem button component={renderLink}>
-                {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
-                <ListItemText primary={primary} />
-            </ListItem>
-        </li>
-    );
+  return (
+    <li>
+      <ListItem button component={renderLink}>
+        {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+        <ListItemText primary={primary} />
+      </ListItem>
+    </li>
+  );
 }
 
 ListItemLink.propTypes = {
-    icon: PropTypes.element,
-    primary: PropTypes.string.isRequired,
-    to: PropTypes.string.isRequired,
+  icon: PropTypes.element,
+  primary: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
 };
-
 
 function App() {
   return (
     <div className="App">
-        <Router>
-            <ThemeProvider theme={mdTheme}>
-                <Box sx={{ display: 'flex'}}>
-                    <CssBaseline />
-                    <AppBar position="absolute">
-                        <Toolbar>
-                            <Typography component="h1">Hello world</Typography></Toolbar></AppBar>
-                    <Drawer variant="permanent">
-                        <Toolbar sx={{ display: 'flex'}}>
-                            Tari Collectibles
-                        </Toolbar>
-                        <List>
-                            <ListItemLink primary="Dashboard" to="/" icon={<DashboardIcon />}/>
-                            <ListItemLink primary ="Library" to="/library" icon={<AppsIcon />}/>
-                            <ListItemLink primary="Create" to="/create" icon={<CreateIcon />}/>
-                        </List>
-                    </Drawer>
-                    <Box component="main" sx={{ flexGrow: 1, height: '100vh', overflow: 'auto'}}>
-
-            <Switch>
-                <Route path="/create"><Create /></Route>
-                <Route path="/library"><Library /></Route>
-                <Route path="/assets/manage/:assetPubKey"><AssetManager /></Route>
-            <Route path="/"><Dashboard /></Route></Switch>
-
-                    </Box>
-                </Box>
-            </ThemeProvider>
-                    </Router>
-
+      <Router>
+        <ThemeProvider theme={mdTheme}>
+          <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <AppBar position="absolute">
+              <Toolbar>
+                <Typography component="h1">Hello world</Typography>
+              </Toolbar>
+            </AppBar>
+            <Drawer variant="permanent">
+              <Toolbar sx={{ display: "flex" }}>Tari Collectibles</Toolbar>
+              <List>
+                <ListItemLink
+                  primary="Dashboard"
+                  to="/"
+                  icon={<DashboardIcon />}
+                />
+                <ListItemLink
+                  primary="Library"
+                  to="/library"
+                  icon={<AppsIcon />}
+                />
+                <ListItemLink
+                  primary="Create"
+                  to="/create"
+                  icon={<CreateIcon />}
+                />
+              </List>
+            </Drawer>
+            <Box
+              component="main"
+              sx={{ flexGrow: 1, height: "100vh", overflow: "auto" }}
+            >
+              <Switch>
+                <Route path="/create">
+                  <Create />
+                </Route>
+                <Route path="/library">
+                  <Manage />
+                </Route>
+                <Route path="/assets/manage/:assetPubKey">
+                  <AssetManager />
+                </Route>
+                <Route path="/">
+                  <Dashboard />
+                </Route>
+              </Switch>
+            </Box>
+          </Box>
+        </ThemeProvider>
+      </Router>
     </div>
   );
 }
