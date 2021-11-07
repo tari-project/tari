2cbc2548b7be1016e7713b97ceb75aa3779e4dfe
Stanimal <sdbondi@users.noreply.github.com>
2021-11-07 09:17:34 +0400

    fix: add missing helpers.js

---
+++ applications/tari_collectibles/web-app/src/helpers.js
@@ -0,0 +1,7 @@
+function toHexString(byteArray) {
+  return Array.from(byteArray, function (byte) {
+    return ("0" + (byte & 0xff).toString(16)).slice(-2);
+  }).join("");
+}
+
+export { toHexString };
