f32a38f409bb342e0ab507af5336abe60eaca2a8
SW van Heerden <swvheerden@gmail.com>
2021-11-19 13:32:28 +0200

    fix: allow bullet proof value only rewinding off one-sided transaction (#3587)
    Description
    ---
    This PR allows us to do bulletproof value-only rewinding on one-sided transactions.
    
    Motivation and Context
    ---
    Currently, it is not possible to do bulletproof rewinding on the value only on a one-sided transaction utxo due to the way the commitment blinding factor and bulletproof rewinding keys are created. 
    Currently, the two bulletproof rewinding keys are created as:
    ```
     let rewind_key = PrivateKey::from_bytes(&hash_secret_key(&commitment_blinding_factor))?;
     let blinding_key = PrivateKey::from_bytes(&hash_secret_key(&rewind_key))?;
    ```
    This means that if you share the rewind key, which is used to do value only rewinding, that a person can calculate the blinding key which is used to do full rewinding and expose the commitment blinding factor.  by changing the calculation order we prevent this and only allow full rewinding by something who needs to be able to do this. 
    
    ```
     let rewind_key = PrivateKey::from_bytes(&hash_secret_key(&blinding_key ))?;
     let blinding_key = PrivateKey::from_bytes(&hash_secret_key(&commitment_blinding_factor))?;
    ```
    
    How Has This Been Tested?
    ---
    
    All current test pass

--- base_layer/wallet/src/transaction_service/service.rs
+++ base_layer/wallet/src/transaction_service/service.rs
@@ -1014,8 +1014,8 @@ where
         .map_err(|e| TransactionServiceProtocolError::new(tx_id, e.into()))?;
 
         let sender_message = TransactionSenderMessage::new_single_round_message(stp.get_single_round_message()?);
-        let rewind_key = PrivateKey::from_bytes(&hash_secret_key(&spend_key))?;
-        let blinding_key = PrivateKey::from_bytes(&hash_secret_key(&rewind_key))?;
+        let blinding_key = PrivateKey::from_bytes(&hash_secret_key(&spend_key))?;
+        let rewind_key = PrivateKey::from_bytes(&hash_secret_key(&blinding_key))?;
         let rewind_data = RewindData {
             rewind_key: rewind_key.clone(),
             rewind_blinding_key: blinding_key.clone(),
