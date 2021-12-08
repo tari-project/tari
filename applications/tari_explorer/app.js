77267d1028c64761d75669297f93216718f98f1b
Mike the Tike <mikethetike@tari.com>
2021-12-08 10:27:15 +0200

    chore: add eslint to explorer

--- applications/tari_explorer/app.js
+++ applications/tari_explorer/app.js
@@ -1,30 +1,30 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const asciichart = require("asciichart");
+const createError = require("http-errors")
+const express = require("express")
+const path = require("path")
+const cookieParser = require("cookie-parser")
+const logger = require("morgan")
+const asciichart = require("asciichart")
 
-var indexRouter = require("./routes/index");
-var blocksRouter = require("./routes/blocks");
-var mempoolRouter = require("./routes/mempool");
+var indexRouter = require("./routes/index")
+var blocksRouter = require("./routes/blocks")
+var mempoolRouter = require("./routes/mempool")
 
-var hbs = require("hbs");
+var hbs = require("hbs")
 hbs.registerHelper("hex", function (buffer) {
-  return buffer ? Buffer.from(buffer).toString("hex") : "";
-});
+  return buffer ? Buffer.from(buffer).toString("hex") : ""
+})
 hbs.registerHelper("json", function (obj) {
-  return Buffer.from(JSON.stringify(obj)).toString("base64");
-});
+  return Buffer.from(JSON.stringify(obj)).toString("base64")
+})
 
 hbs.registerHelper("timestamp", function (timestamp) {
-  var dateObj = new Date(timestamp.seconds * 1000);
-  const day = dateObj.getUTCDate();
-  const month = dateObj.getUTCMonth() + 1;
-  const year = dateObj.getUTCFullYear();
-  const hours = dateObj.getUTCHours();
-  const minutes = dateObj.getUTCMinutes();
-  const seconds = dateObj.getSeconds();
+  var dateObj = new Date(timestamp.seconds * 1000)
+  const day = dateObj.getUTCDate()
+  const month = dateObj.getUTCMonth() + 1
+  const year = dateObj.getUTCFullYear()
+  const hours = dateObj.getUTCHours()
+  const minutes = dateObj.getUTCMinutes()
+  const seconds = dateObj.getSeconds()
 
   return (
     year.toString() +
@@ -38,57 +38,57 @@ hbs.registerHelper("timestamp", function (timestamp) {
     minutes.toString().padStart(2, "0") +
     ":" +
     seconds.toString().padStart(2, "0")
-  );
-});
+  )
+})
 
 hbs.registerHelper("percentbar", function (a, b) {
-  var percent = (a / (a + b)) * 100;
-  var barWidth = percent / 10;
-  var bar = "**********".slice(0, barWidth);
-  var space = "...........".slice(0, 10 - barWidth);
-  return bar + space + " " + parseInt(percent) + "% ";
-});
+  var percent = (a / (a + b)) * 100
+  var barWidth = percent / 10
+  var bar = "**********".slice(0, barWidth)
+  var space = "...........".slice(0, 10 - barWidth)
+  return bar + space + " " + parseInt(percent) + "% "
+})
 
 hbs.registerHelper("chart", function (data, height) {
   return asciichart.plot(data, {
     height: height,
-  });
-});
+  })
+})
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "hbs")
 
-app.use(logger("dev"));
-app.use(express.json());
+app.use(logger("dev"))
+app.use(express.json())
 app.use(
   express.urlencoded({
     extended: false,
   })
-);
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+)
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
 
-app.use("/", indexRouter);
-app.use("/blocks", blocksRouter);
-app.use("/mempool", mempoolRouter);
+app.use("/", indexRouter)
+app.use("/blocks", blocksRouter)
+app.use("/mempool", mempoolRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get("env") === "development" ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+  res.status(err.status || 500)
+  res.render("error")
+})
 
-module.exports = app;
+module.exports = app
