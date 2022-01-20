556d79cb82d1f84a4db0c0f6292a5a9d578d2b32
Stanimal <sdbondi@users.noreply.github.com>
2022-01-20 12:11:17 +0400

    review comments

--- base_layer/core/src/consensus/consensus_constants.rs
+++ base_layer/core/src/consensus/consensus_constants.rs
@@ -248,7 +248,7 @@ impl ConsensusConstants {
         let mut algos = HashMap::new();
         // setting sha3/monero to 40/60 split
         algos.insert(PowAlgorithm::Sha3, PowAlgorithmConstants {
-            max_target_time: 180,
+            max_target_time: 1800,
             min_difficulty: 60_000_000.into(),
             max_difficulty: u64::MAX.into(),
             target_time: 300,
