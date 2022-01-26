997b74b36221abbcb7f107eca0b78eaccb6aea87
Philip Robinson <simian@tari.com>
2022-01-26 16:40:00 +0200

    fix: properly decrypt imported faux tx when reading from db (#3754)
    Description
    ---
    The function that read the imported transaction out of the database didn’t check if the data needed to be decrypted before deserialization. This PR fixes that.
    
    
    How Has This Been Tested?
    ---
    test provided

--- base_layer/wallet/src/transaction_service/storage/sqlite_db.rs
+++ base_layer/wallet/src/transaction_service/storage/sqlite_db.rs
@@ -1217,7 +1217,12 @@ impl TransactionBackend for TransactionServiceSqliteDatabase {
         let conn = self.database_connection.get_pooled_connection()?;
         CompletedTransactionSql::index_by_status_and_cancelled(TransactionStatus::Imported, false, &conn)?
             .into_iter()
-            .map(|ct| CompletedTransaction::try_from(ct).map_err(TransactionStorageError::from))
+            .map(|mut ct: CompletedTransactionSql| {
+                if let Err(e) = self.decrypt_if_necessary(&mut ct) {
+                    return Err(e);
+                }
+                CompletedTransaction::try_from(ct).map_err(TransactionStorageError::from)
+            })
             .collect::<Result<Vec<CompletedTransaction>, TransactionStorageError>>()
     }
 }
