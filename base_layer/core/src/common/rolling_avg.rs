f091c25bca01c3d2fcc2df3f9108db307d8e9f39
Stan Bondi <sdbondi@users.noreply.github.com>
2022-03-11 15:06:18 +0400

    fix(block-sync): use avg latency to determine slow sync peer for block sync (#3912)
    Description
    ---
    Uses average blocks received per second to determine slow sync peer.
    Display average block receive time on status line
    
    Motivation and Context
    ---
    Esp. for large blocks, there could be a single large delay that would previously immediately cause the next peer to be attempted. Using the average blocks received per second, a "blip" in the latency will not cause the sync peer to switch.
    
    How Has This Been Tested?
    ---
    Manually

--- base_layer/core/src/common/rolling_avg.rs
+++ base_layer/core/src/common/rolling_avg.rs
@@ -1,4 +1,4 @@
-//  Copyright 2020, The Tari Project
+//  Copyright 2022, The Tari Project
 //
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 //  following conditions are met:
@@ -20,38 +20,43 @@
 //  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 //  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-use std::time::Duration;
+use std::{convert::TryFrom, time::Duration};
 
-use tari_comms::peer_manager::NodeId;
+use crate::common::rolling_vec::RollingVec;
 
 #[derive(Debug, Clone)]
-pub struct BlockchainSyncConfig {
-    /// The initial max sync latency. If a peer fails to stream a header/block within this deadline another sync peer
-    /// will be selected. If there are no further peers the sync will be restarted with an increased by
-    /// `max_latency_increase`.
-    pub initial_max_sync_latency: Duration,
-    /// If all sync peers exceed latency, increase allowed latency by this value
-    pub max_latency_increase: Duration,
-    /// Longer ban period for potentially malicious infractions (protocol violations etc.)
-    pub ban_period: Duration,
-    /// Short ban period for infractions that are likely not malicious (slow to respond, spotty connections etc)
-    pub short_ban_period: Duration,
-    /// An allowlist of sync peers from which to sync. No other peers will be selected for sync. If empty, sync peers
-    /// are chosen based on their advertised chain metadata.
-    pub forced_sync_peers: Vec<NodeId>,
-    /// Number of threads to use for validation
-    pub validation_concurrency: usize,
+pub struct RollingAverageTime {
+    samples: RollingVec<Duration>,
 }
 
-impl Default for BlockchainSyncConfig {
-    fn default() -> Self {
+impl RollingAverageTime {
+    pub fn new(num_samples: usize) -> Self {
         Self {
-            initial_max_sync_latency: Duration::from_secs(10),
-            max_latency_increase: Duration::from_secs(2),
-            ban_period: Duration::from_secs(30 * 60),
-            short_ban_period: Duration::from_secs(60),
-            forced_sync_peers: Default::default(),
-            validation_concurrency: 6,
+            samples: RollingVec::new(num_samples),
         }
+    }
+
+    pub fn add_sample(&mut self, sample: Duration) {
+        self.samples.push(sample);
+    }
+
+    pub fn calc_samples_per_second(&self) -> Option<f64> {
+        if self.samples.is_empty() {
+            return None;
+        }
+
+        let total_time = self.samples.iter().sum::<Duration>();
+        Some((self.samples.len() as f64 / total_time.as_micros() as f64) * 1_000_000.0)
+    }
+
+    pub fn calculate_average(&self) -> Option<Duration> {
+        if self.samples.is_empty() {
+            return None;
+        }
+
+        let total_time = self.samples.iter().sum::<Duration>();
+        Some(Duration::from_nanos(
+            u64::try_from(total_time.as_nanos()).unwrap_or(u64::MAX) / self.samples.len() as u64,
+        ))
     }
 }
