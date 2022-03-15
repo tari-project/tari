16e02d1eb36eac406602961410541d86641e7f2c
Denis Kolodin <deniskolodin@gmail.com>
2022-03-15 16:44:12 +0300

    fix: adds docs to the ParsedCommand

--- applications/tari_base_node/src/commands/nom_parser.rs
+++ applications/tari_base_node/src/commands/nom_parser.rs
@@ -1,3 +1,25 @@
+//  Copyright 2022, The Tari Project
+//
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+//  following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+//  disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+//  following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+//  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+//  products derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+//  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+//  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+//  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 use derive_more::IntoIterator;
 use nom::{
     branch::alt,
@@ -10,21 +32,26 @@ use nom::{
     IResult,
 };
 
+/// A parser that splits a line into parts
+/// taking into account quotarion marks.
 #[derive(IntoIterator)]
 pub struct ParsedCommand<'a> {
     items: Vec<&'a str>,
 }
 
 impl<'a> ParsedCommand<'a> {
+    /// Parse a string into a command.
     pub fn parse(line: &'a str) -> Result<Self, anyhow::Error> {
         parse(line).map_err(|err| anyhow::Error::msg(err.to_string()))
     }
 }
 
+/// Parses a string and drops the input if succeed.
 fn parse(input: &str) -> Result<ParsedCommand<'_>, Err<Error<&str>>> {
     parse_command(input).map(|pair| pair.1)
 }
 
+/// Trims and parses command.
 fn parse_command(input: &str) -> IResult<&str, ParsedCommand<'_>> {
     let input = input.trim();
     let (input, pairs) = parse_parameters(input)?;
@@ -32,6 +59,7 @@ fn parse_command(input: &str) -> IResult<&str, ParsedCommand<'_>> {
     Ok((input, command))
 }
 
+/// Parses many parameters delimited by a multispace.
 fn parse_parameters(input: &str) -> IResult<&str, Vec<&str>> {
     many0(preceded(multispace0, parse_item))(input)
 }
@@ -39,14 +67,20 @@ fn parse_parameters(input: &str) -> IResult<&str, Vec<&str>> {
 const PQ: &str = "\"";
 const SQ: &str = "'";
 
+/// Not space chars allowed for a paramters.
 fn is_valid_char(c: char) -> bool {
     c != ' ' && c != '\t'
 }
 
+/// Validate parameter's chars.
 fn valid_item(input: &str) -> IResult<&str, &str> {
     take_while1(is_valid_char)(input)
 }
 
+/// Parses a parameter that can be represented as:
+/// - "parameter in double quotes"
+/// - 'parameter in signle quotes'
+/// - parameter-with-no-space
 fn parse_item(input: &str) -> IResult<&str, &str> {
     alt((
         delimited(tag(PQ), take_until(PQ), tag(PQ)),
