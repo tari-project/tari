ae8d3827a30396c904d5efb9dd68d517850030b8
Cormac Quaid <thequad@gmail.com>
2022-04-13 17:32:16 +0200

    Add SVG icon components for extracted Figma icon set (#53)
    * Adding svg icon components for extracted Figma icon set

---
+++ applications/launchpad_v2/src/styles/Icons/Minus.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { SVGProps } from "react";
+
+const SvgMinus = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M8 12h8"
+      stroke="currentColor"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+    />
+  </svg>
+);
+
+export default SvgMinus;
