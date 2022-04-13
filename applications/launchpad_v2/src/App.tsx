f48a34ba61475a500d9d438c0500c822c39194b5
tomaszantas <tom@altalogy.com>
2022-04-13 12:42:54 +0200

    Add ESLint

--- applications/launchpad_v2/src/App.tsx
+++ applications/launchpad_v2/src/App.tsx
@@ -1,42 +1,42 @@
-import {useEffect, useState} from 'react';
-import {invoke} from '@tauri-apps/api/tauri';
+import {useEffect, useState} from 'react'
+import {invoke} from '@tauri-apps/api/tauri'
 
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  const [images, setImages] = useState<string[]>([]);
-  useEffect(() => {
-    const getFromBackend = async () => {
-      const imagesFromBackend = await invoke<string[]>('image_list')
-      setImages(imagesFromBackend)
-    }
+	const [images, setImages] = useState<string[]>([])
+	useEffect(() => {
+		const getFromBackend = async () => {
+			const imagesFromBackend = await invoke<string[]>('image_list')
+			setImages(imagesFromBackend)
+		}
 
-    getFromBackend()
-  }, [])
+		getFromBackend()
+	}, [])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
           Learn React
-        </a>
+				</a>
 
-        <p>available docker images:<br/>
-          {images.map(img => <em key={img}>{img}{', '}</em>)}
-        </p>
-      </header>
-    </div>
-  );
+				<p>available docker images:<br/>
+					{images.map(img => <em key={img}>{img}{', '}</em>)}
+				</p>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
