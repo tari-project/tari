490771ecdb41185a98bf3e11efc091e824e67d5e
Cormac Quaid <69508715+corquaid@users.noreply.github.com>
2022-04-15 16:02:00 +0200

    Add theme files, refactoring

---
+++ applications/launchpad_v2/src/styles/themes/light.ts
@@ -0,0 +1,14 @@
+import styles from '../styles'
+
+const lightTheme = {
+  primary: styles.colors.dark.primary,
+  secondary: styles.colors.dark.secondary,
+  background: styles.colors.light.primary,
+  backgroundImage: styles.colors.light.backgroundImage,
+  accent: styles.colors.tari.purple,
+  accentDark: styles.colors.tari.purpleDark,
+  disabledText: styles.colors.dark.placeholder,
+  tariGradient: styles.gradients.tari,
+}
+
+export default lightTheme
