630a0655845eaab9d61e777ef2c1e07a12fbfbac
Cormac Quaid <69508715+corquaid@users.noreply.github.com>
2022-04-19 21:30:00 +0200

    feat: add typography and fonts

---
+++ applications/launchpad_v2/src/styles/typography.ts
@@ -0,0 +1,72 @@
+/**
+ * Typography styles
+ */
+
+export const font = {
+  400: 'AvenirRegular',
+  500: 'AvenirMedium',
+  800: 'AvenirHeavy'
+}
+
+const typography = {
+  header: {
+    fontSize: 32,
+    lineHeight: '44px',
+    fontFamily: 'AvenirHeavy',
+  },
+  subheader: {
+    fontSize: 24,
+    lineHeight: '38px',
+    fontFamily: 'AvenirHeavy',
+  },
+  defaultHeavy: {
+    fontSize: 16,
+    lineHeight: '25.6px',
+    fontFamily: 'AvenirHeavy',
+  },
+  defaultMedium: {
+    fontSize: 16,
+    lineHeight: '25.6px',
+    fontFamily: 'AvenirMedium',
+  },
+  defaultUnder: {
+    fontSize: 16,
+    lineHeight: '25.6px',
+    fontFamily: 'AvenirMedium',
+    textDecoration: 'underline',
+  },
+  smallHeavy: {
+    fontSize: 14,
+    lineHeight: '22.4px',
+    fontFamily: 'AvenirHeavy',
+  },
+  smallMedium: {
+    fontSize: 14,
+    lineHeight: '22.4px',
+    fontFamily: 'AvenirMedium',
+  },
+  smallUnder: {
+    fontSize: 14,
+    lineHeight: '22.4px',
+    fontFamily: 'AvenirMedium',
+    textDecoration: 'underline',
+  },
+  microHeavy: {
+    fontSize: 12,
+    lineHeight: '18px',
+    fontFamily: 'AvenirHeavy',
+  },
+  microRegular: {
+    fontSize: 12,
+    lineHeight: '18px',
+    fontFamily: 'AvenirRegular',
+  },
+  microOblique: {
+    fontSize: 12,
+    lineHeight: '18px',
+    fontFamily: 'AvenirRegular',
+    fontStyle: 'italic'
+  },
+}
+
+export default typography
