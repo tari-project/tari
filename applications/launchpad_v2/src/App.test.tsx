5b128ffae850e5cd32d5eb9afd8e96415450fc6d
Tom <tom@altalogy.com>
2022-04-19 15:20:24 +0200

    feat: launchpad CI (#55)
    * Add CI job for launchpad
    
    * Update .circleci/config.yml
    
    Co-authored-by: Mateusz Tarnaski <tarnas14@gmail.com>
    
    Co-authored-by: Mateusz Tarnaski <tarnas14@gmail.com>

--- applications/launchpad_v2/src/App.test.tsx
+++ applications/launchpad_v2/src/App.test.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
+import { randomFillSync } from 'crypto'
+import { mockIPC, clearMocks } from '@tauri-apps/api/mocks'
+
 import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />)
+beforeAll(() => {
+  window.crypto = {
+    getRandomValues: function (buffer) {
+      return randomFillSync(buffer)
+    },
+  }
+})
+
+afterEach(() => {
+  clearMocks()
+})
+
+test('renders learn react link', async () => {
+  mockIPC((cmd) => {
+    switch (cmd) {
+    case 'invoke':
+      return ['a', 'b']
+    default:
+      break
+    }
+    return ['v']
+  })
+  await act(async () => {
+    render(<App />)
+  })
   const linkElement = screen.getByText(/learn react/i)
   expect(linkElement).toBeInTheDocument()
 })
--- applications/launchpad_v2/src/index.tsx
+++ applications/launchpad_v2/src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!)
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
