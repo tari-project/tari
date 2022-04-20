7c9be596e435f73ba85cb83d393bc97114db6036
Cormac Quaid <69508715+corquaid@users.noreply.github.com>
2022-04-20 10:31:35 +0200

    feat(primitive): add text component refer issue #74

---
+++ applications/launchpad_v2/src/components/Text/types.ts
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react'
+
+/**
+ * @typedef TextProps
+ * @prop {'header' | 'subheader' | 'defaultHeavy' | 'defaultMedium' | 'defaultUnder' | 'smallHeavy' | 'smallMedium' | 'smallUnder' | 'microHeavy' | 'microRegular'  | 'microOblique' } type - text styles
+ * @prop {ReactNode} children - text content to display
+ * @prop {string} [color] - font color
+ */
+
+export interface TextProps {
+  type:
+    | 'header'
+    | 'subheader'
+    | 'defaultHeavy'
+    | 'defaultMedium'
+    | 'defaultUnder'
+    | 'smallHeavy'
+    | 'smallMedium'
+    | 'smallUnder'
+    | 'microHeavy'
+    | 'microRegular'
+    | 'microOblique'
+  children: ReactNode
+  color?: string
+}
+
+
