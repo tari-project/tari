958de1f5393f867d7dfe80e6c7cd6263844c2a41
tarnas <tarnas@altalogy.com>
2022-04-20 11:24:38 +0200

    fix types for styledComponents in Select

--- applications/launchpad_v2/src/components/Select/Select.tsx
+++ applications/launchpad_v2/src/components/Select/Select.tsx
@@ -6,24 +6,24 @@ import ArrowBottom from '../../styles/Icons/ArrowBottom1'
 import {Label, SelectButton, SelectorIcon, OptionsContainer, Option} from './styledComponents'
 
 type Option = { value: string; label: string; key: string; }
-type MyListboxProps = { fullWidth?: boolean; onDark?: boolean; label: string; value: Option; options: Option[]; onChange: (option: Option) => void }
+type MyListboxProps = { darkBackground?: boolean; label: string; value: Option; options: Option[]; onChange: (option: Option) => void }
 
-export function Select({ value, options, onChange, fullWidth, onDark, label }: MyListboxProps) {
+export function Select({ value, options, onChange, darkBackground, label }: MyListboxProps) {
   return (
     <Listbox value={value} onChange={onChange}>
       {({ open }) => <>
-        <Label onDark={onDark}>{label}</Label>
-        <SelectButton open={open} fullWidth={fullWidth} onDark={onDark}>
-          <span>{value.label}</span>
-          <SelectorIcon onDark={onDark}>
+        <Label darkBackground={darkBackground}>{label}</Label>
+        <SelectButton open={open} darkBackground={darkBackground}>
+          <span>{value?.label || ''}</span>
+          <SelectorIcon darkBackground={darkBackground}>
             <ArrowBottom />
           </SelectorIcon>
         </SelectButton>
-        <OptionsContainer fullWidth={fullWidth} onDark={onDark}>
+        <OptionsContainer darkBackground={darkBackground}>
           {options.map((option) => (
             <Listbox.Option key={option.key} value={option} as={Fragment}>
               {({ active, selected }) => (
-                <Option selected={selected} active={active} onDark={onDark}>
+                <Option selected={selected} active={active} darkBackground={darkBackground}>
                   {option.label}
                 </Option>
               )}
--- applications/launchpad_v2/src/styles/useTheme.ts
+++
@@ -1,5 +0,0 @@
-import { useSelector } from 'react-redux'
-
-import { selectThemeConfig } from '../store/app/selectors'
-
-export const useTheme = () => useSelector(selectThemeConfig)
