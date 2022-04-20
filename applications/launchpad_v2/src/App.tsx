feee761724e12ca5a6ad56b678d1e7e07a1c7073
Cormac Quaid <69508715+corquaid@users.noreply.github.com>
2022-04-20 16:19:49 +0200

    remove unnecessary fragment, formatting

--- applications/launchpad_v2/src/App.tsx
+++ applications/launchpad_v2/src/App.tsx
@@ -13,27 +13,25 @@ import './styles/App.css'
   return (
     <ThemeProvider theme={{}}>
       <GlobalStyle />
-      <>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
           Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
           Learn React
-            </a>
-            <p>available docker images:<br/>
-              {images.map(img => <em key={img}>{img}{', '}</em>)}
-            </p>
-          </header>
-        </div>
-      </>
+          </a>
+          <p>available docker images:<br/>
+            {images.map(img => <em key={img}>{img}{', '}</em>)}
+          </p>
+        </header>
+      </div>
     </ThemeProvider>
   )
 }
