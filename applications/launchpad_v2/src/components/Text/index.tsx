980cfc75f277f21d23278105b4a3a3ff8d079ee7
tarnas <tarnas@altalogy.com>
2022-04-21 19:31:41 +0200

    lint fix with prettier after merge

--- applications/launchpad_v2/src/components/Text/index.tsx
+++ applications/launchpad_v2/src/components/Text/index.tsx
@@ -1,27 +1,23 @@
-import { StyledText } from './styles' 
+import { StyledText } from './styles'
 import { TextProps } from './types'
 import styles from '../../styles/styles'
 
 /**
  * @name Text
- * 
+ *
  * @typedef TextProps
  * @prop {'header' | 'subheader' | 'defaultHeavy' | 'defaultMedium' | 'defaultUnder' | 'smallHeavy' | 'smallMedium' | 'smallUnder' | 'microHeavy' | 'microRegular'  | 'microOblique' } type - text styles
  * @prop {ReactNode} children - text content to display
- * @prop {string} [color] - font color 
- * 
+ * @prop {string} [color] - font color
+ *
  * @example
  * <Text type='defaultMedium' color={styles.colors.dark.primary}>...text goes here...</Text>
  */
 
-const Text = ({
-  type = 'defaultMedium',
-  color,
-  children,
-}: TextProps) => {
+const Text = ({ type = 'defaultMedium', color, children }: TextProps) => {
   const textStyles = {
     color: color,
-    ...styles.typography[type]
+    ...styles.typography[type],
   }
   return (
     <>
