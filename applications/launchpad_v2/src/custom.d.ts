595614039c782bcf464556ffb76719b5f85b060b
tarnas <tarnas@altalogy.com>
2022-04-21 21:31:14 +0200

    add all styles declaration to DefaultTheme in custom.d.ts

--- applications/launchpad_v2/src/custom.d.ts
+++ applications/launchpad_v2/src/custom.d.ts
@@ -11,9 +11,36 @@ declare module '*.otf'
 declare module 'styled-components' {
   export interface DefaultTheme {
     spacing: (count?: number) => string
+    spacingHorizontal: (count?: number) => string
+    spacingVertical: (count?: number) => string
     borderRadius: (count?: number) => string
+    primary: string
+    secondary: string
+    background: string
+    backgroundSecondary: string
+    backgroundImage: string
+    accent: string
+    accentDark: string
+    disabledText: string
+    tariGradient: string
     borderColor: string
     actionBackground: string
-    accent: string
+
+    titleBar: string
+
+    controlBackground: string
+
+    inverted: {
+      controlBackground: string
+      primary: string
+      secondary: string
+      background: string
+      backgroundSecondary: string
+      backgroundImage: string
+      accent: string
+      accentDark: string
+      disabledText: string
+      tariGradient: string
+    }
   }
 }
