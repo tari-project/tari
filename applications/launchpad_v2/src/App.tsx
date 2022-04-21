4778462f5dc5fa7d9033f37cfcba1008609588a4
Cormac Quaid <69508715+corquaid@users.noreply.github.com>
2022-04-21 17:08:16 +0200

    Removed GlobalStyle, moved fonts to App.css

--- applications/launchpad_v2/src/App.tsx
+++ applications/launchpad_v2/src/App.tsx
@@ -10,28 +10,22 @@ import HomePage from './pages/home'
 
 import './styles/App.css'
 
+const AppContainer = styled.div`
+  background: ${({ theme }) => theme.background};
+  display: flex;
+  flex: 1;
+  overflow: hidden;
+  borderradius: 10;
+`
+
+const App = () => {
+  const themeConfig = useSelector(selectThemeConfig)
+
   return (
-    <ThemeProvider theme={{}}>
-      <GlobalStyle />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          Learn React
-          </a>
-          <p>available docker images:<br/>
-            {images.map(img => <em key={img}>{img}{', '}</em>)}
-          </p>
-        </header>
-      </div>
+    <ThemeProvider theme={themeConfig}>
+      <AppContainer>
+        <HomePage />
+      </AppContainer>
     </ThemeProvider>
   )
 }
--- applications/launchpad_v2/src/assets/fonts/fonts.ts
+++
@@ -1,4 +0,0 @@
-export { default as AvenirRegular } from './AvenirLTStd-Book.otf'
-export { default as AvenirMedium } from './AvenirLTStd-Medium.otf'
-export { default as AvenirHeavy } from './AvenirLTStd-Heavy.otf'
-
