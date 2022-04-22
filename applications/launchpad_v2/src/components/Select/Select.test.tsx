531ae7195e3a81baee4934c594362aa94727d410
tarnas <tarnas@altalogy.com>
2022-04-22 12:25:08 +0200

    update Select tests to conform to test standard

--- applications/launchpad_v2/src/components/Select/Select.test.tsx
+++ applications/launchpad_v2/src/components/Select/Select.test.tsx
@@ -1,64 +1,65 @@
-import React from 'react'
 import { act, render, screen } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 
 import themes from '../../styles/themes'
 import Select from './'
 
-test('renders label with select', async () => {
-  // given
-  const options = [
-    {
-      value: 'Test value',
-      key: 'test',
-      label: 'Test label',
-    },
-  ]
+describe('Select', () => {
+  it('should render label with select', async () => {
+    // given
+    const options = [
+      {
+        value: 'Test value',
+        key: 'test',
+        label: 'Test label',
+      },
+    ]
 
-  // when
-  await act(async () => {
-    render(
-      <ThemeProvider theme={themes.light}>
-        <Select
-          label='Test select label'
-          value={options[0]}
-          options={options}
-          onChange={() => null}
-        />
-      </ThemeProvider>,
-    )
+    // when
+    await act(async () => {
+      render(
+        <ThemeProvider theme={themes.light}>
+          <Select
+            label='Test select label'
+            value={options[0]}
+            options={options}
+            onChange={() => null}
+          />
+        </ThemeProvider>,
+      )
+    })
+
+    // then
+    const label = screen.getByText(/Test select label/i)
+    expect(label).toBeInTheDocument()
   })
 
-  // then
-  const label = screen.getByText(/Test select label/i)
-  expect(label).toBeInTheDocument()
-})
+  it('should render selected option', async () => {
+    // given
+    const options = [
+      {
+        value: 'Test value',
+        key: 'test',
+        label: 'Test label',
+      },
+    ]
 
-test('render selected option', async () => {
-  // given
-  const options = [
-    {
-      value: 'Test value',
-      key: 'test',
-      label: 'Test label',
-    },
-  ]
+    // when
+    await act(async () => {
+      render(
+        <ThemeProvider theme={themes.light}>
+          <Select
+            label='Test select label'
+            value={options[0]}
+            options={options}
+            onChange={() => null}
+          />
+        </ThemeProvider>,
+      )
+    })
 
-  // when
-  await act(async () => {
-    render(
-      <ThemeProvider theme={themes.light}>
-        <Select
-          label='Test select label'
-          value={options[0]}
-          options={options}
-          onChange={() => null}
-        />
-      </ThemeProvider>,
-    )
+    // then
+    const label = screen.getByText(/Test label/i)
+    expect(label).toBeInTheDocument()
   })
-
-  // then
-  const label = screen.getByText(/Test label/i)
-  expect(label).toBeInTheDocument()
 })
