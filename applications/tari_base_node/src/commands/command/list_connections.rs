5833cde090e1fd93dd49f7dffaa4817381fc8624
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2022-04-23 13:03:35 +0200

    remove unused import

--- applications/tari_base_node/src/commands/command/list_connections.rs
+++ applications/tari_base_node/src/commands/command/list_connections.rs
@@ -23,7 +23,6 @@
 use anyhow::Error;
 use async_trait::async_trait;
 use clap::Parser;
-use tari_comms::peer_manager::PeerFeatures;
 use tari_core::base_node::state_machine_service::states::PeerMetadata;
 
 use super::{CommandContext, HandleCommand};
