164d63e7f4269f5adec24012d4d675b65e61e353
Cormac Quaid <69508715+corquaid@users.noreply.github.com>
2022-04-25 17:11:10 +0200

    Add password strength + smiley icons

---
+++ applications/launchpad_v2/src/styles/Icons/Password1.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { SVGProps } from 'react'
+
+const SvgPassword2 = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width='1em'
+    height='1em'
+    viewBox='0 0 22 22'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    {...props}
+  >
+    <path
+      d='M22 11A11 11 0 0 0 11 0v2.22A8.78 8.78 0 0 1 19.78 11H22Z'
+      fill='#A2281D'
+    />
+  </svg>
+)
+
+export default SvgPassword2
