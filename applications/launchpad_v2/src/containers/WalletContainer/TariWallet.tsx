8b78ef3095065b0a11327630d02502c370ca2027
tarnas <tarnas@altalogy.com>
2022-04-27 23:31:29 +0200

    add tari wallet box with emoji toggle

--- applications/launchpad_v2/src/containers/WalletContainer/TariWallet.tsx
+++ applications/launchpad_v2/src/containers/WalletContainer/TariWallet.tsx
@@ -1,4 +1,5 @@
-import { useTheme } from 'styled-components'
+import { useState } from 'react'
+import styled, { useTheme } from 'styled-components'
 
 import Box from '../../components/Box'
 import Text from '../../components/Text'
@@ -6,6 +7,63 @@ import Tag from '../../components/Tag'
 import t from '../../locales'
 
 import { TariBackgroundSignet } from './styles'
+import Smiley from './Smiley'
+
+export const Label = styled.label`
+  font-size: 1em;
+  display: inline-block;
+  margin-bottom: ${({ theme }) => theme.spacingVertical()};
+  color: ${({ theme }) => theme.inverted.primary};
+`
+
+export const SemiTransparent = styled.span`
+  opacity: 0.7;
+`
+
+const TariId = ({ tariId }: { tariId: string }) => {
+  const [showEmoji, setShowEmoji] = useState(false)
+  const theme = useTheme()
+
+  return (
+    <>
+      <Label>
+        Tari Wallet ID <SemiTransparent>(address)</SemiTransparent>
+      </Label>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          columnGap: theme.spacing(0.5),
+        }}
+      >
+        <div
+          style={{
+            flexGrow: 1,
+            border: `1px solid ${theme.borderColor}`,
+            borderRadius: theme.tightBorderRadius(),
+            padding: `${theme.spacingVertical()} ${theme.spacingHorizontal(
+              0.75,
+            )}`,
+            color: theme.borderColor,
+            backgroundColor: theme.resetBackground,
+          }}
+        >
+          {showEmoji ? 'emojis here' : tariId}
+        </div>
+        <div
+          onClick={() => setShowEmoji(a => !a)}
+          style={{ cursor: 'pointer', width: 25, textAlign: 'center' }}
+        >
+          <Smiley
+            on={showEmoji}
+            style={{ color: theme.borderColor, display: 'inline-block' }}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
 
 const TariWallet = ({ address }: { address: string }) => {
   const theme = useTheme()
@@ -32,7 +90,7 @@ const TariWallet = ({ address }: { address: string }) => {
       >
         {t.wallet.wallet.title}
       </Text>
-      <span>{address}</span>
+      <TariId tariId={address} />
     </Box>
   )
 }
