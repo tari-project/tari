c3b095ac9b82a640b2130d8efaa59765d15093c9
tarnas <tarnas@altalogy.com>
2022-04-27 08:40:24 +0200

    prepared ui for password input

--- applications/launchpad_v2/src/containers/WalletContainer/index.tsx
+++ applications/launchpad_v2/src/containers/WalletContainer/index.tsx
@@ -1,11 +1,39 @@
-/**
- * @TODO move user-facing text to i18n file when implementing
- */
+import { useTheme } from 'styled-components'
+import Box from '../../components/Box'
+import Text from '../../components/Text'
+import Button from '../../components/Button'
+import Loading from '../../components/Loading'
 
 const WalletContainer = () => {
+  const theme = useTheme()
+  const disabled = false
+  const loading = false
+
   return (
-    <div>
-      <h2>Wallet</h2>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+      }}
+    >
+      <Box>
+        <Text type='header' style={{ marginBottom: theme.spacing() }}>
+          Enter Password
+        </Text>
+        <Text>to access your wallet:</Text>
+        <Box border={false}>placeholder for input</Box>
+        <Button
+          disabled={disabled}
+          variant={disabled ? 'disabled' : undefined}
+          rightIcon={<Loading loading={loading} />}
+        >
+          <Text type='defaultMedium' style={{ lineHeight: '100%' }}>
+            Continue
+          </Text>
+        </Button>
+      </Box>
     </div>
   )
 }
