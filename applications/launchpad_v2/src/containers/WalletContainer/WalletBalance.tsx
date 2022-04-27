03eb635456aaef3f001a7f32e0b41155f8a5436a
tarnas <tarnas@altalogy.com>
2022-04-27 14:57:12 +0200

    remove Send funds button

--- applications/launchpad_v2/src/containers/WalletContainer/WalletBalance.tsx
+++ applications/launchpad_v2/src/containers/WalletContainer/WalletBalance.tsx
@@ -1,22 +1,13 @@
-import styled, { useTheme } from 'styled-components'
+import { useTheme } from 'styled-components'
 
 import Box from '../../components/Box'
 import Text from '../../components/Text'
-import Button from '../../components/Button'
 import * as FormatUtils from '../../utils/Format'
-import Arrow from '../../styles/Icons/ArrowTop2'
 import t from '../../locales'
 
 import Chart from './Chart'
 
 import { TariSignet, TariAmountContainer } from './styles'
-
-const StyledArrow = styled(Arrow)`
-  transform: rotate(45deg);
-  margin-top: -0.4em;
-  width: 2em;
-  height: 2em;
-`
 
 const WalletBalance = ({
   balance,
@@ -49,24 +40,6 @@ const WalletBalance = ({
       <Text type='defaultHeavy' style={{ display: 'inline-block' }}>
         {FormatUtils.amount(available)}
       </Text>
-      <Button
-        rightIcon={<StyledArrow />}
-        style={{
-          paddingRight: theme.spacingHorizontal(1.5),
-          marginTop: theme.spacingVertical(1),
-        }}
-      >
-        <Text
-          type='defaultMedium'
-          style={{
-            lineHeight: '100%',
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          {t.wallet.balance.sendCta}
-        </Text>
-      </Button>
     </Box>
   )
 }
