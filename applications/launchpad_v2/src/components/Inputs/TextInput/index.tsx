aac1f235bbdb9b00ae01f6ddf517266065e818b5
Cormac Quaid <69508715+corquaid@users.noreply.github.com>
2022-04-28 17:38:23 +0200

    Add JSDoc

--- applications/launchpad_v2/src/components/Inputs/TextInput/index.tsx
+++ applications/launchpad_v2/src/components/Inputs/TextInput/index.tsx
@@ -9,6 +9,21 @@ import {
 } from './styles'
 import { ChangeEvent } from 'react'
 
+/**
+ * @name TextInput component
+ * @typedef TextInputProps
+ *
+ * @prop {'static' | 'disabled'} [type] - controls some input styling and behaviour
+ * @prop {string} [value] - input text value
+ * @prop {boolean} [hideText] - show/hide input text
+ * @prop {string} [placeholder] - placeholer text
+ * @prop {ReactNode} [inputIcon] - optional icon rendered inside input field
+ * @prop {string} [inputUnits] - optional units text, e.g. 'MB' on right-hand side of input field
+ * @prop {() => void} [onIconClick] - icon click event
+ * @prop {(value: string) => void} [onChangeText] - text change event handler
+ * @prop {string} [testId] - for testing purposes
+ */
+
 const TextInput = ({
   type = 'static',
   value,
