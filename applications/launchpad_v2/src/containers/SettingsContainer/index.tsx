2a44d6910687ce3455965d8f0e1b45cd3ddc960d
tarnas <tarnas@altalogy.com>
2022-05-02 00:27:31 +0200

    created static layout for wallet settings

--- applications/launchpad_v2/src/containers/SettingsContainer/index.tsx
+++ applications/launchpad_v2/src/containers/SettingsContainer/index.tsx
@@ -1,11 +1,18 @@
 import { useState, ReactNode, ChangeEvent } from 'react'
-import styled from 'styled-components'
+import styled, { useTheme } from 'styled-components'
+import { clipboard } from '@tauri-apps/api'
 
 import Modal from '../../components/Modal'
 import Button from '../../components/Button'
 import Text from '../../components/Text'
+import Tag from '../../components/Tag'
+import Box from '../../components/Box'
+import Loading from '../../components/Loading'
+import CopyIcon from '../../styles/Icons/Copy'
 import { Settings } from '../../store/settings/types'
 import t from '../../locales'
+
+const address = '7a6ffed9-4252-427e-af7d-3dcaaf2db2df'
 
 const MainContainer = styled.div`
   display: flex;
@@ -23,7 +30,7 @@ const Sidebar = styled.aside`
   min-width: 160px;
   height: 100%;
   border-right: 1px solid ${({ theme }) => theme.borderColor};
-  padding-top: ${({ theme }) => theme.spacing()};
+  padding-top: ${({ theme }) => theme.spacing(2)};
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
@@ -66,15 +73,52 @@ const Footer = styled.footer`
 
 const MainContent = styled.main`
   flex-grow: 1;
+  padding-top: ${({ theme }) => theme.spacing(2)};
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
   & > * {
     max-width: 100%;
-    width: 60%;
+    width: 70%;
   }
 `
+
+const Link = (props: any) => <a target='_blank' {...props} />
+
+const CopyBox = ({ label, value }: { label: string; value: string }) => {
+  const theme = useTheme()
+
+  const copy = async () => {
+    await clipboard.writeText(value)
+
+    alert(`copied ${value}`)
+  }
+
+  return (
+    <>
+      <Text>{label}</Text>
+      <span
+        style={{
+          background: theme.backgroundImage,
+          border: `1px solid ${theme.borderColor}`,
+          borderRadius: theme.tightBorderRadius(),
+          color: theme.secondary,
+          padding: `${theme.spacingVertical()} ${theme.spacingHorizontal()}`,
+          margin: `${theme.spacingVertical()} 0`,
+          boxSizing: 'border-box',
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        {value}
+        <Button variant='text' style={{ padding: 0, margin: 0 }} onClick={copy}>
+          <CopyIcon />
+        </Button>
+      </span>
+    </>
+  )
+}
 
 const WalletSettings = ({
   onClose,
@@ -83,17 +127,57 @@ const WalletSettings = ({
   onClose: () => void
   onSettingsChanged: (changed: boolean) => void
 }) => {
+  const theme = useTheme()
+
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { checked } = event.target
 
     onSettingsChanged(checked)
   }
 
+  const running = true
+  const pending = false
+
   return (
     <>
-      <p>wallet settings</p>
-      <input type='checkbox' onChange={onChange} /> changed settings
-      <button onClick={onClose}>close</button>
+      <Text type='header'>Wallet Settings</Text>
+      <Box
+        style={{
+          borderRadius: 0,
+          borderLeft: 'none',
+          borderRight: 'none',
+          display: 'flex',
+          justifyContent: 'space-between',
+          paddingLeft: 0,
+          paddingRight: 0,
+        }}
+      >
+        <span
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            columnGap: theme.spacingVertical(),
+          }}
+        >
+          <Text>Wallet</Text>
+          {running && !pending ? (
+            <Tag variant='small' type='running'>
+              <span>{t.common.adjectives.running}</span>
+            </Tag>
+          ) : null}
+          {pending ? <Loading loading={true} size='12px' /> : null}
+        </span>
+        <Button variant='secondary'>Stop</Button>
+      </Box>
+      <CopyBox label='Tari Wallet ID (address)' value={address} />
+      <Text type='smallMedium' color={theme.secondary}>
+        Mined Tari is stored in Launchpad&apos;s wallet. Send funds to wallet of
+        your choice (try{' '}
+        <Link href='https://aurora.tari.com/'>Tari Aurora</Link> - it&apos;s
+        great!) and enjoy extended functionality (including payment requests,
+        recurring payments, ecommerce payments and more). To do this, you may
+        need to convert the ID to emoji format.
+      </Text>
     </>
   )
 }
--- applications/launchpad_v2/src/styles/Icons/Copy.tsx
+++ applications/launchpad_v2/src/styles/Icons/Copy.tsx
@@ -1,10 +1,9 @@
-import * as React from 'react'
 import { SVGProps } from 'react'
 
 const SvgCopy = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    width='1em'
-    height='1em'
+    width='1.25em'
+    height='1.25em'
     viewBox='0 0 24 24'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
