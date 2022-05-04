84bcb0137682ff25c3f7876f34f5457ef722c3e9
tarnas <tarnas@altalogy.com>
2022-05-04 08:23:19 +0200

    add JSDoc for copybox

--- applications/launchpad_v2/src/components/CopyBox/index.tsx
+++ applications/launchpad_v2/src/components/CopyBox/index.tsx
@@ -6,6 +6,14 @@ import CopyIcon from '../../styles/Icons/Copy'
 
 import { StyledBox } from './styles'
 
+/**
+ * @name CopyBoc
+ * @description Renders a box with value with a button that allows to copy it
+ *
+ *
+ * @prop {string} label - label describing the value
+ * @prop {string} value - value that can be copied to clipboard
+ */
 const CopyBox = ({ label, value }: { label: string; value: string }) => {
   const copy = async () => {
     await clipboard.writeText(value)
