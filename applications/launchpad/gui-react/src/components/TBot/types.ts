4eb2eec43201d473a4b2bfdc1ff9cb97fd8414b7
Cormac Quaid <69508715+corquaid@users.noreply.github.com>
2022-05-06 12:19:53 +0200

    Add JSDoc

--- applications/launchpad/gui-react/src/components/TBot/types.ts
+++ applications/launchpad/gui-react/src/components/TBot/types.ts
@@ -2,7 +2,7 @@ import { CSSProperties } from 'styled-components'
 export type TBotType = 'base' | 'hearts' | 'heartsMonero' | 'loading' | 'radar'
 
 export interface TBotProps {
-  type: TBotType
+  type?: TBotType
   size?: number
   style?: CSSProperties
 }
