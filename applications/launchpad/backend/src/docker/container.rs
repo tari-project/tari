6c4785dbb868905e0f9217f119e9f5b3634a620e
Alexander Todorov <alexander.todorov@ayagasha.com>
2022-05-06 16:23:50 +0300

    wip: Decoupling backend and UI.
    Description
    
    Provide static container holder for all created, running or stopped containers.
    Move the logic out of Workspace and create new `container` crate.
    
    Motivation and Context
    Managing docker containers state is a indepedent functionality and should be splitted from workspace configuration.
    
    How Has This Been Tested?
    
    Unit test is provide covering create, update, get and delete functionality.
    Error cases are tested as well.
    The launchpad is started and works without new bugs.

--- applications/launchpad/backend/src/docker/container.rs
+++ applications/launchpad/backend/src/docker/container.rs
@@ -1,18 +1,18 @@
 use bollard::models::ContainerCreateResponse;
 use log::debug;
 
-use super::{ContainerId, CONTAINERS, ContainerState, DockerWrapperError, ContainerStatus};
+use super::{ContainerId, ContainerState, ContainerStatus, DockerWrapperError, CONTAINERS};
 
- /// Add the container info to the list of containers the wrapper is managing
- pub fn add_container(name: &str, state: ContainerState) {
+/// Add the container info to the list of containers the wrapper is managing
+pub fn add_container(id: &str, state: ContainerState) {
     // let id = ContainerId::from(container.id.clone());
     // let state = ContainerState::new(name.to_string(), id, container);
-    CONTAINERS.write().unwrap().insert(name.to_string(), state);
+    CONTAINERS.write().unwrap().insert(id.to_string(), state);
 }
 
-// Tag the container with id `id` as Running
-pub fn change_container_status(name: &str, status: ContainerStatus) -> Result<(), DockerWrapperError> {
-    if let Some(container) = CONTAINERS.write().unwrap().get_mut(name) {
+// Tag the container with name/id `id` as Running
+pub fn change_container_status(id: &str, status: ContainerStatus) -> Result<(), DockerWrapperError> {
+    if let Some(container) = CONTAINERS.write().unwrap().get_mut(id) {
         if status == container.status() {
             debug!("Status is already set to: {:?}", status.clone());
         } else {
@@ -25,16 +25,54 @@ pub fn change_container_status(name: &str, status: ContainerStatus) -> Result<()
         }
         Ok(())
     } else {
-        Err(DockerWrapperError::ContainerNotFound(name.to_string()))
+        Err(DockerWrapperError::ContainerNotFound(id.to_string()))
     }
 }
-
-pub fn container_state(name: &str) -> Result<ContainerState, DockerWrapperError> {
-    if let Some(container) = CONTAINERS.read().unwrap().get(name) {
+///Get the state of the container by name or id.
+pub fn container_state(id: &str) -> Result<ContainerState, DockerWrapperError> {
+    if let Some(container) = CONTAINERS.read().unwrap().get(id) {
         Ok((*container).clone())
     } else {
-        Err(DockerWrapperError::ContainerNotFound(name.to_string()))
+        Err(DockerWrapperError::ContainerNotFound(id.to_string()))
+    }
+}
+///Remove the container and state.
+pub fn remove_container(id: &str) -> Result<(), DockerWrapperError> {
+    if let Some(_state) = CONTAINERS.write().unwrap().remove(id) {
+        Ok(())
+    } else {
+        Err(DockerWrapperError::ContainerNotFound(id.to_string()))
     }
 }
 
+#[test]
+fn create_get_update_and_delete_container_test() {
+    let state = ContainerState::new(
+        "tor".to_string(),
+        ContainerId("1234".to_string()),
+        ContainerCreateResponse {
+            id: "1234".to_string(),
+            warnings: vec![],
+        },
+    );
+    add_container("tor", state.clone());
+    assert_eq!(1, CONTAINERS.read().unwrap().len());
 
+    let value = container_state("tor").unwrap();
+    assert_eq!(state.name(), value.clone().name());
+    assert_eq!(state.id(), value.id());
+    assert_eq!(ContainerStatus::Created, value.status());
+
+    change_container_status("tor", ContainerStatus::Running).unwrap();
+    let value = container_state("tor").unwrap();
+    assert_eq!(ContainerStatus::Running, value.status());
+
+    change_container_status("tor", ContainerStatus::Deleted).unwrap();
+    let value = container_state("tor").unwrap();
+    assert_eq!(ContainerStatus::Deleted, value.status());
+
+    remove_container("tor").unwrap();
+    assert!(container_state("tor").is_err());
+    assert!(change_container_status("tor", ContainerStatus::Running).is_err());
+    assert!(remove_container("tor").is_err());
+}
