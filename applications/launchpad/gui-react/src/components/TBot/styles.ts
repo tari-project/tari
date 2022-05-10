4c9cbf64f3a80940df42cc7d9b4b7cb2cb780a34
Cormac Quaid <69508715+corquaid@users.noreply.github.com>
2022-05-10 13:42:29 +0200

    Add initial animations for T-Bot

--- applications/launchpad/gui-react/src/components/TBot/styles.ts
+++ applications/launchpad/gui-react/src/components/TBot/styles.ts
@@ -1,3 +1,16 @@
+import { animated } from 'react-spring'
 import styled from 'styled-components'
 
-export const TBotContainer = styled.div``
+export const TBotContainer = styled(animated.div)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+  width: 100%;
+`
+
+export const TBotScaleContainer = styled(animated.div)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
