349a65044ab7cbf7748ae898f7ac5a4c2902472f
Cormac Quaid <69508715+corquaid@users.noreply.github.com>
2022-05-10 13:42:29 +0200

    Add initial animations for T-Bot

--- applications/launchpad/gui-react/src/components/TBot/index.tsx
+++ applications/launchpad/gui-react/src/components/TBot/index.tsx
@@ -1,8 +1,10 @@
+import { useSpring, config } from 'react-spring'
 import SvgTBotBase from '../../styles/Icons/TBotBase'
 import SvgTBotHearts from '../../styles/Icons/TBotHearts'
 import SvgTBotHeartsMonero from '../../styles/Icons/TBotHeartsMonero'
 import SvgTBotLoading from '../../styles/Icons/TBotLoading'
 import SvgTBotRadar from '../../styles/Icons/TBotSearch'
+import { TBotContainer, TBotScaleContainer } from './styles'
 
 import { TBotProps } from './types'
 
@@ -11,12 +13,13 @@ import { TBotProps } from './types'
  *
  * @prop {TBotType} [type] - TBot variant to render
  * @prop {CSSProperties} [style] - optional TBot additional styling
+ * @prop {boolean} [animate] - optional prop to trigger the new message T-Bot animation
  *
  * @example
  * <TBot type='hearts' style={{ fontSize: '24px' }} />
  */
 
-const TBot = ({ type = 'base', style }: TBotProps) => {
+const TBot = ({ type = 'base', style, animate }: TBotProps) => {
   const botVariants = {
     base: SvgTBotBase,
     hearts: SvgTBotHearts,
@@ -25,9 +28,40 @@ const TBot = ({ type = 'base', style }: TBotProps) => {
     search: SvgTBotRadar,
   }
 
+  // Animation for T-Bot first render
+  const enterAnim = useSpring({
+    from: { width: '0px', height: '0px' },
+    to: {
+      width: '100%',
+      height: '100%',
+    },
+    config: config.wobbly,
+  })
+
+  // Animation for new T-Bot messages
+  const scaleAnim = useSpring({
+    from: { transform: 'scale(1)' },
+    to: {
+      transform: animate ? 'scale(1.2)' : 'scale(1)',
+      transition: 'all ease-in-out',
+    },
+    config: {
+      duration: 200,
+    },
+    loop: {
+      transform: 'scale(1)',
+    },
+  })
+
   const TBotComponent = botVariants[type]
 
-  return <TBotComponent fontSize={74} style={style} data-testid='tbot-cmp' />
+  return (
+    <TBotContainer style={scaleAnim}>
+      <TBotScaleContainer style={enterAnim}>
+        <TBotComponent fontSize={74} style={style} data-testid='tbot-cmp' />
+      </TBotScaleContainer>
+    </TBotContainer>
+  )
 }
 
 export default TBot
