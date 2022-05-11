0f3d325244f69295d12cd526b1c1421195bfc07b
tarnas <tarnas@altalogy.com>
2022-05-11 09:21:03 +0200

    catch and show generic error on basenode start/stop actions

--- applications/launchpad/gui-react/src/containers/BaseNodeContainer/index.tsx
+++ applications/launchpad/gui-react/src/containers/BaseNodeContainer/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { useAppSelector, useAppDispatch } from '../../store/hooks'
 import {
   selectState,
@@ -8,10 +10,14 @@ import {
 } from '../../store/baseNode/selectors'
 import { setExpertView } from '../../store/app'
 import { actions } from '../../store/baseNode'
+import Alert from '../../components/Alert'
+import t from '../../locales'
 
 import BaseNode from './BaseNode'
 
 const BaseNodeContainer = () => {
+  const [error, setError] = useState('')
+
   const { network } = useAppSelector(selectState)
   const pending = useAppSelector(selectPending)
   const running = useAppSelector(selectRunning)
@@ -19,18 +25,42 @@ const BaseNodeContainer = () => {
   const unhealthyContainers = useAppSelector(selectUnhealthyContainers)
   const dispatch = useAppDispatch()
 
+  const startNode = async () => {
+    try {
+      await dispatch(actions.startNode()).unwrap()
+    } catch (e) {
+      setError(t.baseNode.errors.start)
+    }
+  }
+
+  const stopNode = async () => {
+    try {
+      await dispatch(actions.stopNode()).unwrap()
+    } catch (e) {
+      setError(t.baseNode.errors.stop)
+    }
+  }
+
   return (
-    <BaseNode
-      running={running}
-      pending={pending}
-      healthy={healthy}
-      unhealthyContainers={unhealthyContainers}
-      startNode={() => dispatch(actions.startNode())}
-      stopNode={() => dispatch(actions.stopNode())}
-      tariNetwork={network}
-      setTariNetwork={network => dispatch(actions.setTariNetwork(network))}
-      openExpertView={() => dispatch(setExpertView('open'))}
-    />
+    <>
+      <BaseNode
+        running={running}
+        pending={pending}
+        healthy={healthy}
+        unhealthyContainers={unhealthyContainers}
+        startNode={startNode}
+        stopNode={stopNode}
+        tariNetwork={network}
+        setTariNetwork={network => dispatch(actions.setTariNetwork(network))}
+        openExpertView={() => dispatch(setExpertView('open'))}
+      />
+      <Alert
+        title='Error'
+        open={Boolean(error)}
+        onClose={() => setError('')}
+        content={error}
+      />
+    </>
   )
 }
 
