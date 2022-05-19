0afc622e466cd5643c80dc5a5cc776811bbeeb8a
Cormac Quaid <69508715+corquaid@users.noreply.github.com>
2022-05-19 14:31:08 +0200

    Fixing broken unit tests

--- applications/launchpad/gui-react/src/components/TBot/HelpComponents/CryptoMining/CryptoMining.test.tsx
+++ applications/launchpad/gui-react/src/components/TBot/HelpComponents/CryptoMining/CryptoMining.test.tsx
@@ -1,15 +1,20 @@
 import { render, screen } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 import themes from '../../../../styles/themes'
+import { Provider } from 'react-redux'
+import { store } from '../../../../store'
 
 import { Message1 } from '.'
 
 describe('CryptoMiningMessages', () => {
   it('should render the message component without crashing when set to open', () => {
     render(
-      <ThemeProvider theme={themes.light}>
-        <Message1 />
-      </ThemeProvider>,
+      <Provider store={store}>
+        <ThemeProvider theme={themes.light}>
+          <Message1 />
+        </ThemeProvider>
+        ,
+      </Provider>,
     )
 
     const el = screen.getByTestId('message-cmp')
--- applications/launchpad/gui-react/src/components/TBot/HelpComponents/MergedMining/MergedMining.test.tsx
+++ applications/launchpad/gui-react/src/components/TBot/HelpComponents/MergedMining/MergedMining.test.tsx
@@ -1,15 +1,20 @@
 import { render, screen } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 import themes from '../../../../styles/themes'
+import { Provider } from 'react-redux'
+import { store } from '../../../../store'
 
 import { Message1, Message2 } from '.'
 
 describe('MergedMiningMessages', () => {
   it('should render the first message component without crashing when set to open', () => {
     render(
-      <ThemeProvider theme={themes.light}>
-        <Message1 />
-      </ThemeProvider>,
+      <Provider store={store}>
+        <ThemeProvider theme={themes.light}>
+          <Message1 />
+        </ThemeProvider>
+        ,
+      </Provider>,
     )
 
     const el = screen.getByTestId('message1-cmp')
@@ -18,9 +23,12 @@ describe('MergedMiningMessages', () => {
 
   it('should render the second message component without crashing when set to open', () => {
     render(
-      <ThemeProvider theme={themes.light}>
-        <Message2 />
-      </ThemeProvider>,
+      <Provider store={store}>
+        <ThemeProvider theme={themes.light}>
+          <Message2 />
+        </ThemeProvider>
+        ,
+      </Provider>,
     )
 
     const el = screen.getByTestId('message2-cmp')
