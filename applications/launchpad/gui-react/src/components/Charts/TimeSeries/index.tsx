9575f4e149fb5b8ed0a9dbe093118db59ed32bcf
tarnas <tarnas@altalogy.com>
2022-06-07 11:35:56 +0200

    extracted charts to separate component files

---
+++ applications/launchpad/gui-react/src/components/Charts/TimeSeries/index.tsx
@@ -0,0 +1,241 @@
+import { useMemo, CSSProperties } from 'react'
+import { useTheme } from 'styled-components'
+import ApexChart from 'react-apexcharts'
+
+import colors from '../../../styles/styles/colors'
+import VisibleIcon from '../../../styles/Icons/Eye'
+import HiddenIcon from '../../../styles/Icons/EyeSlash'
+import t from '../../../locales'
+import Text from '../../Text'
+import IconButton from '../../IconButton'
+
+const graphColors = [
+  colors.secondary.infoText,
+  colors.secondary.onTextLight,
+  colors.secondary.warningDark,
+  colors.graph.fuchsia,
+  colors.secondary.warning,
+  colors.tari.purple,
+  colors.graph.yellow,
+  colors.graph.lightGreen,
+]
+
+export type ChartData = {
+  empty: boolean
+  visible: boolean
+  name: string
+  data: { x: number; y: number }[]
+}
+
+const DEFAULT_PERCENTAGE_TICK_RESOLUTION = 25
+
+const getDefaultYAxisDefinition = () => ({
+  labels: {
+    formatter: (val: number) => val.toFixed(0),
+  },
+})
+const getPercentageYAxisDefinition = (
+  data: ChartData[],
+  tickResolution: number,
+) => {
+  const ys = data.flatMap(({ data }) => data.map(({ y }) => y))
+
+  const maxY = Math.ceil(Math.max(100, ...ys) / tickResolution) * tickResolution
+
+  return {
+    min: 0,
+    max: maxY,
+    tickAmount: Math.ceil(maxY / tickResolution),
+    ...getDefaultYAxisDefinition(),
+  }
+}
+
+const TimeSeriesChart = ({
+  chartHeight,
+  data,
+  from,
+  onUserInteraction,
+  percentageValues,
+  style,
+  title,
+  to,
+  toggleSeries,
+  unit,
+}: {
+  chartHeight: number
+  data: ChartData[]
+  from: Date
+  onUserInteraction: (options: { interacting: boolean }) => void
+  percentageValues?: boolean
+  style: CSSProperties
+  title: string
+  to: Date
+  toggleSeries: (seriesName: string) => void
+  unit?: string
+}) => {
+  const theme = useTheme()
+  const unitToUse = percentageValues ? '%' : unit
+  const chartId = title
+
+  const options = useMemo(
+    () => ({
+      chart: {
+        id: chartId,
+        fontFamily: 'AvenirMedium',
+        foreColor: theme.textSecondary,
+        animations: {
+          enabled: false,
+        },
+        stacked: false,
+        zoom: {
+          enabled: false,
+        },
+        toolbar: {
+          show: false,
+        },
+        events: {
+          mouseMove: () => onUserInteraction({ interacting: true }),
+          mouseLeave: () => onUserInteraction({ interacting: false }),
+        },
+      },
+      colors: graphColors,
+      dataLabels: {
+        enabled: false,
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shadeIntensity: 1,
+          inverseColors: false,
+          opacityFrom: 0.5,
+          opacityTo: 0,
+          stops: [0, 90, 100],
+        },
+      },
+      grid: {
+        show: true,
+        position: 'back' as 'back' | 'front' | undefined,
+        borderColor: theme.resetBackground,
+        xaxis: {
+          lines: {
+            show: true,
+          },
+        },
+      },
+      stroke: {
+        show: true,
+        curve: 'smooth' as
+          | 'smooth'
+          | 'straight'
+          | 'stepline'
+          | ('smooth' | 'straight' | 'stepline')[]
+          | undefined,
+        lineCap: 'butt' as 'butt' | 'round' | 'square' | undefined,
+        colors: undefined,
+        width: 2,
+      },
+      yaxis: percentageValues
+        ? getPercentageYAxisDefinition(data, DEFAULT_PERCENTAGE_TICK_RESOLUTION)
+        : getDefaultYAxisDefinition(),
+      xaxis: {
+        type: 'datetime' as 'datetime' | 'numeric' | 'category' | undefined,
+        min: from.getTime(),
+        max: to.getTime(),
+        labels: {
+          datetimeUTC: false,
+          formatter: (value: string) =>
+            new Date(value).toLocaleTimeString([], {
+              hour: '2-digit',
+              minute: '2-digit',
+            }),
+        },
+      },
+      tooltip: {
+        theme: 'dark',
+        shared: true,
+        marker: {
+          show: true,
+        },
+        y: {
+          title: {
+            formatter: (seriesName: string) => t.common.containers[seriesName],
+          },
+          formatter: (val: number) =>
+            unitToUse ? `${val.toFixed(3)}${unitToUse}` : val.toFixed(2),
+        },
+        x: {
+          formatter: (val: number) =>
+            `${new Date(val).toLocaleDateString()} ${new Date(
+              val,
+            ).toLocaleTimeString()}`,
+        },
+      },
+      legend: {
+        show: false,
+      },
+    }),
+    [from, to],
+  )
+
+  return (
+    <div
+      style={{
+        backgroundColor: '#141414',
+        padding: theme.spacing(),
+        borderRadius: theme.borderRadius(),
+        maxWidth: '100%',
+        ...style,
+      }}
+    >
+      <Text color={theme.textSecondary} style={{ textAlign: 'center' }}>
+        {title}
+        {unitToUse ? ` [${unitToUse}]` : ''}
+      </Text>
+      <ApexChart
+        options={options}
+        series={data}
+        type='area'
+        width='100%'
+        height={chartHeight}
+      />
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          flexWrap: 'wrap',
+          columnGap: theme.spacing(),
+        }}
+      >
+        {data
+          .filter(s => !s.empty)
+          .map(({ name, visible }, seriesId) => (
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                columnGap: theme.spacing(0.5),
+              }}
+              key={name}
+            >
+              <div
+                style={{
+                  width: '1em',
+                  height: '0.1em',
+                  borderRadius: '2px',
+                  backgroundColor: graphColors[seriesId],
+                }}
+              />
+              <Text type='smallMedium' color={theme.textSecondary}>
+                {t.common.containers[name]}
+              </Text>
+              <IconButton onClick={() => toggleSeries(name)}>
+                {visible ? <VisibleIcon /> : <HiddenIcon />}
+              </IconButton>
+            </div>
+          ))}
+      </div>
+    </div>
+  )
+}
+
+export default TimeSeriesChart
---
+++ applications/launchpad/gui-react/src/containers/Dashboard/ExpertView/Performance/PerformanceChart.tsx
@@ -0,0 +1,106 @@
+import { useCallback, useEffect, useState, useMemo, CSSProperties } from 'react'
+
+import { Container } from '../../../../store/containers/types'
+import { StatsEntry } from '../../../../store/containers/statsRepository'
+import TimeSeriesChart from '../../../../components/Charts/TimeSeries'
+
+import usePerformanceStats from './usePerformanceStats'
+
+const PerformanceChart = ({
+  enabled,
+  extractor,
+  percentageValues,
+  title,
+  unit,
+  style,
+  from,
+  to,
+  onUserInteraction,
+  chartHeight,
+}: {
+  enabled: boolean
+  extractor: (entry: StatsEntry) => { timestamp: string; value: number }
+  percentageValues?: boolean
+  title: string
+  unit?: string
+  style: CSSProperties
+  from: Date
+  to: Date
+  chartHeight: number
+  onUserInteraction: (options: { interacting: boolean }) => void
+}) => {
+  const [latchedFrom, setLatchedFrom] = useState(() => from)
+  useEffect(() => {
+    if (enabled) {
+      setLatchedFrom(from)
+    }
+  }, [enabled, from])
+  const [latchedTo, setLatchedTo] = useState(() => to)
+  useEffect(() => {
+    if (enabled) {
+      setLatchedTo(to)
+    }
+  }, [enabled, to])
+
+  const performanceData = usePerformanceStats({
+    extractor,
+    enabled,
+    from: latchedFrom,
+    to: latchedTo,
+  })
+
+  const [hiddenSeries, setHiddenSeries] = useState<Container[]>([])
+  const data = useMemo<
+    {
+      visible: boolean
+      empty: boolean
+      name: string
+      data: { x: number; y: number }[]
+    }[]
+  >(
+    () =>
+      Object.entries(performanceData).map(([container, containerData]) => {
+        const data = containerData.map(({ timestamp, value }) => ({
+          x: new Date(timestamp).getTime(),
+          y: value,
+        }))
+        const visible = !hiddenSeries.includes(container as Container)
+        return {
+          name: container,
+          empty: !data.length,
+          visible,
+          data: visible ? data : [],
+        }
+      }),
+    [performanceData, hiddenSeries],
+  )
+
+  const toggleSeries = useCallback(
+    (seriesName: string) =>
+      setHiddenSeries(hidden => {
+        if (hidden.includes(seriesName as Container)) {
+          return hidden.filter(h => h !== (seriesName as Container))
+        }
+
+        return [...hidden, seriesName as Container]
+      }),
+    [performanceData],
+  )
+
+  return (
+    <TimeSeriesChart
+      data={data}
+      percentageValues={percentageValues}
+      toggleSeries={toggleSeries}
+      unit={unit}
+      from={latchedFrom}
+      to={latchedTo}
+      title={title}
+      onUserInteraction={onUserInteraction}
+      style={style}
+      chartHeight={chartHeight}
+    />
+  )
+}
+
+export default PerformanceChart
