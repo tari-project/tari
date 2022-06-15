c082fe305201d6ec32876a8053a614281049fefe
Cormac Quaid <69508715+corquaid@users.noreply.github.com>
2022-06-15 18:18:32 +0200

    Rename TitleBar prop for show/hide Settings button

--- applications/launchpad/gui-react/src/components/TitleBar/types.ts
+++ applications/launchpad/gui-react/src/components/TitleBar/types.ts
@@ -1,4 +1,4 @@
 export interface TitleBarProps {
   drawerViewWidth?: string
-  mode?: 'help' | 'onboarding'
+  hideSettingsButton?: boolean
 }
--- applications/launchpad/gui-react/src/pages/onboarding/index.tsx
+++ applications/launchpad/gui-react/src/pages/onboarding/index.tsx
@@ -37,7 +37,7 @@ const Onboarding = () => {
 
   return (
     <div>
-      <TitleBar mode='onboarding' />
+      <TitleBar hideSettingsButton />
       <div
         style={{
           display: 'flex',
