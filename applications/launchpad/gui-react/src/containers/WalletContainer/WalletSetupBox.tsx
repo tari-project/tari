f3295c0d7535a9d00b6fbc7188720aaed59d1a50
tarnas <tarnas@altalogy.com>
2022-06-21 14:55:03 +0200

    using simple NodeBox for WalletSetupBox

--- applications/launchpad/gui-react/src/containers/WalletContainer/WalletSetupBox.tsx
+++ applications/launchpad/gui-react/src/containers/WalletContainer/WalletSetupBox.tsx
@@ -1,42 +1,26 @@
 import { useTheme } from 'styled-components'
 
-import { useAppSelector } from '../../store/hooks'
-import SvgTariSignet from '../../styles/Icons/TariSignet'
 import WalletPasswordWizard from '../../containers/WalletPasswordWizard'
-import MiningBox from '../../containers/MiningContainer/MiningBox'
-import { MiningBoxStatus } from '../../containers/MiningContainer/MiningBox/types'
+import NodeBox from '../../components/NodeBox'
 import t from '../../locales'
-import {
-  selectTariContainers,
-  selectTariMiningState,
-} from '../../store/mining/selectors'
 
 const WalletSetupBox = () => {
   const theme = useTheme()
 
-  const nodeState = useAppSelector(selectTariMiningState)
-  const containersState = useAppSelector(selectTariContainers)
-
   return (
-    <MiningBox
-      node='tari'
-      icons={[{ coin: 'xtr', component: <SvgTariSignet key='tari-icon' /> }]}
-      testId='tari-mining-box'
-      statuses={{
-        [MiningBoxStatus.SetupRequired]: {
-          title: t.wallet.setUpTariWalletTitle,
-          boxStyle: {
-            boxShadow: theme.shadow40,
-            borderColor: 'transparent',
-          },
-        },
+    <NodeBox
+      title={t.wallet.setUpTariWalletTitle}
+      tag={{
+        content: t.common.phrases.startHere,
       }}
-      currentStatus={MiningBoxStatus.SetupRequired}
-      nodeState={nodeState}
-      containersState={containersState}
+      style={{
+        position: 'relative',
+        boxShadow: theme.shadow40,
+        borderColor: 'transparent',
+      }}
     >
       <WalletPasswordWizard submitBtnText={t.wallet.setUpTariWalletSubmitBtn} />
-    </MiningBox>
+    </NodeBox>
   )
 }
 
