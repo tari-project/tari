a6e7639c423509a1015dc8069148ee772addbeec
tarnas <tarnas@altalogy.com>
2022-06-23 08:36:00 +0200

    removed unnecessary fragment

--- applications/launchpad/gui-react/src/containers/BaseNodeContainer/BaseNode.tsx
+++ applications/launchpad/gui-react/src/containers/BaseNodeContainer/BaseNode.tsx
@@ -21,71 +21,69 @@ const BaseNode = ({
   const theme = useTheme()
 
   return (
-    <>
-      <Box
-        border={!running}
-        gradient={
-          running
-            ? { start: theme.actionBackground, end: theme.accent }
-            : undefined
-        }
+    <Box
+      border={!running}
+      gradient={
+        running
+          ? { start: theme.actionBackground, end: theme.accent }
+          : undefined
+      }
+    >
+      <Text
+        type='header'
+        style={{
+          margin: 0,
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+        color={running ? theme.inverted.primary : undefined}
       >
-        <Text
-          type='header'
-          style={{
-            margin: 0,
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}
-          color={running ? theme.inverted.primary : undefined}
-        >
-          {t.baseNode.title}
-          {running && (
-            <Tag type='running' variant='large'>
-              {t.common.adjectives.running}
-            </Tag>
-          )}
-        </Text>
-        <Box
-          border={false}
-          style={{
-            minWidth: 0,
-            width: 'auto',
-            padding: 0,
-            background: running ? 'transparent' : undefined,
-          }}
-        >
-          <Select
-            inverted={running}
-            disabled={running}
-            value={networkOptions.find(({ value }) => value === tariNetwork)}
-            options={networkOptions}
-            onChange={({ value }) => setTariNetwork(value as Network)}
-            label={t.baseNode.tari_network_label}
-          />
-        </Box>
-        {!running && (
-          <Button disabled={pending} onClick={startNode} loading={pending}>
-            {t.baseNode.start}
-          </Button>
-        )}
+        {t.baseNode.title}
         {running && (
-          <Button
-            onClick={stopNode}
-            disabled={pending}
-            loading={pending}
-            style={{
-              color: theme.inverted.primary,
-              background: theme.resetBackground,
-              border: 'none',
-            }}
-          >
-            {t.common.verbs.stop}
-          </Button>
+          <Tag type='running' variant='large'>
+            {t.common.adjectives.running}
+          </Tag>
         )}
+      </Text>
+      <Box
+        border={false}
+        style={{
+          minWidth: 0,
+          width: 'auto',
+          padding: 0,
+          background: running ? 'transparent' : undefined,
+        }}
+      >
+        <Select
+          inverted={running}
+          disabled={running}
+          value={networkOptions.find(({ value }) => value === tariNetwork)}
+          options={networkOptions}
+          onChange={({ value }) => setTariNetwork(value as Network)}
+          label={t.baseNode.tari_network_label}
+        />
       </Box>
-    </>
+      {!running && (
+        <Button disabled={pending} onClick={startNode} loading={pending}>
+          {t.baseNode.start}
+        </Button>
+      )}
+      {running && (
+        <Button
+          onClick={stopNode}
+          disabled={pending}
+          loading={pending}
+          style={{
+            color: theme.inverted.primary,
+            background: theme.resetBackground,
+            border: 'none',
+          }}
+        >
+          {t.common.verbs.stop}
+        </Button>
+      )}
+    </Box>
   )
 }
 
