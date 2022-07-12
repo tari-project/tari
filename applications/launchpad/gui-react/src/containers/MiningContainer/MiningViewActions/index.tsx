9537c7836c1d0d41e55398f34f7a201ac5564adb
Cormac Quaid <69508715+corquaid@users.noreply.github.com>
2022-07-12 20:21:33 +0200

    Removing code used for development testing

--- applications/launchpad/gui-react/src/containers/MiningContainer/MiningViewActions/index.tsx
+++ applications/launchpad/gui-react/src/containers/MiningContainer/MiningViewActions/index.tsx
@@ -31,8 +31,7 @@ const MiningViewActions = ({
         variant='text'
         leftIcon={<SvgClock width='1.5rem' height='1.5rem' />}
         testId='mining-action-setup-mining-hours'
-        // @TODO: ADD BANG OPERATOR IN DISABLED PROP!!
-        disabled={canAnyMiningBeRun}
+        disabled={!canAnyMiningBeRun}
         onClick={openScheduling}
         style={{ paddingLeft: 0, color: theme.helpTipText }}
       >
