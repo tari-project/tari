ee2d9173c94962670c3c26e54579c40b427e35ed
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2022-07-28 11:09:17 +0100

    refactor abi function type mapping

--- dan_layer/engine/tests/macros/src/ast.rs
+++ dan_layer/engine/tests/macros/src/ast.rs
@@ -1,109 +1,89 @@
 use syn::{
     parse::{Parse, ParseStream},
-    parse_quote,
-    Expr,
+    punctuated::Punctuated,
+    token::Comma,
+    FnArg,
+    Ident,
+    ImplItem,
     ItemImpl,
     ItemStruct,
     Result,
+    ReturnType,
 };
-use tari_template_abi::{FunctionDef, Type};
 
 #[allow(dead_code)]
 pub struct TemplateAst {
+    pub template_name: Ident,
     pub struct_section: ItemStruct,
     pub impl_section: ItemImpl,
 }
 
 impl Parse for TemplateAst {
     fn parse(input: ParseStream) -> Result<Self> {
+        let struct_section: ItemStruct = input.parse()?;
+        let impl_section = input.parse()?;
+        let template_name = struct_section.ident.clone();
+
         Ok(Self {
-            struct_section: input.parse()?,
-            impl_section: input.parse()?,
+            template_name,
+            struct_section,
+            impl_section,
         })
     }
 }
 
 impl TemplateAst {
-    pub fn get_function_definitions(&self) -> Vec<FunctionDef> {
+    pub fn get_functions(&self) -> Vec<FunctionAst> {
         self.impl_section
             .items
             .iter()
-            .map(|item| match item {
-                syn::ImplItem::Method(m) => FunctionDef {
-                    name: m.sig.ident.to_string(),
-                    arguments: Self::map_abi_input_types(&m.sig.inputs),
-                    output: Self::map_abi_return_type(&m.sig.output),
-                },
-                _ => todo!(),
-            })
+            .map(Self::get_function_from_item)
             .collect()
     }
 
-    fn map_abi_input_types(inputs: &syn::punctuated::Punctuated<syn::FnArg, syn::token::Comma>) -> Vec<Type> {
-        inputs
-            .iter()
-            .map(|input| {
-                match input {
-                    // TODO: handle the "self" case
-                    syn::FnArg::Receiver(_) => todo!(),
-                    syn::FnArg::Typed(t) => Self::map_to_abi_type(&t.ty),
-                }
-            })
-            .collect()
-    }
-
-    fn map_abi_return_type(ast_return_type: &syn::ReturnType) -> Type {
-        match ast_return_type {
-            syn::ReturnType::Default => Type::Unit,
-            syn::ReturnType::Type(_, t) => Self::map_to_abi_type(t),
-        }
-    }
-
-    fn map_to_abi_type(ast_type: &syn::Type) -> Type {
-        match ast_type {
-            syn::Type::Path(type_path) => {
-                // TODO: handle "Self"
-                // TODO: detect more complex types
-                let ident = type_path.path.segments[0].ident.to_string();
-                // TODO: refactor to avoid these hardcoded string values
-                match ident.as_str() {
-                    "()" => Type::Unit,
-                    "bool" => Type::Bool,
-                    "i8" => Type::I8,
-                    "i16" => Type::I16,
-                    "i32" => Type::I32,
-                    "i64" => Type::I64,
-                    "i128" => Type::I128,
-                    "u8" => Type::U8,
-                    "u16" => Type::U16,
-                    "u32" => Type::U32,
-                    "u64" => Type::U64,
-                    "u128" => Type::U128,
-                    "String" => Type::String,
-                    _ => todo!(),
-                }
+    fn get_function_from_item(item: &ImplItem) -> FunctionAst {
+        match item {
+            ImplItem::Method(m) => FunctionAst {
+                name: m.sig.ident.to_string(),
+                input_types: Self::get_input_type_tokens(&m.sig.inputs),
+                output_type: Self::get_output_type_token(&m.sig.output),
             },
             _ => todo!(),
         }
     }
 
-    // TODO: this function probably should not be here
-    pub fn get_abi_type_expr(abi_type: &Type) -> Expr {
-        // TODO: there must be a better way of doing this...
-        match *abi_type {
-            Type::Unit => parse_quote!(Type::Unit),
-            Type::Bool => parse_quote!(Type::Bool),
-            Type::I8 => parse_quote!(Type::I8),
-            Type::I16 => parse_quote!(Type::I16),
-            Type::I32 => parse_quote!(Type::I32),
-            Type::I64 => parse_quote!(Type::I64),
-            Type::I128 => parse_quote!(Type::I128),
-            Type::U8 => parse_quote!(Type::U8),
-            Type::U16 => parse_quote!(Type::U16),
-            Type::U32 => parse_quote!(Type::U32),
-            Type::U64 => parse_quote!(Type::U64),
-            Type::U128 => parse_quote!(Type::U128),
-            Type::String => parse_quote!(Type::String),
+    fn get_input_type_tokens(inputs: &Punctuated<FnArg, Comma>) -> Vec<String> {
+        inputs
+            .iter()
+            .map(|arg| match arg {
+                // TODO: handle the "self" case
+                syn::FnArg::Receiver(_) => todo!(),
+                syn::FnArg::Typed(t) => Self::get_type_token(&t.ty),
+            })
+            .collect()
+    }
+
+    fn get_output_type_token(ast_type: &ReturnType) -> String {
+        match ast_type {
+            syn::ReturnType::Default => String::new(), // the function does not return anything
+            syn::ReturnType::Type(_, t) => Self::get_type_token(t),
         }
     }
+
+    fn get_type_token(syn_type: &syn::Type) -> String {
+        match syn_type {
+            syn::Type::Path(type_path) => {
+                // TODO: handle "Self"
+                // TODO: detect more complex types
+                type_path.path.segments[0].ident.to_string()
+            },
+            _ => todo!(),
+        }
+    }
+}
+
+pub struct FunctionAst {
+    pub name: String,
+    pub input_types: Vec<String>,
+    pub output_type: String,
 }
--- dan_layer/engine/tests/macros/src/output/abi.rs
+++ dan_layer/engine/tests/macros/src/output/abi.rs
@@ -1,25 +1,22 @@
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
 use syn::{parse_quote, Expr, Result};
-use tari_template_abi::FunctionDef;
 
-use crate::ast::TemplateAst;
+use crate::ast::{FunctionAst, TemplateAst};
 
 pub fn generate_abi(ast: &TemplateAst) -> Result<TokenStream> {
-    let template_name_str = format!("{}", ast.struct_section.ident);
-    let function_name = format_ident!("{}_abi", ast.struct_section.ident);
-
-    let function_defs = ast.get_function_definitions();
-    let function_defs_output: Vec<Expr> = function_defs.iter().map(generate_function_def).collect();
+    let abi_function_name = format_ident!("{}_abi", ast.struct_section.ident);
+    let template_name_as_str = ast.template_name.to_string();
+    let function_defs: Vec<Expr> = ast.get_functions().iter().map(generate_function_def).collect();
 
     let output = quote! {
         #[no_mangle]
-        pub extern "C" fn #function_name() -> *mut u8 {
+        pub extern "C" fn #abi_function_name() -> *mut u8 {
             use ::tari_template_abi::{encode_with_len, FunctionDef, TemplateDef, Type};
 
             let template = TemplateDef {
-                template_name: #template_name_str.to_string(),
-                functions: vec![ #(#function_defs_output),* ],
+                template_name: #template_name_as_str.to_string(),
+                functions: vec![ #(#function_defs),* ],
             };
 
             let buf = encode_with_len(&template);
@@ -30,10 +27,15 @@ pub fn generate_abi(ast: &TemplateAst) -> Result<TokenStream> {
     Ok(output)
 }
 
-fn generate_function_def(fd: &FunctionDef) -> Expr {
-    let name = fd.name.clone();
-    let arguments: Vec<Expr> = fd.arguments.iter().map(TemplateAst::get_abi_type_expr).collect();
-    let output = TemplateAst::get_abi_type_expr(&fd.output);
+fn generate_function_def(f: &FunctionAst) -> Expr {
+    let name = f.name.clone();
+    let arguments: Vec<Expr> = f
+        .input_types
+        .iter()
+        .map(String::as_str)
+        .map(generate_abi_type)
+        .collect();
+    let output = generate_abi_type(&f.output_type);
 
     parse_quote!(
         FunctionDef {
@@ -42,4 +44,24 @@ fn generate_function_def(fd: &FunctionDef) -> Expr {
             output: #output,
         }
     )
+}
+
+fn generate_abi_type(rust_type: &str) -> Expr {
+    // TODO: there may be a better way of handling this
+    match rust_type {
+        "" => parse_quote!(Type::Unit),
+        "bool" => parse_quote!(Type::Bool),
+        "i8" => parse_quote!(Type::I8),
+        "i16" => parse_quote!(Type::I16),
+        "i32" => parse_quote!(Type::I32),
+        "i64" => parse_quote!(Type::I64),
+        "i128" => parse_quote!(Type::I128),
+        "u8" => parse_quote!(Type::U8),
+        "u16" => parse_quote!(Type::U16),
+        "u32" => parse_quote!(Type::U32),
+        "u64" => parse_quote!(Type::U64),
+        "u128" => parse_quote!(Type::U128),
+        "String" => parse_quote!(Type::String),
+        _ => todo!(),
+    }
 }
