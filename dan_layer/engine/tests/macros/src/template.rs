b9191a9a50556ad6c7a05eecf27ab41b3a4da610
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2022-07-28 08:58:11 +0100

    remove references to the "common" project

--- dan_layer/engine/tests/macros/src/template.rs
+++ dan_layer/engine/tests/macros/src/template.rs
@@ -33,7 +33,7 @@ pub fn generate_template_output(input: TokenStream) -> Result<TokenStream> {
     let mod_output = generate_mod_output(&ast);
     let abi_output = generate_abi_output(&ast)?;
     let main_output = generate_main_output(&ast)?;
-    let engine_output = generate_engine_output();
+    let dependencies_output = generate_dependencies_output();
 
     let output = quote! {
         #mod_output
@@ -42,7 +42,7 @@ pub fn generate_template_output(input: TokenStream) -> Result<TokenStream> {
 
         #main_output
 
-        #engine_output
+        #dependencies_output
     };
 
     Ok(output)
@@ -77,7 +77,6 @@ fn generate_abi_output(ast: &TemplateAst) -> Result<TokenStream> {
     let output = quote! {
         #[no_mangle]
         pub extern "C" fn #function_name() -> *mut u8 {
-            use ::common::wrap_ptr;
             use ::tari_template_abi::{encode_with_len, FunctionDef, TemplateDef, Type};
 
             let template = TemplateDef {
@@ -95,11 +94,7 @@ fn generate_abi_output(ast: &TemplateAst) -> Result<TokenStream> {
 
 fn generate_function_def_output(fd: &FunctionDef) -> Expr {
     let name = fd.name.clone();
-    let arguments: Vec<Expr> = fd
-        .arguments
-        .iter()
-        .map(TemplateAst::get_abi_type_expr)
-        .collect();
+    let arguments: Vec<Expr> = fd.arguments.iter().map(TemplateAst::get_abi_type_expr).collect();
     let output = TemplateAst::get_abi_type_expr(&fd.output);
 
     parse_quote!(
@@ -117,7 +112,6 @@ fn generate_main_output(ast: &TemplateAst) -> Result<TokenStream> {
     let output = quote! {
         #[no_mangle]
         pub extern "C" fn #function_name(call_info: *mut u8, call_info_len: usize) -> *mut u8 {
-            use ::common::wrap_ptr;
             use ::tari_template_abi::{decode, encode_with_len, CallInfo};
 
             if call_info.is_null() {
@@ -139,10 +133,41 @@ fn generate_main_output(ast: &TemplateAst) -> Result<TokenStream> {
     Ok(output)
 }
 
-fn generate_engine_output() -> TokenStream {
+fn generate_dependencies_output() -> TokenStream {
     quote! {
         extern "C" {
             pub fn tari_engine(op: u32, input_ptr: *const u8, input_len: usize) -> *mut u8;
+        }
+
+        pub fn wrap_ptr(mut v: Vec<u8>) -> *mut u8 {
+            use std::mem;
+
+            let ptr = v.as_mut_ptr();
+            mem::forget(v);
+            ptr
+        }
+
+        #[no_mangle]
+        pub unsafe extern "C" fn tari_alloc(len: u32) -> *mut u8 {
+            use std::{mem, intrinsics::copy};
+
+            let cap = (len + 4) as usize;
+            let mut buf = Vec::<u8>::with_capacity(cap);
+            let ptr = buf.as_mut_ptr();
+            mem::forget(buf);
+            copy(len.to_le_bytes().as_ptr(), ptr, 4);
+            ptr
+        }
+
+        #[no_mangle]
+        pub unsafe extern "C" fn tari_free(ptr: *mut u8) {
+            use std::intrinsics::copy;
+
+            let mut len = [0u8; 4];
+            copy(ptr, len.as_mut_ptr(), 4);
+
+            let cap = (u32::from_le_bytes(len) + 4) as usize;
+            let _ = Vec::<u8>::from_raw_parts(ptr, cap, cap);
         }
     }
 }
