087631690631c7f681f5c819922d04be16324a7d
jorgeantonio21 <jtiago1993@gmail.com>
2022-08-08 10:12:14 +0100

    add contacts table

--- base_layer/wallet/migrations/2022-08-08_initial/up.sql
+++ base_layer/wallet/migrations/2022-08-08_initial/up.sql
@@ -4,15 +4,33 @@ CREATE TABLE client_key_values (
 )
 
 CREATE TABLE completed_transactions (
-    tx_id                  BIGINT PRIMARY KEY NOT NULL,
-    source_public_key      BLOB               NOT NULL,
-    destination_public_key BLOB               NOT NULL,
-    amount                 BIGINT             NOT NULL,
-    fee                    BIGINT             NOT NULL, 
-    transaction_protocol   TEXT               NOT NULL,
-    status                 INTEGER            NOT NULL,
-    message                TEXT               NOT NULL,
-    timestamp              DATETIME           NOT NULL,
-    cancelled 
-
+    tx_id                       BIGINT PRIMARY KEY NOT NULL,
+    source_public_key           BLOB               NOT NULL,
+    destination_public_key      BLOB               NOT NULL,
+    amount                      BIGINT             NOT NULL,
+    fee                         BIGINT             NOT NULL, 
+    transaction_protocol        TEXT               NOT NULL,
+    status                      INTEGER            NOT NULL,
+    message                     TEXT               NOT NULL,
+    timestamp                   DATETIME           NOT NULL,
+    cancelled                   INTEGER,
+    direction                   INTEGER,
+    coinbase_block_height       BIGINT,
+    send_count                  INTEGER default 0  NOT NULL,
+    last_send_timestamp         DATETIME,
+    confirmations               BIGINT,
+    mined_height                BIGINT,
+    mined_in_block              BLOB,
+    mined_timestamp             DATETIME,
+    transaction_signature_nonce BLOB    default 0  NOT NULL,
+    transaction_signature_key   BLOB    default 0  NOT NULL,
 )
+
+CREATE TABLE contacts (
+    public_key  BLOB PRIMARY KEY NOT NULL,
+    node_id     BLOB             NOT NULL,
+    alias       TEXT             NOT NULL,
+    last_seen   DATETIME,
+    latency     INTEGER,
+)
+
