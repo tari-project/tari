3490b47680f4db1f7707e4a020fb8ea32e4fff80
jorgeantonio21 <jtiago1993@gmail.com>
2022-08-08 09:52:29 +0100

    add state

--- base_layer/wallet/migrations/2022-08-08_initial/up.sql
+++ base_layer/wallet/migrations/2022-08-08_initial/up.sql
@@ -2,3 +2,16 @@ CREATE TABLE client_key_values (
     key   TEXT PRIMARY KEY NOT NULL,
     value TEXT             NOT NULL,
 )
+
+CREATE TABLE completed_transactions (
+    tx_id                  BIGINT PRIMARY KEY NOT NULL,
+    source_public_key      BLOB               NOT NULL,
+    destination_public_key BLOB               NOT NULL,
+    amount                 BIGINT             NOT NULL,
+    fee                    BIGINT             NOT NULL, 
+    transaction_protocol   TEXT               NOT NULL,
+    status                 INTEGER            NOT NULL,
+    message                TEXT               NOT NULL,
+    timestamp              DATETIME           NOT NULL,
+    
+)
