a98a4619d5d658a1881585a3f1010965a81af14f
jorgeantonio21 <jtiago1993@gmail.com>
2022-08-12 12:51:07 +0100

    run cargo fmt

--- base_layer/wallet/src/transaction_service/storage/sqlite_db.rs
+++ base_layer/wallet/src/transaction_service/storage/sqlite_db.rs
@@ -2213,9 +2213,7 @@ mod test {
             SenderTransactionProtocol,
         },
     };
-    use tari_crypto::{
-        keys::{PublicKey as PublicKeyTrait, SecretKey as SecretKeyTrait},
-    };
+    use tari_crypto::keys::{PublicKey as PublicKeyTrait, SecretKey as SecretKeyTrait};
     use tari_script::{script, ExecutionStack, TariScript};
     use tari_test_utils::random::string;
     use tempfile::tempdir;
