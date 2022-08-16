2e66f2453ee81e2deff723471c3f75bc5f84b47a
jorgeantonio21 <jtiago1993@gmail.com>
2022-08-16 19:02:15 +0100

    refactor feature tests

--- integration_tests/helpers/transactionOutputHashing.js
+++ integration_tests/helpers/transactionOutputHashing.js
@@ -3,7 +3,9 @@
 
 const { Blake256 } = require("./hashing");
 const {
+  toLittleEndian,
   encodeOption,
+  toLengthEncoded,
   assertBufferType,
 } = require("./util");
 
@@ -35,6 +37,11 @@ const getTransactionOutputHash = function (output) {
   // TODO: Keep this number in sync with 'get_current_version()' in 'transaction_output_version.rs'
   const OUTPUT_FEATURES_VERSION = 0x00;
 
+  let hasher = new Blake256();
+  assertBufferType(output.commitment, 32);
+  assertBufferType(output.script);
+  assertBufferType(output.covenant);
+  assertBufferType(output.encrypted_value, 24);
   const hash = hasher
     // version
     .chain(Buffer.from([OUTPUT_FEATURES_VERSION]))
