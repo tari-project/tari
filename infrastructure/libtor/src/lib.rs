3d73ae0946115b75254e136146ff45f58d20663a
Stan Bondi <sdbondi@users.noreply.github.com>
2022-09-07 14:33:42 +0400

    fix: exclude libtor from windows build

--- infrastructure/libtor/src/lib.rs
+++ infrastructure/libtor/src/lib.rs
@@ -20,5 +20,9 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#[cfg(unix)]
+// For some inexplicable reason if we don't include extern crate then we get libtor not defined errors in this crate on
+// matrix builds
+#[allow(unused_extern_crates)]
+extern crate libtor;
+
 pub mod tor;
