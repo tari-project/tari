a1831556d4941532620c3a963cd36a10f602bdf3
SW van Heerden <swvheerden@gmail.com>
2022-10-04 10:35:58 +0200

    point to tari repos

--- applications/tari_app_grpc/src/authentication/basic_auth.rs
+++ applications/tari_app_grpc/src/authentication/basic_auth.rs
@@ -20,8 +20,7 @@
 //  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 //  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-use std::{borrow::Cow, string::FromUtf8Error};
-use std::ops::Deref;
+use std::{borrow::Cow, ops::Deref, string::FromUtf8Error};
 
 use argon2::{password_hash::Encoding, Argon2, PasswordHash, PasswordVerifier};
 use tari_utilities::SafePassword;
