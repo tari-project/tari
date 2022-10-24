ef17979fb9c15af35a3cab86c8b2bc1818afc5f7
jorgeantonio21 <jtiago1993@gmail.com>
2022-10-24 11:05:51 +0100

    cargo clippy

--- applications/tari_app_grpc/src/conversions/consensus_constants.rs
+++ applications/tari_app_grpc/src/conversions/consensus_constants.rs
@@ -33,25 +33,25 @@ impl From<ConsensusConstants> for grpc::ConsensusConstants {
         let input_version_range = cc.input_version_range().clone().into_inner();
         let input_version_range = if input_version_range.0 != input_version_range.1 {
             vec![
-                input_version_range.0.as_u8() as i32,
-                input_version_range.1.as_u8() as i32,
+                i32::from(input_version_range.0.as_u8()),
+                i32::from(input_version_range.1.as_u8()),
             ]
         } else {
-            vec![input_version_range.0.as_u8() as i32]
+            vec![i32::from(input_version_range.0.as_u8())]
         };
         let kernel_version_range = cc.kernel_version_range().clone().into_inner();
         let kernel_version_range = if kernel_version_range.0 != kernel_version_range.1 {
             vec![
-                kernel_version_range.0.as_u8() as i32,
-                kernel_version_range.1.as_u8() as i32,
+                i32::from(kernel_version_range.0.as_u8()),
+                i32::from(kernel_version_range.1.as_u8()),
             ]
         } else {
             vec![kernel_version_range.0.as_u8() as i32]
         };
         let valid_blockchain_version_range = cc.valid_blockchain_version_range().clone().into_inner();
         let valid_blockchain_version_range = grpc::Range {
-            min: valid_blockchain_version_range.0 as u64,
-            max: valid_blockchain_version_range.1 as u64,
+            min: u64::from(valid_blockchain_version_range.0),
+            max: u64::from(valid_blockchain_version_range.1),
         };
         let transaction_weight = cc.transaction_weight();
         let metadata_bytes_per_gram = if let Some(val) = transaction_weight.params().metadata_bytes_per_gram {
@@ -67,12 +67,12 @@ impl From<ConsensusConstants> for grpc::ConsensusConstants {
         };
         let output_version_range = cc.output_version_range();
         let outputs = grpc::Range {
-            min: output_version_range.outputs.start().as_u8() as u64,
-            max: output_version_range.outputs.end().as_u8() as u64,
+            min: u64::from(output_version_range.outputs.start().as_u8()),
+            max: u64::from(output_version_range.outputs.end().as_u8()),
         };
         let features = grpc::Range {
-            min: output_version_range.features.start().as_u8() as u64,
-            max: output_version_range.features.end().as_u8() as u64,
+            min: u64::from(output_version_range.features.start().as_u8()),
+            max: u64::from(output_version_range.features.end().as_u8()),
         };
 
         let output_version_range = grpc::OutputsVersion {
@@ -83,7 +83,7 @@ impl From<ConsensusConstants> for grpc::ConsensusConstants {
         let permitted_output_types = cc.permitted_output_types();
         let permitted_output_types = permitted_output_types
             .iter()
-            .map(|ot| ot.as_byte() as i32)
+            .map(|ot| i32::from(ot.as_byte()))
             .collect::<Vec<i32>>();
 
         let monero_pow = PowAlgorithm::Monero;
