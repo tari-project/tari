d109371a6bf7e66649f39a7e93029dadab041bfc
CjS77 <CjS77@users.noreply.github.com>
2022-10-28 09:52:32 +0100

    fix: mining_test_difficulty

--- base_layer/tari_mining_helper_ffi/src/lib.rs
+++ base_layer/tari_mining_helper_ffi/src/lib.rs
@@ -370,8 +370,8 @@ mod tests {
 
     #[test]
     fn detect_change_in_consensus_encoding() {
-        const NONCE: u64 = 15151693527177504675;
-        const DIFFICULTY: Difficulty = Difficulty::from_u64(8707);
+        const NONCE: u64 = 7520709748303934033;
+        const DIFFICULTY: Difficulty = Difficulty::from_u64(2187);
         unsafe {
             let mut error = -1;
             let error_ptr = &mut error as *mut c_int;
