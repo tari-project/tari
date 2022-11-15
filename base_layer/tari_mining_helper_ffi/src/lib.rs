33ca28b7df24c125b7bf478bec26a7f740c88eb2
Aaron Feickert <66188213+AaronFeickert@users.noreply.github.com>
2022-11-14 19:36:05 -0600

    Fix failed consensus encoding test

--- base_layer/tari_mining_helper_ffi/src/lib.rs
+++ base_layer/tari_mining_helper_ffi/src/lib.rs
@@ -370,8 +370,8 @@ mod tests {
 
     #[test]
     fn detect_change_in_consensus_encoding() {
-        const NONCE: u64 = 18006431465547767508;
-        const DIFFICULTY: Difficulty = Difficulty::from_u64(13879);
+        const NONCE: u64 = 4238854535428725996;
+        const DIFFICULTY: Difficulty = Difficulty::from_u64(12185);
         unsafe {
             let mut error = -1;
             let error_ptr = &mut error as *mut c_int;
