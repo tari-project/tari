9ac514c68895bf9b899cc440870b1e8623b22c7c
Martin Stefcek <gcifko@gmail.com>
2023-01-13 10:39:58 +0100

    test: merge mining proxy

--- integration_tests/tests/cucumber.rs
+++ integration_tests/tests/cucumber.rs
@@ -39,6 +39,7 @@ use futures::StreamExt;
 use indexmap::IndexMap;
 use log::*;
 use rand::Rng;
+use serde_json::Value;
 use tari_app_grpc::tari_rpc::{self as grpc};
 use tari_app_utilities::utilities::UniPublicKey;
 use tari_base_node::BaseNodeConfig;
@@ -97,11 +98,11 @@ use tari_wallet_grpc_client::grpc::{
 };
 use thiserror::Error;
 use tokio::runtime::Runtime;
-use utils::wallet_ffi::create_seed_words;
 
 use crate::utils::{
     base_node_process::{spawn_base_node, spawn_base_node_with_config, BaseNodeProcess},
     get_peer_addresses,
+    merge_mining_proxy::{register_merge_mining_proxy_process, MergeMiningProxyProcess},
     miner::{
         mine_block,
         mine_block_before_submit,
@@ -111,7 +112,7 @@ use crate::utils::{
         MinerProcess,
     },
     transaction::{build_transaction_with_output, build_transaction_with_output_and_fee},
-    wallet_ffi::{create_contact, get_mnemonic_word_list_for_language, spawn_wallet_ffi, WalletFFI},
+    wallet_ffi::{create_contact, create_seed_words, get_mnemonic_word_list_for_language, spawn_wallet_ffi, WalletFFI},
     wallet_process::{create_wallet_client, get_default_cli, spawn_wallet, WalletProcess},
 };
 
@@ -131,6 +132,8 @@ pub enum TariWorldError {
     FFIWalletNotFound(String),
     #[error("Miner process not found: {0}")]
     MinerProcessNotFound(String),
+    #[error("Merge miner process not found: {0}")]
+    MergeMinerProcessNotFound(String),
     #[error("Base node error: {0}")]
     GrpcBaseNodeError(#[from] GrpcBaseNodeError),
     #[error("No base node, or wallet client found: {0}")]
@@ -144,6 +147,7 @@ pub struct TariWorld {
     miners: IndexMap<String, MinerProcess>,
     ffi_wallets: IndexMap<String, WalletFFI>,
     wallets: IndexMap<String, WalletProcess>,
+    merge_mining_proxies: IndexMap<String, MergeMiningProxyProcess>,
     transactions: IndexMap<String, Transaction>,
     wallet_addresses: IndexMap<String, String>, // values are strings representing tari addresses
     utxos: IndexMap<String, UnblindedOutput>,
@@ -156,6 +160,8 @@ pub struct TariWorld {
     errors: VecDeque<String>,
     // We need to store this in between steps when importing and checking the imports.
     last_imported_tx_ids: Vec<u64>,
+    // We need to store this for the merge mining proxy steps. The checks are get and check are done on separate steps.
+    last_merge_miner_response: Value,
 }
 
 enum NodeClient {
@@ -249,6 +255,20 @@ impl TariWorld {
             .miners
             .get(miner_name.as_ref())
             .ok_or_else(|| TariWorldError::MinerProcessNotFound(miner_name.as_ref().to_string()))?)
+    }
+
+    fn get_merge_miner<S: AsRef<str>>(&self, miner_name: S) -> anyhow::Result<&MergeMiningProxyProcess> {
+        Ok(self
+            .merge_mining_proxies
+            .get(miner_name.as_ref())
+            .ok_or_else(|| TariWorldError::MergeMinerProcessNotFound(miner_name.as_ref().to_string()))?)
+    }
+
+    fn get_mut_merge_miner<S: AsRef<str>>(&mut self, miner_name: S) -> anyhow::Result<&mut MergeMiningProxyProcess> {
+        Ok(self
+            .merge_mining_proxies
+            .get_mut(miner_name.as_ref())
+            .ok_or_else(|| TariWorldError::MergeMinerProcessNotFound(miner_name.as_ref().to_string()))?)
     }
 
     pub fn all_seed_nodes(&self) -> &[String] {
@@ -4747,6 +4767,195 @@ async fn ffi_fee_per_gram_stats(world: &mut TariWorld, wallet: String, min: u64,
         assert_eq!(fee_per_gram_stat.get_avg_fee_per_gram(), avg);
         assert_eq!(fee_per_gram_stat.get_max_fee_per_gram(), max);
     }
+}
+
+// Merge mining proxy steps
+
+#[when(expr = "I have a merge mining proxy {word} connected to {word} and {word} with origin submission {word}")]
+async fn merge_mining_proxy_with_submission(
+    world: &mut TariWorld,
+    mining_proxy_name: String,
+    base_node_name: String,
+    wallet_name: String,
+    enabled: String,
+) {
+    let enabled = match enabled.as_str() {
+        "enabled" => true,
+        "disabled" => false,
+        _ => panic!("This should be a boolean"),
+    };
+    register_merge_mining_proxy_process(world, mining_proxy_name, base_node_name, wallet_name, enabled).await;
+}
+
+#[when(expr = "I have a merge mining proxy {word} connected to {word} and {word} with default config")]
+async fn merge_mining_proxy_with_default_config(
+    world: &mut TariWorld,
+    mining_proxy_name: String,
+    base_node_name: String,
+    wallet_name: String,
+) {
+    register_merge_mining_proxy_process(world, mining_proxy_name, base_node_name, wallet_name, true).await;
+}
+
+#[when(expr = "I ask for a block height from proxy {word}")]
+async fn merge_mining_ask_for_block_height(world: &mut TariWorld, mining_proxy_name: String) {
+    let merge_miner = world.get_merge_miner(&mining_proxy_name).unwrap();
+    world.last_merge_miner_response = merge_miner.get_height().await;
+}
+
+#[then(expr = "Proxy response height is valid")]
+async fn merge_mining_response_height(world: &mut TariWorld) {
+    let height = world.last_merge_miner_response.get("height");
+    assert!(
+        height.is_some(),
+        "Response is invalid {}",
+        world.last_merge_miner_response
+    );
+    let height = height.unwrap();
+    assert!(height.as_u64().is_some(), "Height is invalid {}", height);
+}
+
+#[when(expr = "I ask for a block template from proxy {word}")]
+async fn merge_mining_ask_for_block_template(world: &mut TariWorld, mining_proxy_name: String) {
+    let merge_miner = world.get_mut_merge_miner(&mining_proxy_name).unwrap();
+    world.last_merge_miner_response = merge_miner.get_block_template().await;
+}
+
+#[then(expr = "Proxy response block template is valid")]
+async fn merge_mining_response_block_template_is_valid(world: &mut TariWorld) {
+    let result = world.last_merge_miner_response.get("result");
+    assert!(
+        result.is_some(),
+        "Response is invalid {}",
+        world.last_merge_miner_response
+    );
+    let result = result.unwrap();
+    assert!(result.get("_aux").is_some(), "Result has no `_aux` {}", result);
+    assert_eq!(
+        result.get("status").unwrap().as_str().unwrap(),
+        "OK",
+        "Result has no `status` {}",
+        result
+    );
+}
+
+#[when(expr = "I submit a block through proxy {word}")]
+async fn merge_mining_submit_block(world: &mut TariWorld, mining_proxy_name: String) {
+    let block_template_blob = world
+        .last_merge_miner_response
+        .get("result")
+        .unwrap()
+        .get("blocktemplate_blob");
+    assert!(
+        block_template_blob.is_some(),
+        "The last response doesn't have `blocktemplate_blob` {}",
+        world.last_merge_miner_response
+    );
+    let block_template_blob = block_template_blob.unwrap().clone();
+    let merge_miner = world.get_mut_merge_miner(&mining_proxy_name).unwrap();
+    println!("block_template {:?}", block_template_blob);
+    world.last_merge_miner_response = merge_miner.submit_block(&block_template_blob).await;
+    println!("last_merge_miner_response {:?}", world.last_merge_miner_response);
+    println!("last_merge_miner_response {:?}", world.last_merge_miner_response);
+    println!("last_merge_miner_response {:?}", world.last_merge_miner_response);
+}
+
+#[then(expr = "Proxy response block submission is valid {word} submitting to origin")]
+async fn merge_mining_submission_is_valid(world: &mut TariWorld, how: String) {
+    let result = world.last_merge_miner_response.get("result");
+    assert!(
+        result.is_some(),
+        "Response is invalid {}",
+        world.last_merge_miner_response
+    );
+    let result = result.unwrap();
+    if how == *"with" {
+        assert!(result.get("_aux").is_some(), "Result has no `_aux` {}", result);
+        let status = result.get("status");
+        assert!(status.is_some(), "Result has no status {}", result);
+    } else {
+        assert!(
+            world.last_merge_miner_response.get("status").is_some(),
+            "Response has no `status` {}",
+            world.last_merge_miner_response
+        );
+    }
+}
+
+#[when(expr = "I merge mine {int} blocks via {word}")]
+async fn merge_mining_mine(world: &mut TariWorld, count: u64, mining_proxy_name: String) {
+    let merge_miner = world.get_mut_merge_miner(&mining_proxy_name).unwrap();
+    for _ in 0..count {
+        merge_miner.mine().await;
+    }
+}
+
+#[when(expr = "I ask for the last block header from proxy {word}")]
+async fn merge_mining_ask_for_last_block_header(world: &mut TariWorld, mining_proxy_name: String) {
+    let merge_miner = world.get_mut_merge_miner(&mining_proxy_name).unwrap();
+    world.last_merge_miner_response = merge_miner.get_last_block_header().await;
+}
+
+#[then(expr = "Proxy response for block header by hash is valid")]
+async fn merge_mining_bloch_header_by_hash_is_valid(world: &mut TariWorld) {
+    let result = world.last_merge_miner_response.get("result");
+    assert!(
+        result.is_some(),
+        "Response is invalid {}",
+        world.last_merge_miner_response
+    );
+    let result = result.unwrap();
+    let status = result.get("status");
+    assert!(status.is_some(), "Result has no status {}", result);
+    assert_eq!(
+        result.get("status").unwrap().as_str().unwrap(),
+        "OK",
+        "Result has no `status` {}",
+        result
+    );
+}
+
+#[then(expr = "Proxy response for last block header is valid")]
+async fn merge_mining_response_last_block_header_is_valid(world: &mut TariWorld) {
+    let result = world.last_merge_miner_response.get("result");
+    assert!(
+        result.is_some(),
+        "Response is invalid {}",
+        world.last_merge_miner_response
+    );
+    let result = result.unwrap();
+    assert!(result.get("_aux").is_some(), "Result has no `_aux` {}", result);
+    let status = result.get("status");
+    assert!(status.is_some(), "Result has no status {}", result);
+    assert_eq!(
+        result.get("status").unwrap().as_str().unwrap(),
+        "OK",
+        "Result has no `status` {}",
+        result
+    );
+    let block_header = result.get("block_header");
+    assert!(block_header.is_some(), "Result has no `block_header` {}", result);
+    let block_header = block_header.unwrap();
+    assert!(
+        block_header.get("hash").is_some(),
+        "Block_header has no `hash` {}",
+        block_header
+    );
+}
+
+#[when(expr = "I ask for a block header by hash using last block header from proxy {word}")]
+async fn merge_mining_ask_for_block_header_by_hash(world: &mut TariWorld, mining_proxy_name: String) {
+    let hash = world
+        .last_merge_miner_response
+        .get("result")
+        .unwrap()
+        .get("block_header")
+        .unwrap()
+        .get("hash")
+        .unwrap()
+        .clone();
+    let merge_miner = world.get_mut_merge_miner(&mining_proxy_name).unwrap();
+    world.last_merge_miner_response = merge_miner.get_block_header_by_hash(hash).await;
 }
 
 fn flush_stdout(buffer: &Arc<Mutex<Vec<u8>>>) {
