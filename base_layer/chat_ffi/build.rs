211843ebdf4b00b936515ed580610db1d4b87fb9
brianp <brian.o.pearce@gmail.com>
2023-04-26 13:43:03 +0200

    Remove unneeded deps and clean up build file

--- base_layer/chat_ffi/build.rs
+++ base_layer/chat_ffi/build.rs
@@ -45,14 +45,3 @@ fn main() {
         .unwrap()
         .write_to_file(output_file);
 }
-
-// /// Find the location of the `target/` directory. Note that this may be
-// /// overridden by `cmake`, so we also need to check the `CARGO_TARGET_DIR`
-// /// variable.
-// fn target_dir() -> PathBuf {
-//     if let Ok(target) = env::var("CARGO_TARGET_DIR") {
-//         PathBuf::from(target)
-//     } else {
-//         PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("target")
-//     }
-// }
