07c88f5eb351ba8d7135c9eb46467f62693c7a84
Stan Bondi <sdbondi@users.noreply.github.com>
2023-06-22 10:48:23 +0200

    add details on usage of core hash domains

--- base_layer/core/src/transactions/mod.rs
+++ base_layer/core/src/transactions/mod.rs
@@ -29,12 +29,11 @@ pub mod key_manager;
 #[macro_use]
 pub mod test_helpers;
 
+// Hash domain for all transaction-related hashes, including the script signature challenge, transaction hash and kernel
+// signature challenge
 hash_domain!(TransactionHashDomain, "com.tari.base_layer.core.transactions", 0);
-hash_domain!(
-    TransactionFixedNonceKdfDomain,
-    "com.tari.base_layer.core.transactions.fixed_nonce_kdf",
-    0
-);
+
+// Hash domain used to derive the final AEAD encryption key for encrypted data in UTXOs
 hash_domain!(
     TransactionSecureNonceKdfDomain,
     "com.tari.base_layer.core.transactions.secure_nonce_kdf",
