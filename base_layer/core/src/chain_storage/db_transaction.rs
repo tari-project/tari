a13dbc0946ed415d09216ca438fef0100194a399
SW van Heerden <swvheerden@gmail.com>
2023-06-28 09:33:17 +0200

    fmt

--- base_layer/core/src/chain_storage/db_transaction.rs
+++ base_layer/core/src/chain_storage/db_transaction.rs
@@ -66,7 +66,8 @@ impl DbTransaction {
         DbTransaction::default()
     }
 
-    /// deletes the orphan, and if it was a tip will delete the orphan tip and make its prev header the new tip. This will not fail if the orphan does not exist.
+    /// deletes the orphan, and if it was a tip will delete the orphan tip and make its prev header the new tip. This
+    /// will not fail if the orphan does not exist.
     pub fn delete_orphan(&mut self, hash: HashOutput) -> &mut Self {
         self.operations.push(WriteOperation::DeleteOrphan(hash));
         self
