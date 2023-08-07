dc361b3dc94e6e464c6f4b79051e959578958951
Hansie Odendaal <pluto@tari.com>
2023-08-07 12:06:00 +0200

    Fix Windows Install
    Fixed Windows Install

--- scripts/file_license_check.sh
+++ scripts/file_license_check.sh
@@ -5,10 +5,20 @@ rg -i "Copyright.*The Tari Project" --files-without-match \
     -g '!*.{Dockerfile,asc,bat,config,config.js,css,csv,drawio,env,gitkeep,hbs,html,ini,iss,json,lock,md,min.js,ps1,py,rc,scss,sh,sql,svg,toml,txt,yml,vue}' . \
     | sort > /tmp/rgtemp
 
-# sort the .license.ignore file as sorting seems to behave differently on different platforms
+# Alternative - Exclude files without extensions as well as those with extensions that are not in the list
+#
+# rg -i "Copyright.*The Tari Project" --files-without-match \
+#     -g '!*.{Dockerfile,asc,bat,config,config.js,css,csv,drawio,env,gitkeep,hbs,html,ini,iss,json,lock,md,min.js,ps1,py,rc,scss,sh,sql,svg,toml,txt,yml,vue}' . \
+#     | while IFS= read -r file; do
+#         if [[ -n $(basename "$file" | grep -E '\.') ]]; then
+#             echo "$file"
+#         fi
+#     done | sort > /tmp/rgtemp
+
+# Sort the .license.ignore file as sorting seems to behave differently on different platforms
 cat .license.ignore | sort > /tmp/.license.ignore
 
-DIFFS=$(diff -u /tmp/.license.ignore /tmp/rgtemp)
+DIFFS=$(diff -y --strip-trailing-cr /tmp/.license.ignore /tmp/rgtemp)
 
 if [ -n "$DIFFS" ]; then
     echo "New files detected that either need copyright/license identifiers added, or they need to be added to .license.ignore"
