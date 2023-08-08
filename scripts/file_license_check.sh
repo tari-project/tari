96ad9b9593178edec8a30445cc6924b0762a1a2f
hansieodendaal <pluto@tari.com>
2023-08-08 17:03:31 +0200

    fix licence ignore

--- scripts/file_license_check.sh
+++ scripts/file_license_check.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 # Must be run from the repo root
 
+# rg -i "Copyright.*The Tari Project" --files-without-match \
+#    -g '!*.{Dockerfile,asc,bat,config,config.js,css,csv,drawio,env,gitkeep,hbs,html,ini,iss,json,lock,md,min.js,ps1,py,rc,scss,sh,sql,svg,toml,txt,yml,vue}' . \
+#    | sort > /tmp/rgtemp
+
+# Exclude files without extensions as well as those with extensions that are not in the list
+#
 rg -i "Copyright.*The Tari Project" --files-without-match \
     -g '!*.{Dockerfile,asc,bat,config,config.js,css,csv,drawio,env,gitkeep,hbs,html,ini,iss,json,lock,md,min.js,ps1,py,rc,scss,sh,sql,svg,toml,txt,yml,vue}' . \
-    | sort > /tmp/rgtemp
-
-# Alternative - Exclude files without extensions as well as those with extensions that are not in the list
-#
-# rg -i "Copyright.*The Tari Project" --files-without-match \
-#     -g '!*.{Dockerfile,asc,bat,config,config.js,css,csv,drawio,env,gitkeep,hbs,html,ini,iss,json,lock,md,min.js,ps1,py,rc,scss,sh,sql,svg,toml,txt,yml,vue}' . \
-#     | while IFS= read -r file; do
-#         if [[ -n $(basename "$file" | grep -E '\.') ]]; then
-#             echo "$file"
-#         fi
-#     done | sort > /tmp/rgtemp
+    | while IFS= read -r file; do
+        if [[ -n $(basename "$file" | grep -E '\.') ]]; then
+            echo "$file"
+        fi
+    done | sort > /tmp/rgtemp
 
 # Sort the .license.ignore file as sorting seems to behave differently on different platforms
 cat .license.ignore | sort > /tmp/.license.ignore
 
-DIFFS=$(diff -y --strip-trailing-cr /tmp/.license.ignore /tmp/rgtemp)
+DIFFS=$(diff -u --strip-trailing-cr /tmp/.license.ignore /tmp/rgtemp)
 
 if [ -n "$DIFFS" ]; then
     echo "New files detected that either need copyright/license identifiers added, or they need to be added to .license.ignore"
