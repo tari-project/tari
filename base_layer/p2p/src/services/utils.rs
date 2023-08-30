1ddaf3ab51e952508137b59bb73006c1d767eb16
stringhandler <stringhandler@gmail.com>
2023-08-30 20:37:51 +0200

    Update base_layer/p2p/src/services/utils.rs
    Co-authored-by: SW van Heerden <swvheerden@gmail.com>

--- base_layer/p2p/src/services/utils.rs
+++ base_layer/p2p/src/services/utils.rs
@@ -24,7 +24,6 @@ use std::sync::Arc;
 
 use crate::{comms_connector::PeerMessage, domain_message::DomainMessage};
 
-// const LOG_TARGET: &str = "p2p::services";
 
 pub fn map_decode<T>(serialized: Arc<PeerMessage>) -> DomainMessage<Result<T, prost::DecodeError>>
 where T: prost::Message + Default {
