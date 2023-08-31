d0ea406e57b8bbb65196c2e880671da2e51f2b62
Stan Bondi <sdbondi@users.noreply.github.com>
2023-08-31 13:07:43 +0400

    fix(comms)!: use noise XX handshake pattern for improved privacy (#5696)
    Description
    ---
    Replaces the noise IX handshake with the XX handshake
    Adds a prologue to the noise hash state
    Update snow from 0.9.1 to 0.9.3 (correctness issue fixed:
    https://github.com/mcginty/snow/releases/tag/v0.9.2)
    
    Motivation and Context
    ---
    
    http://www.noiseprotocol.org/noise.html
    
    The IX protocol provides no privacy for the initiator static key. We
    replace it with XX which (essentially) encrypts the initiator static key
    with an ephemeral Diffie-Hellman before sending the initiator static
    public key at the cost of an extra send/recv i.e 1.5 RTT for XX vs 1 RTT
    for IX. This provides forward secrecy for the initiator but weaker
    privacy for the responder (Encrypted with forward secrecy, but can be
    probed by an anonymous initiator.). This shouldn't be an issue as any
    initiator can perform a valid connection and get the peer information
    anyway,
    
    An alternative is XK (initiator key is transmitted, responder key is
    known) which does fit our requirements and has stronger privacy and
    similar security guarantees, however is a 2 RTT pattern
    
    A potential way to offset the increased RTT by -0.5 is to include
    identity information usually performed as a separate protocol directly
    after noise, as part of the noise handshake.
    
    How Has This Been Tested?
    ---
    Existing comms tests and all tests using comms
    
    What process can a PR reviewer use to test or verify this change?
    ---
    Create a localnet and check that nodes connect
    
    <!-- Checklist -->
    <!-- 1. Is the title of your PR in the form that would make nice release
    notes? The title, excluding the conventional commit
    tag, will be included exactly as is in the CHANGELOG, so please think
    about it carefully. -->
    
    
    Breaking Changes
    ---
    
    - [ ] None
    - [ ] Requires data directory on base node to be deleted
    - [ ] Requires hard fork
    - [x] Other - Cannot connect to peers without this change
    
    <!-- Does this include a breaking change? If so, include this line as a
    footer -->
    <!-- BREAKING CHANGE: Description what the user should do, e.g. delete a
    database, resync the chain -->

--- comms/core/src/noise/socket.rs
+++ comms/core/src/noise/socket.rs
@@ -228,9 +228,12 @@ where
         let mut read_buf = ReadBuf::new(&mut buf[*offset..]);
         let prev_rem = read_buf.remaining();
         ready!(socket.as_mut().poll_read(context, &mut read_buf))?;
-        let n = prev_rem
-            .checked_sub(read_buf.remaining())
-            .expect("buffer underflow: prev_rem < read_buf.remaining()");
+        let n = prev_rem.checked_sub(read_buf.remaining()).ok_or_else(|| {
+            io::Error::new(
+                io::ErrorKind::Other,
+                "buffer underflow: prev_rem < read_buf.remaining()",
+            )
+        })?;
         trace!(
             target: LOG_TARGET,
             "poll_read_exact: read {}/{} bytes",
@@ -404,7 +407,9 @@ where TSocket: AsyncWrite + Unpin
                             &mut self.buffers.write_encrypted,
                         ) {
                             Ok(encrypted_len) => {
-                                let frame_len = encrypted_len.try_into().expect("offset should be able to fit in u16");
+                                let frame_len = encrypted_len.try_into().map_err(|_| {
+                                    io::Error::new(io::ErrorKind::Other, "offset should be able to fit in u16")
+                                })?;
                                 self.write_state = WriteState::WriteFrameLen {
                                     frame_len,
                                     buf: u16::to_be_bytes(frame_len),
@@ -525,8 +530,8 @@ where TSocket: AsyncRead + AsyncWrite + Unpin
 {
     /// Perform a Single Round-Trip noise IX handshake returning the underlying [NoiseSocket]
     /// (switched to transport mode) upon success.
-    pub async fn handshake_1rt(mut self) -> io::Result<NoiseSocket<TSocket>> {
-        match self.perform_handshake().await {
+    pub async fn perform_handshake(mut self) -> io::Result<NoiseSocket<TSocket>> {
+        match self.handshake_1_5rtt().await {
             Ok(_) => self.build(),
             Err(err) => {
                 warn!(
@@ -539,21 +544,29 @@ where TSocket: AsyncRead + AsyncWrite + Unpin
         }
     }
 
-    pub async fn perform_handshake(&mut self) -> io::Result<()> {
+    /// Performs a 1.5 RTT handshake. For example, the noise XX handshake.
+    async fn handshake_1_5rtt(&mut self) -> io::Result<()> {
         if self.socket.state.is_initiator() {
-            // -> e, s
+            //   -> e
             self.send().await?;
             self.flush().await?;
 
             // <- e, ee, se, s, es
             self.receive().await?;
+
+            //   -> s, se
+            self.send().await?;
+            self.flush().await?;
         } else {
-            // -> e, s
+            //   -> e
             self.receive().await?;
 
             // <- e, ee, se, s, es
             self.send().await?;
             self.flush().await?;
+
+            //   -> s, se
+            self.receive().await?;
         }
 
         Ok(())
@@ -647,11 +660,11 @@ mod test {
     use snow::{params::NoiseParams, Builder, Error, Keypair};
 
     use super::*;
-    use crate::{memsocket::MemorySocket, noise::config::NOISE_IX_PARAMETER};
+    use crate::{memsocket::MemorySocket, noise::config::NOISE_PARAMETERS};
 
     async fn build_test_connection(
     ) -> Result<((Keypair, Handshake<MemorySocket>), (Keypair, Handshake<MemorySocket>)), Error> {
-        let parameters: NoiseParams = NOISE_IX_PARAMETER.parse().expect("Invalid protocol name");
+        let parameters: NoiseParams = NOISE_PARAMETERS.parse().expect("Invalid protocol name");
 
         let dialer_keypair = Builder::new(parameters.clone()).generate_keypair()?;
         let listener_keypair = Builder::new(parameters.clone()).generate_keypair()?;
@@ -679,7 +692,7 @@ mod test {
         dialer: Handshake<MemorySocket>,
         listener: Handshake<MemorySocket>,
     ) -> io::Result<(NoiseSocket<MemorySocket>, NoiseSocket<MemorySocket>)> {
-        let (dialer_result, listener_result) = join(dialer.handshake_1rt(), listener.handshake_1rt()).await;
+        let (dialer_result, listener_result) = join(dialer.perform_handshake(), listener.perform_handshake()).await;
 
         Ok((dialer_result?, listener_result?))
     }
