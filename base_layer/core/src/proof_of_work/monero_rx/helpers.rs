fa191c6ddc4fcaf90f9a38d2491488564c45b566
stringhandler <mikethetike@tari.com>
2023-09-04 14:11:47 +0200

    logic bug

--- base_layer/core/src/proof_of_work/monero_rx/helpers.rs
+++ base_layer/core/src/proof_of_work/monero_rx/helpers.rs
@@ -81,13 +81,15 @@ pub fn verify_header(header: &BlockHeader) -> Result<MoneroPowData, MergeMineErr
     // and that only 1 tari header is found
 
     let mut is_found = false;
+    let mut already_seen_mmfield = false;
     for item in extra_field.0 {
         if let SubField::MergeMining(Some(depth), merge_mining_hash) = item {
-            if is_found {
+            if already_seen_mmfield {
                 return Err(MergeMineError::ValidationError(
                     "More than one merge mining tag found in coinbase".to_string(),
                 ));
             }
+            already_seen_mmfield = true;
             if depth == VarInt(0) && merge_mining_hash.as_bytes() == expected_merge_mining_hash.as_slice() {
                 is_found = true;
             }
