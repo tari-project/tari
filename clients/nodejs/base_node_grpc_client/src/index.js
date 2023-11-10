ebc45398ea7f9eda7f08830cec93f2bf8d4a0e38
Martin Stefcek <35243812+Cifko@users.noreply.github.com>
2023-11-10 11:56:23 +0100

    feat: add getheaderbyhash method to grpc-js (#5942)
    Description
    ---
    add `getHeaderByHash` to the exported method, so we can search by hash
    in the tari explorer.
    
    Motivation and Context
    ---
    
    How Has This Been Tested?
    ---
    
    What process can a PR reviewer use to test or verify this change?
    ---
    
    <!-- Checklist -->
    <!-- 1. Is the title of your PR in the form that would make nice release
    notes? The title, excluding the conventional commit
    tag, will be included exactly as is in the CHANGELOG, so please think
    about it carefully. -->
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory on base node to be deleted
    - [ ] Requires hard fork
    - [ ] Other - Please specify
    
    <!-- Does this include a breaking change? If so, include this line as a
    footer -->
    <!-- BREAKING CHANGE: Description what the user should do, e.g. delete a
    database, resync the chain -->

--- clients/nodejs/base_node_grpc_client/src/index.js
+++ clients/nodejs/base_node_grpc_client/src/index.js
@@ -44,6 +44,7 @@ function Client(address = "127.0.0.1:18142") {
     "getTokens",
     "getNetworkDifficulty",
     "getActiveValidatorNodes",
+    "getHeaderByHash"
   ];
   methods.forEach((method) => {
     this[method] = (arg) => this.inner[method]().sendMessage(arg);
