4274c77a7e0e6b04fa3fb3bf69b7802d699830f4
brianp <brian.o.pearce@gmail.com>
2023-11-23 13:35:37 +0100

    Update consensus encoding change

--- base_layer/tari_mining_helper_ffi/src/lib.rs
+++ base_layer/tari_mining_helper_ffi/src/lib.rs
@@ -386,8 +386,8 @@ mod tests {
 
     #[test]
     fn detect_change_in_consensus_encoding() {
-        const NONCE: u64 = 15659498815241072292;
-        let difficulty = Difficulty::from_u64(2817).expect("Failed to create difficulty");
+        const NONCE: u64 = 9517441887467162514;
+        let difficulty = Difficulty::from_u64(5354).expect("Failed to create difficulty");
         unsafe {
             let mut error = -1;
             let error_ptr = &mut error as *mut c_int;
