2143a5fa4c5ea4afab463d38854e3fc01d33c6ab
brianp <brian.o.pearce@gmail.com>
2023-11-24 16:34:09 +0100

    Update more encoding sanity checks

--- base_layer/tari_mining_helper_ffi/src/lib.rs
+++ base_layer/tari_mining_helper_ffi/src/lib.rs
@@ -386,8 +386,8 @@ mod tests {
 
     #[test]
     fn detect_change_in_consensus_encoding() {
-        const NONCE: u64 = 9517441887467162514;
-        let difficulty = Difficulty::from_u64(5354).expect("Failed to create difficulty");
+        const NONCE: u64 = 16685442580080948032;
+        let difficulty = Difficulty::from_u64(1828).expect("Failed to create difficulty");
         unsafe {
             let mut error = -1;
             let error_ptr = &mut error as *mut c_int;
