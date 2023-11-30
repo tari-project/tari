b3460815cec96ba8e1a9c3984b8affa59b78a67f
SW van Heerden <swvheerden@gmail.com>
2023-11-30 15:58:22 +0200

    update mainet

--- base_layer/core/src/consensus/consensus_constants.rs
+++ base_layer/core/src/consensus/consensus_constants.rs
@@ -574,12 +574,12 @@ impl ConsensusConstants {
     pub fn nextnet() -> Vec<Self> {
         let mut algos = HashMap::new();
         algos.insert(PowAlgorithm::Sha3x, PowAlgorithmConstants {
-            min_difficulty: Difficulty::from_u64(60_000_000).expect("valid difficulty"),
+            min_difficulty: Difficulty::from_u64(1_200_000_000).expect("valid difficulty"),
             max_difficulty: Difficulty::max(),
             target_time: 300,
         });
         algos.insert(PowAlgorithm::RandomX, PowAlgorithmConstants {
-            min_difficulty: Difficulty::from_u64(60_000).expect("valid difficulty"),
+            min_difficulty: Difficulty::from_u64(1_200_000).expect("valid difficulty"),
             max_difficulty: Difficulty::max(),
             target_time: 200,
         });
