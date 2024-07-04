cb1eb6383a0887c95f869146b94c0da00f3faffd
Richard Bertok <bertok.richard.94@gmail.com>
2024-07-04 16:44:54 +0200

    feat(miner): add SHA P2Pool mining related configuration and changes (#6370)
    Description
    ---
    There is a new decentralized P2Pool implementation in progress for SHA3
    based mining on Tari.
    To be able to connect and use this new implementation we need to have
    some new changes introduced in this PR.
    Overall to make the usage of the new mining pool as easy as possible, I
    introduced a new configuration in the miner called `sha_p2pool_enabled`
    and if it's set to true and `base_node_grpc_address` config is set to
    the address of the `p2pool node` then the mining starts on the
    decentralized pool.
    
    Motivation and Context
    ---
    
    How Has This Been Tested?
    ---
    
    What process can a PR reviewer use to test or verify this change?
    ---
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory on base node to be deleted
    - [ ] Requires hard fork
    - [ ] Other - Please specify

--- applications/minotari_miner/src/config.rs
+++ applications/minotari_miner/src/config.rs
@@ -1,28 +1,25 @@
-// Copyright 2021. The Tari Project
+//  Copyright 2024. The Tari Project
 //
-// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
-// following conditions are met:
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+//  following conditions are met:
 //
-// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
-// disclaimer.
+//  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+//  disclaimer.
 //
-// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
-// following disclaimer in the documentation and/or other materials provided with the distribution.
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+//  following disclaimer in the documentation and/or other materials provided with the distribution.
 //
-// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
-// products derived from this software without specific prior written permission.
+//  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+//  products derived from this software without specific prior written permission.
 //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-//! Miner specific configuration
-//!
-//! Minotari Miner Node derives all configuration management
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+//  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+//  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+//  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. Minotari Miner Node derives all
+// configuration management
 //! from [tari_common] crate, also extending with few
 //! specific options:
 //! - base_node_grpc_address - is IPv4/IPv6 address including port
@@ -87,6 +84,8 @@ pub struct MinerConfig {
     pub wallet_payment_address: String,
     /// Range proof type - revealed_value or bullet_proof_plus: (default = revealed_value)
     pub range_proof_type: RangeProofType,
+    /// SHA based p2pool decentralized mining enabled or not
+    pub sha_p2pool_enabled: bool,
 }
 
 /// The proof of work data structure that is included in the block header. For the Minotari miner only `Sha3x` is
@@ -123,6 +122,7 @@ impl Default for MinerConfig {
             config_dir: PathBuf::from("config/miner"),
             wallet_payment_address: TariAddress::default().to_base58(),
             range_proof_type: RangeProofType::RevealedValue,
+            sha_p2pool_enabled: false,
         }
     }
 }
