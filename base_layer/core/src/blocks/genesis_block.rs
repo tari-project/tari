c3eb5159e341bc4299c6d37eadda0e8e013ad3ff
SW van Heerden <swvheerden@gmail.com>
2024-09-11 10:37:35 +0200

    new release

--- base_layer/core/src/blocks/genesis_block.rs
+++ base_layer/core/src/blocks/genesis_block.rs
@@ -195,7 +195,7 @@ pub fn get_nextnet_genesis_block() -> ChainBlock {
 
 fn get_nextnet_genesis_block_raw() -> Block {
     // Set genesis timestamp
-    let genesis_timestamp = DateTime::parse_from_rfc2822("11 Mar 2024 08:00:00 +0200").expect("parse may not fail");
+    let genesis_timestamp = DateTime::parse_from_rfc2822("11 Sep 2024 08:00:00 +0200").expect("parse may not fail");
     // Let us add a "not before" proof to the genesis block
     let not_before_proof = b"nextnet has a blast, its prowess echoed in every gust \
         \
