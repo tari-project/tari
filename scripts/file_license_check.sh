4ee8ae824503c6c0d1cadee52b027ea6a06c485d
C.Lee Taylor <47312074+leet4tari@users.noreply.github.com>
2024-10-08 11:59:52 +0200

    chore(ci): add riscv64 builds and misc script fixes (#6611)
    Description
    Bump rcgen crate (removes ring v0.16)
    Extend cross-compile scripts to support riscv64
    Add make to build system, ubuntu 20.04 and above don't include by
    default.
    
    Motivation and Context
    Add working riscv64 builds - enabled by default
    
    How Has This Been Tested?
    Builds locally, both emulated and cross-compile for riscv64

--- scripts/file_license_check.sh
+++ scripts/file_license_check.sh
@@ -1,5 +1,49 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
 # Must be run from the repo root
+#
+
+#set -xo pipefail
+set -e
+
+check_for() {
+  if prog_location=$(which ${1}) ; then
+    if result=$(${1} --version 2>/dev/null); then
+      result="${1}: ${result} INSTALLED ✓"
+    else
+      result="${1}: INSTALLED ✓"
+    fi
+  else
+    result="${1}: MISSING ⨯"
+  fi
+}
+
+check_requirements() {
+  echo "List of requirements and possible test:"
+  req_progs=(
+    mktemp
+    rg
+    diff
+  )
+  for RProg in "${req_progs[@]}"; do
+    check_for ${RProg}
+    echo "${result}"
+    if [[ "${result}" == "${RProg}: MISSING ⨯" ]]; then
+      echo "!! Install ${RProg} and try again !!"
+      exit -1
+    fi
+  done
+
+  if [ ! -f .license.ignore ]; then
+    echo "!! No .license.ignore file !!"
+    exit -1
+  fi
+}
+
+check_requirements
+
+diffparms=${diffparms:-"-u --suppress-blank-empty --strip-trailing-cr --color=never"}
+rgTemp=${rgTemp:-$(mktemp)}
 
 # rg -i "Copyright.*The Tari Project" --files-without-match \
 #    -g '!*.{Dockerfile,asc,bat,config,config.js,css,csv,drawio,env,gitkeep,hbs,html,ini,iss,json,lock,md,min.js,ps1,py,rc,scss,sh,sql,svg,toml,txt,yml,vue}' . \
@@ -8,23 +52,31 @@
 # Exclude files without extensions as well as those with extensions that are not in the list
 #
 rg -i "Copyright.*The Tari Project" --files-without-match \
-    -g '!*.{Dockerfile,asc,bat,config,config.js,css,csv,drawio,env,gitkeep,hbs,html,ini,iss,json,lock,md,min.js,ps1,py,rc,scss,sh,sql,svg,toml,txt,yml,vue}' . \
+    -g '!*.{Dockerfile,asc,bat,config,config.js,css,csv,drawio,env,gitkeep,hbs,html,ini,iss,json,lock,md,min.js,ps1,py,rc,scss,sh,sql,svg,toml,txt,yml,vue,liquid,otf,d.ts,mjs}' . \
     | while IFS= read -r file; do
         if [[ -n $(basename "$file" | grep -E '\.') ]]; then
             echo "$file"
         fi
-    done | sort > /tmp/rgtemp
+    done | sort > ${rgTemp}
 
 # Sort the .license.ignore file as sorting seems to behave differently on different platforms
-cat .license.ignore | sort > /tmp/.license.ignore
+licenseIgnoreTemp=${licenseIgnoreTemp:-$(mktemp)}
+cat .license.ignore | sort > ${licenseIgnoreTemp}
 
-DIFFS=$(diff -u --strip-trailing-cr /tmp/.license.ignore /tmp/rgtemp)
+DIFFS=$( diff ${diffparms} ${licenseIgnoreTemp} ${rgTemp} || true )
 
-if [ -n "$DIFFS" ]; then
-    echo "New files detected that either need copyright/license identifiers added, or they need to be added to .license.ignore"
+# clean up
+rm -vf ${rgTemp}
+rm -vf ${licenseIgnoreTemp}
+
+if [ -n "${DIFFS}" ]; then
+    echo "New files detected that either need copyright/license identifiers added, "
+    echo "or they need to be added to .license.ignore"
     echo "NB: The ignore file must be sorted alphabetically!"
 
     echo "Diff:"
-    echo "$DIFFS"
+    echo "${DIFFS}"
     exit 1
+else
+    exit 0
 fi
