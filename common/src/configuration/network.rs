5f23adc2fedcdf537ef1924bc5e935e94e122ddc
SW van Heerden <swvheerden@gmail.com>
2024-10-08 12:25:35 +0200

    chore: changes mainnet default network (#6610)
    Description
    ---
    Changes stagenet dev target to run mainnet network by default and not
    stagenet

--- common/src/configuration/network.rs
+++ common/src/configuration/network.rs
@@ -120,8 +120,8 @@ impl Default for Network {
     #[cfg(tari_target_network_mainnet)]
     fn default() -> Self {
         match std::env::var("TARI_NETWORK") {
-            Ok(network) => Network::from_str(network.as_str()).unwrap_or(Network::StageNet),
-            Err(_) => Network::StageNet,
+            Ok(network) => Network::from_str(network.as_str()).unwrap_or(Network::MainNet),
+            Err(_) => Network::MainNet,
         }
     }
 
