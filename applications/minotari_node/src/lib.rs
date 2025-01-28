43d454e7a3e6a3b6d905d7fa95c4f4f53d8d471c
SW van Heerden <swvheerden@gmail.com>
2025-01-28 17:26:04 +0200

    fix error code

--- applications/minotari_node/src/lib.rs
+++ applications/minotari_node/src/lib.rs
@@ -153,7 +153,7 @@ pub async fn run_base_node_with_cli(
     }
 
     ctx.start()
-        .map_err(|e| ExitError::new(ExitCode::UnknownError, &format!("Could not start database.{:?}", e)))?;
+        .map_err(|e| ExitError::new(ExitCode::DatabaseError, &format!("Could not start database.{:?}", e)))?;
 
     // Run, node, run!
     let context = CommandContext::new(&ctx, shutdown.clone());
