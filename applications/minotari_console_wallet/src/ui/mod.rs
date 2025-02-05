tari-project/tari

commit 3ad35afe0355569e593eb552f2f90ff312f4e866

feat: upgrade crossterm (#6783)
Description
---
- Upgraded crossterm to the latest version (0.28)
- Fixed Windows compatibility issues for the console wallet

Closes #6211

Motivation and Context
---
crossterm needed updating - see #6211

How Has This Been Tested?
---
System-level testing on Windows and Linux (Ubuntu WSL)

What process can a PR reviewer use to test or verify this change?
---
System-level testing 
Code review

<!-- Checklist -->
<!-- 1. Is the title of your PR in the form that would make nice release
notes? The title, excluding the conventional commit
tag, will be included exactly as is in the CHANGELOG, so please think
about it carefully. -->


Breaking Changes
---

- [x] None
- [ ] Requires data directory on base node to be deleted
- [ ] Requires hard fork
- [ ] Other - Please specify

<!-- Does this include a breaking change? If so, include this line as a
footer -->
<!-- BREAKING CHANGE: Description what the user should do, e.g. delete a
database, resync the chain -->

applications/minotari_console_wallet/src/ui/mod.rs
@@ -38,7 +38,7 @@ use std::io::{stdout, Stdout};
 
 pub use app::*;
 use crossterm::{
-    event::{KeyCode, KeyModifiers},
+    event::{KeyCode, KeyEventState, KeyModifiers},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
@@ -74,7 +74,9 @@ pub fn run(app: App<CrosstermBackend<Stdout>>) -> Result<(), ExitError> {
         .map_err(|e| ExitError::new(ExitCode::WalletError, e))?;
     crossterm_loop(app)
 }
+
 /// This is the main loop of the application UI using Crossterm based events
+#[allow(clippy::too_many_lines)]
 fn crossterm_loop(mut app: App<CrosstermBackend<Stdout>>) -> Result<(), ExitError> {
     let events = CrosstermEvents::new();
     enable_raw_mode().map_err(|e| {
@@ -108,25 +110,54 @@ fn crossterm_loop(mut app: App<CrosstermBackend<Stdout>>) -> Result<(), ExitErro
             error!(target: LOG_TARGET, "Error drawing interface. {}", e);
             ExitCode::InterfaceError
         })?;
+        let event = events.next();
         #[allow(clippy::blocks_in_conditions)]
-        match events.next().map_err(|e| {
+        match event.map_err(|e| {
             error!(target: LOG_TARGET, "Error reading input event: {}", e);
             ExitCode::InterfaceError
         })? {
-            Event::Input(event) => match (event.code, event.modifiers) {
-                (KeyCode::Char(c), KeyModifiers::CONTROL) => app.on_control_key(c),
-                (KeyCode::Char(c), _) => app.on_key(c),
-                (KeyCode::Left, _) => app.on_left(),
-                (KeyCode::Up, _) => app.on_up(),
-                (KeyCode::Right, _) => app.on_right(),
-                (KeyCode::Down, _) => app.on_down(),
-                (KeyCode::Esc, _) => app.on_esc(),
-                (KeyCode::Backspace, _) => app.on_backspace(),
-                (KeyCode::Enter, _) => app.on_key('\n'),
-                (KeyCode::Tab, _) => app.on_key('\t'),
-                (KeyCode::BackTab, _) => app.on_backtab(),
-                (KeyCode::F(10), _) => app.on_f10(),
-                _ => {},
+            Event::Input(event) => {
+                trace!(target: LOG_TARGET, "event: '{:?}' '{}' '{:?}' '{}'",
+                    event.code,
+                    event.modifiers,
+                    event.kind,
+                    match event.state {
+                        KeyEventState::KEYPAD => "KEYPAD",
+                        KeyEventState::CAPS_LOCK => "CAPS_LOCK",
+                        KeyEventState::NUM_LOCK => "NUM_LOCK",
+                        _ => "NONE",
+                    }
+                );
+                #[cfg(target_os = "windows")]
+                {
+                    use crossterm::event::KeyEventKind;
+                    let action_now = {
+                        match (event.kind, event.modifiers, event.code) {
+                            (KeyEventKind::Press, KeyModifiers::CONTROL, KeyCode::Char(c)) => c == 'q' || c == 'c',
+                            (KeyEventKind::Press, _, KeyCode::F(c)) => c == 10,
+                            (KeyEventKind::Release, _, _) => true,
+                            (..) => false,
+                        }
+                    };
+                }
+                #[cfg(not(target_os = "windows"))]
+                let action_now = true;
+                match (event.code, event.modifiers, action_now) {
+                    (_, _, false) => {},
+                    (KeyCode::Char(c), KeyModifiers::CONTROL, _) => app.on_control_key(c),
+                    (KeyCode::Char(c), _, _) => app.on_key(c),
+                    (KeyCode::Left, _, _) => app.on_left(),
+                    (KeyCode::Up, _, _) => app.on_up(),
+                    (KeyCode::Right, _, _) => app.on_right(),
+                    (KeyCode::Down, _, _) => app.on_down(),
+                    (KeyCode::Esc, _, _) => app.on_esc(),
+                    (KeyCode::Backspace, _, _) => app.on_backspace(),
+                    (KeyCode::Enter, _, _) => app.on_key('\n'),
+                    (KeyCode::Tab, _, _) => app.on_key('\t'),
+                    (KeyCode::BackTab, _, _) => app.on_backtab(),
+                    (KeyCode::F(10), _, _) => app.on_f10(),
+                    _ => {},
+                }
             },
             Event::Tick => {
                 app.on_tick();

applications/minotari_console_wallet/Cargo.toml
@@ -37,7 +37,7 @@ blake2 = "0.10"
 chrono = { version = "0.4.39", default-features = false }
 clap = { version = "3.2", features = ["derive", "env"] }
 config = "0.14.0"
-crossterm = { version = "0.25.0" }
+crossterm = { version = "0.28" }
 digest = "0.10"
 dirs-next = "2.0"
 futures = { version = "^0.3.16", default-features = false, features = [

applications/minotari_console_wallet/src/ui/app.rs
@@ -20,6 +20,7 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+use log::trace;
 use minotari_wallet::{error::WalletError, util::wallet_identity::WalletIdentity, WalletConfig, WalletSqlite};
 use tari_common::exit_codes::{ExitCode, ExitError};
 use tari_comms::peer_manager::Peer;
@@ -136,6 +137,7 @@ impl<B: Backend> App<B> {
     }
 
     pub fn on_control_key(&mut self, c: char) {
+        trace!(target: LOG_TARGET, "on_control_key: {}", c);
         match c {
             'q' | 'c' => {
                 self.should_quit = true;
@@ -145,6 +147,7 @@ impl<B: Backend> App<B> {
     }
 
     pub fn on_key(&mut self, c: char) {
+        trace!(target: LOG_TARGET, "on_key: {}", c);
         match c {
             '\t' => {
                 self.tabs.next();
@@ -154,34 +157,42 @@ impl<B: Backend> App<B> {
     }
 
     pub fn on_backtab(&mut self) {
+        trace!(target: LOG_TARGET, "on_backtab");
         self.tabs.previous();
     }
 
     pub fn on_up(&mut self) {
+        trace!(target: LOG_TARGET, "on_up");
         self.tabs.on_up(&mut self.app_state);
     }
 
     pub fn on_down(&mut self) {
+        trace!(target: LOG_TARGET, "on_down");
         self.tabs.on_down(&mut self.app_state);
     }
 
     pub fn on_f10(&mut self) {
+        trace!(target: LOG_TARGET, "on_f10");
         self.should_quit = true;
     }
 
     pub fn on_right(&mut self) {
+        trace!(target: LOG_TARGET, "on_right");
         self.tabs.next();
     }
 
     pub fn on_left(&mut self) {
+        trace!(target: LOG_TARGET, "on_left");
         self.tabs.previous();
     }
 
     pub fn on_esc(&mut self) {
+        trace!(target: LOG_TARGET, "on_esc");
         self.tabs.on_esc(&mut self.app_state);
     }
 
     pub fn on_backspace(&mut self) {
+        trace!(target: LOG_TARGET, "on_backspace");
         self.tabs.on_backspace(&mut self.app_state);
     }
 

applications/minotari_merge_mining_proxy/Cargo.toml
@@ -33,7 +33,7 @@ bytes = "1.1"
 chrono = { version = "0.4.39", default-features = false }
 clap = { version = "3.2", features = ["derive", "env"] }
 config = { version = "0.14.0" }
-crossterm = { version = "0.25.0" }
+crossterm = { version = "0.28" }
 futures = { version = "^0.3.16", features = ["async-await"] }
 hex = "0.4.2"
 hyper = { version = "0.14.12", features = ["default"] }

applications/minotari_miner/Cargo.toml
@@ -25,7 +25,7 @@ bufstream = "0.1"
 chrono = { version = "0.4.39", default-features = false }
 clap = { version = "3.2", features = ["derive"] }
 crossbeam = "0.8"
-crossterm = { version = "0.25.0" }
+crossterm = { version = "0.28" }
 derivative = "2.2.0"
 futures = "0.3"
 hex = "0.4.2"

applications/minotari_node/Cargo.toml
@@ -32,7 +32,7 @@ chrono = { version = "0.4.39", default-features = false }
 clap = { version = "3.2", features = ["derive", "env"] }
 console-subscriber = "0.1.8"
 config = { version = "0.14.0" }
-crossterm = { version = "0.25.0", features = ["event-stream"] }
+crossterm = { version = "0.28", features = ["event-stream"] }
 derive_more = "0.99.17"
 either = "1.6.1"
 futures = { version = "^0.3.16", default-features = false, features = [

commit 431b47a0b14af02bfea287895011fa76882efb67

Verify wallet recovery
- Verified wallet recovery does not always start scanning from the genesis block instead of from
a bit before the closest wallet birthday block.
- Fixed a Windows build error introduced by #6783.

applications/minotari_console_wallet/src/ui/mod.rs
@@ -129,17 +129,15 @@ fn crossterm_loop(mut app: App<CrosstermBackend<Stdout>>) -> Result<(), ExitErro
                     }
                 );
                 #[cfg(target_os = "windows")]
-                {
+                let action_now = {
                     use crossterm::event::KeyEventKind;
-                    let action_now = {
-                        match (event.kind, event.modifiers, event.code) {
-                            (KeyEventKind::Press, KeyModifiers::CONTROL, KeyCode::Char(c)) => c == 'q' || c == 'c',
-                            (KeyEventKind::Press, _, KeyCode::F(c)) => c == 10,
-                            (KeyEventKind::Release, _, _) => true,
-                            (..) => false,
-                        }
-                    };
-                }
+                    match (event.kind, event.modifiers, event.code) {
+                        (KeyEventKind::Press, KeyModifiers::CONTROL, KeyCode::Char(c)) => c == 'q' || c == 'c',
+                        (KeyEventKind::Press, _, KeyCode::F(c)) => c == 10,
+                        (KeyEventKind::Release, _, _) => true,
+                        (..) => false,
+                    }
+                };
                 #[cfg(not(target_os = "windows"))]
                 let action_now = true;
                 match (event.code, event.modifiers, action_now) {

base_layer/core/src/base_node/rpc/service.rs
@@ -575,7 +575,7 @@ impl<B: BlockchainBackend + 'static> BaseNodeWalletService for BaseNodeWalletRpc
 
     async fn get_height_at_time(&self, request: Request<u64>) -> Result<Response<u64>, RpcStatus> {
         let requested_epoch_time: u64 = request.into_message();
-
+        trace!(target: LOG_TARGET, "requested_epoch_time: {}", requested_epoch_time);
         let tip_header = self
             .db()
             .fetch_tip_header()
@@ -584,6 +584,13 @@ impl<B: BlockchainBackend + 'static> BaseNodeWalletService for BaseNodeWalletRpc
 
         let mut left_height = 0u64;
         let mut right_height = tip_header.height();
+        trace!(
+            target: LOG_TARGET,
+            "requested_epoch_time: {}, left: {}, right: {}",
+            requested_epoch_time,
+            left_height,
+            right_height
+        );
 
         while left_height <= right_height {
             let mut mid_height = (left_height + right_height) / 2;
@@ -612,11 +619,32 @@ impl<B: BlockchainBackend + 'static> BaseNodeWalletService for BaseNodeWalletRpc
                 .ok_or_else(|| {
                     RpcStatus::not_found(&format!("Header not found during search at height {}", mid_height - 1))
                 })?;
+            trace!(
+                target: LOG_TARGET,
+                "requested_epoch_time: {}, left: {}, mid: {}/{} ({}/{}), right: {}",
+                requested_epoch_time,
+                left_height,
+                mid_height,
+                mid_height-1,
+                mid_header.timestamp.as_u64(),
+                before_mid_header.timestamp.as_u64(),
+                right_height
+            );
             if requested_epoch_time < mid_header.timestamp.as_u64() &&
                 requested_epoch_time >= before_mid_header.timestamp.as_u64()
             {
+                trace!(
+                    target: LOG_TARGET,
+                    "requested_epoch_time: {}, selected height: {}",
+                    requested_epoch_time, before_mid_header.height
+                );
                 return Ok(Response::new(before_mid_header.height));
             } else if mid_height == right_height {
+                trace!(
+                    target: LOG_TARGET,
+                    "requested_epoch_time: {}, selected height: {}",
+                    requested_epoch_time, right_height
+                );
                 return Ok(Response::new(right_height));
             } else if requested_epoch_time <= mid_header.timestamp.as_u64() {
                 right_height = mid_height;

base_layer/key_manager/src/cipher_seed.rs
@@ -70,6 +70,7 @@ pub const CIPHER_SEED_MAC_BYTES: usize = 5;
 pub const CIPHER_SEED_ENCRYPTION_KEY_BYTES: usize = 32;
 pub const CIPHER_SEED_MAC_KEY_BYTES: usize = 32;
 pub const CIPHER_SEED_CHECKSUM_BYTES: usize = 4;
+pub const SECONDS_PER_DAY: u64 = 24 * 60 * 60;
 
 /// This is an implementation of a Cipher Seed based on the `aezeed` encoding scheme:
 /// https://github.com/lightningnetwork/lnd/tree/master/aezeed
@@ -134,7 +135,6 @@ impl CipherSeed {
     /// Generate a new seed
     pub fn new() -> Self {
         use std::time::{Duration, SystemTime, UNIX_EPOCH};
-        const SECONDS_PER_DAY: u64 = 24 * 60 * 60;
         let birthday_genesis_date = UNIX_EPOCH + Duration::from_secs(BIRTHDAY_GENESIS_FROM_UNIX_EPOCH);
         let days = SystemTime::now()
             .duration_since(birthday_genesis_date)
@@ -148,7 +148,7 @@ impl CipherSeed {
     #[cfg(target_arch = "wasm32")]
     /// Generate a new seed
     pub fn new() -> Self {
-        const MILLISECONDS_PER_DAY: u64 = 24 * 60 * 60 * 1000;
+        const MILLISECONDS_PER_DAY: u64 = SECONDS_PER_DAY * 1000;
         let millis = js_sys::Date::now() as u64;
         let days = millis / MILLISECONDS_PER_DAY;
         let birthday = u16::try_from(days).unwrap_or(0u16); // default to the epoch on error
@@ -447,10 +447,11 @@ impl Mnemonic<CipherSeed> for CipherSeed {
 mod test {
     use std::str::FromStr;
 
+    use chrono::{DateTime, TimeZone, Utc};
     use crc32fast::Hasher as CrcHasher;
     use tari_utilities::{Hidden, SafePassword};
 
-    use super::BIRTHDAY_GENESIS_FROM_UNIX_EPOCH;
+    use super::{BIRTHDAY_GENESIS_FROM_UNIX_EPOCH, SECONDS_PER_DAY};
     use crate::{
         cipher_seed::{
             CipherSeed,
@@ -663,7 +664,7 @@ mod test {
         let birthday_genesis_time_in_seconds = get_birthday_from_unix_epoch_in_seconds(birthday, to_days);
         assert_eq!(
             birthday_genesis_time_in_seconds,
-            BIRTHDAY_GENESIS_FROM_UNIX_EPOCH + u64::from(birthday - to_days) * 24 * 60 * 60
+            BIRTHDAY_GENESIS_FROM_UNIX_EPOCH + u64::from(birthday - to_days) * SECONDS_PER_DAY
         );
     }
 
@@ -677,5 +678,57 @@ mod test {
         // 1656806400 corresponds to the duration, in seconds, from unix epoch
         // to 3th July 2022 00:00:00
         assert_eq!(birthday_from_unix_epoch, 1656806400);
+    }
+
+    // This test simulates a wallet that was created on the 29th January 2025, one day after the birthday genesis date
+    // of 28th January 2025. The birthday is 1124 days after the genesis date of 1st January 2022. The wallet will
+    // request blocks from the base node 14 days before its birthday, which is 15th January 2025.
+    #[test]
+    fn verify_birthday_genesis_from_unix_epoch_constant() {
+        let birthday = 1124u16;
+        let to_days = 14u16;
+        let year = 2025;
+        let month = 1;
+        let day = 29;
+        let day_less_to_days = day - u32::from(to_days);
+
+        // Verify birthday genesis from unix epoch constant
+        let date = Utc.with_ymd_and_hms(2022, 1, 1, 0, 0, 0);
+        let seconds_since_epoch = date.unwrap().timestamp();
+        assert_eq!(
+            BIRTHDAY_GENESIS_FROM_UNIX_EPOCH,
+            u64::try_from(seconds_since_epoch).unwrap()
+        );
+
+        // The wallet birthday is 1124 days after the genesis date
+        let birthday_epoch_time = i64::try_from(BIRTHDAY_GENESIS_FROM_UNIX_EPOCH).unwrap() +
+            i64::from(birthday) * i64::try_from(SECONDS_PER_DAY).unwrap();
+        assert_eq!(birthday_epoch_time, 1738108800i64);
+        let birthday_epoch_time_date = Utc.timestamp_opt(birthday_epoch_time, 0);
+        assert_eq!(
+            birthday_epoch_time_date.unwrap(),
+            Utc.with_ymd_and_hms(year, month, day, 0, 0, 0).unwrap()
+        );
+
+        // The wallet will request blocks from the base node 14 days before its birthday
+        let birthday_epoch_time_less_to_days =
+            birthday_epoch_time - i64::from(to_days) * i64::try_from(SECONDS_PER_DAY).unwrap();
+        assert_eq!(
+            birthday_epoch_time_less_to_days,
+            i64::try_from(get_birthday_from_unix_epoch_in_seconds(birthday, to_days)).unwrap()
+        );
+        assert_eq!(birthday_epoch_time_less_to_days, 1736899200i64);
+        let birthday_epoch_time_less_to_days_date = Utc.timestamp_opt(birthday_epoch_time_less_to_days, 0);
+        assert_eq!(
+            birthday_epoch_time_less_to_days_date.unwrap(),
+            Utc.with_ymd_and_hms(year, month, day_less_to_days, 0, 0, 0).unwrap()
+        );
+
+        // The base node will return the genesis block as the first block to scan from
+        let genesis_date_time = DateTime::parse_from_rfc2822("28 Jan 2025 08:00:00 +0200").unwrap();
+        let genesis_epoch_time = genesis_date_time.timestamp();
+        assert_eq!(genesis_epoch_time, 1738044000i64);
+        assert!(genesis_epoch_time > birthday_epoch_time_less_to_days);
+        assert!(genesis_epoch_time < birthday_epoch_time);
     }
 }

base_layer/wallet/src/utxo_scanner_service/utxo_scanner_task.rs
@@ -765,6 +765,13 @@ where
                 0
             },
         };
+        trace!(
+            target: LOG_TARGET,
+            "Wallet birthday: {}, epoch time: {}, block height: {}",
+            birthday,
+            epoch_time,
+            block_height
+        );
         let header = client.get_header_by_height(block_height).await?;
         let header = BlockHeader::try_from(header).map_err(UtxoScannerError::ConversionError)?;
         let header_hash = header.hash();