b4c45a2656b3ddc2d443cf57556ce3f04db09df9
SW van Heerden <swvheerden@gmail.com>
2025-02-28 10:11:31 +0200

    monero block

--- base_layer/core/src/blocks/genesis_block.rs
+++ base_layer/core/src/blocks/genesis_block.rs
@@ -293,7 +293,8 @@ pub fn get_mainnet_genesis_block() -> ChainBlock {
 fn get_mainnet_genesis_block_raw() -> Block {
     // Set genesis timestamp
     let genesis_timestamp = DateTime::parse_from_rfc2822("28 Feb 2025 08:00:00 +0200").expect("parse may not fail");
-    let not_before_proof = b"{Place holder}";
+    let not_before_proof: Vec<u8> =
+        from_hex("b1af2e243a168eecafabf1f418a7cd12e820c6d8f3acc0caca14a403cdf0145e").unwrap();
     if not_before_proof.len() > PowData::default().max_size() {
         panic!(
             "Not-before-proof data is too large, exceeds limit by '{}' bytes",
