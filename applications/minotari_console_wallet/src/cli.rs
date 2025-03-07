e340be156fa1751b39aea7a48a808191c4611606
Marcin Papież <mmpapiez@gmail.com>
2025-03-07 14:53:22 +0100

    chore: handle seed words env var

--- applications/minotari_console_wallet/src/cli.rs
+++ applications/minotari_console_wallet/src/cli.rs
@@ -61,7 +61,7 @@ pub struct Cli {
     pub recovery: bool,
     /// Supply the optional wallet seed words for recovery on the command line. They should be in one string space
     /// separated. e.g. --seed-words "seed1 seed2 ..."
-    #[clap(long, alias = "seed-words")]
+    #[clap(long, alias = "seed-words", env = "MINOTARI_WALLET_SEED_WORDS", hide_env_values = true)]
     pub seed_words: Option<SeedWords>,
     /// Supply the optional file name to save the wallet seed words into
     #[clap(long, aliases = &["seed_words_file_name", "seed-words-file"], parse(from_os_str))]
