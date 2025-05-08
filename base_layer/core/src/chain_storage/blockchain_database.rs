a4f8bd85bc8742241057bfef08a628a82a360f6a
stringhandler <stringhandler@protonmail.com>
2025-05-08 15:07:48 +0200

    logging

--- base_layer/core/src/chain_storage/blockchain_database.rs
+++ base_layer/core/src/chain_storage/blockchain_database.rs
@@ -466,8 +466,8 @@ where
                     .is_some();
                 info!(
                     target: LOG_TARGET,
-                    "fetch_outputs_with_spend_status_at_tip: smt_key: {}, spent: {}",
-                    smt_key.to_hex(),
+                    "fetch_outputs_with_spend_status_at_tip: smt_key: {:?}, spent: {}",
+                    smt_key,
                     spent
                 );
                 result.push(Some((mined_info.output, spent)));
