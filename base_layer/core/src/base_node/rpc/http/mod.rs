c37de02f4bf70c5b282c6625dcd7ab5e507d095f
ksrichard <bertok.richard.94@gmail.com>
2025-05-22 11:51:33 +0200

    fix wallet ffi check

--- base_layer/core/src/base_node/rpc/http/mod.rs
+++ base_layer/core/src/base_node/rpc/http/mod.rs
@@ -2,6 +2,9 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 pub mod client;
+#[cfg(feature = "base_node")]
 pub mod handler;
+#[cfg(feature = "base_node")]
 pub mod query_service;
+#[cfg(feature = "base_node")]
 pub mod server;
