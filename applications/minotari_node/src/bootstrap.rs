a41155ab40e9ae66ada92740fa10b6b821b8c674
ksrichard <bertok.richard.94@gmail.com>
2025-05-26 11:12:09 +0200

    small update

--- applications/minotari_node/src/bootstrap.rs
+++ applications/minotari_node/src/bootstrap.rs
@@ -87,8 +87,7 @@ pub struct BaseNodeBootstrapper<'a, B> {
 }
 
 impl<B> BaseNodeBootstrapper<'_, B>
-where
-    B: BlockchainBackend + 'static,
+where B: BlockchainBackend + 'static
 {
     #[allow(clippy::too_many_lines)]
     pub async fn bootstrap(self) -> Result<ServiceHandles, ExitError> {
@@ -219,11 +218,11 @@ where
         // Save final node identity after comms has initialized. This is required because the public_address can be
         // changed by comms during initialization when using tor.
         match p2p_config.transport.transport_type {
-            TransportType::Tcp => {} // Do not overwrite TCP public_address in the base_node_id!
+            TransportType::Tcp => {}, // Do not overwrite TCP public_address in the base_node_id!
             _ => {
                 identity_management::save_as_json(&base_node_config.identity_file, &*comms.node_identity())
                     .map_err(|e| ExitError::new(ExitCode::IdentityError, e))?;
-            }
+            },
         };
 
         handles.register(comms);
