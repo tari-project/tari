da318b87cc60539595d20bc0829c9edb9a813717
SW van Heerden <swvheerden@gmail.com>
2025-06-02 09:29:27 +0200

    fix win

--- integration_tests/build.rs
+++ integration_tests/build.rs
@@ -11,6 +11,9 @@ fn main() {
     #[cfg(target_os = "linux")]
     let out_dirs = env::var("LD_LIBRARY_PATH").unwrap();
 
+    #[cfg(windows)]
+    let out_dir = out_dirs.split(';').next().unwrap_or(".");
+    #[cfg(not(windows))]
     let out_dir = out_dirs.split(':').next().unwrap_or(".");
     println!("cargo::rustc-link-search=native={}", out_dir);
     println!("cargo::rustc-link-lib=dylib=minotari_wallet_ffi");
