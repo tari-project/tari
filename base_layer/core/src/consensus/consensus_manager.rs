6787bbc86e776148bb8fd1413b46137cb54ad5ba
Hansie Odendaal <pluto@tari.com>
2025-07-08 11:44:10 +0200

    merge

--- base_layer/core/src/consensus/consensus_manager.rs
+++ base_layer/core/src/consensus/consensus_manager.rs
@@ -20,7 +20,9 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-use std::{convert::TryFrom, sync::Arc};
+#[cfg(feature = "base_node")]
+use std::convert::TryFrom;
+use std::sync::Arc;
 
 use tari_common::configuration::Network;
 use thiserror::Error;
