9019282fb7f9c2d8f8047f8ba5030877a4d241b4
SW van Heerden <swvheerden@gmail.com>
2025-07-10 12:49:42 +0200

    clippy

--- base_layer/wallet_ffi/src/lib.rs
+++ base_layer/wallet_ffi/src/lib.rs
@@ -11535,7 +11535,7 @@ mod test {
                 OutputType::from_byte(output_type as u8).unwrap()
             );
             assert_eq!((*output_features).maturity, maturity);
-            assert!((&(*output_features).coinbase_extra).is_empty());
+            assert!((*output_features).coinbase_extra.is_empty());
 
             output_features_destroy(output_features);
             byte_vector_destroy(metadata);
