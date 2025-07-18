43531e0a234b15e7c7b58336ab32e8c410030925
SW van Heerden <swvheerden@gmail.com>
2025-07-18 10:47:58 +0200

    clippy

--- applications/minotari_console_wallet/src/grpc/wallet_debouncer.rs
+++ applications/minotari_console_wallet/src/grpc/wallet_debouncer.rs
@@ -190,10 +190,11 @@ impl WalletDebouncer {
                                 },
                                 TransactionEvent::TransactionValidationStateChanged{faux, ..} => {
                                     self.set_refresh_needed(true).await;
+                                    #[allow(clippy::collapsible_if)]
                                     if faux {
                                         let mut intial = self.initial_validation_done.lock().await;
                                         if !(*intial) {
-                                            if self.intial_scanning_done.lock().await{
+                                            if *self.intial_scanning_done.lock().await{
                                                 // we should only set this after we completed initial scanning and then completed faux tx validation
                                                 *intial = true;
                                             }
