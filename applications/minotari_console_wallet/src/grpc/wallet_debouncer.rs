9bd3b122ade050c8e2ca1b772dfbf3dbfcbd093d
SW van Heerden <swvheerden@gmail.com>
2025-07-18 09:56:00 +0200

    fix

--- applications/minotari_console_wallet/src/grpc/wallet_debouncer.rs
+++ applications/minotari_console_wallet/src/grpc/wallet_debouncer.rs
@@ -192,7 +192,12 @@ impl WalletDebouncer {
                                     self.set_refresh_needed(true).await;
                                     if faux {
                                         let mut intial = self.initial_validation_done.lock().await;
-                                        *intial = true;
+                                        if !(*intial) {
+                                            if self.intial_scanning_done.lock().await{
+                                                // we should only set this after we completed initial scanning and then completed faux tx validation
+                                                *intial = true;
+                                            }
+                                        }
                                     }
                                 },
                                 _ => (),
