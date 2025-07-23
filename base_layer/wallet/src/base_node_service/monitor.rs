1397eec35ee470031d44be02909f84043192921a
SW van Heerden <swvheerden@gmail.com>
2025-07-23 14:52:47 +0200

    change to hash

--- base_layer/wallet/src/base_node_service/monitor.rs
+++ base_layer/wallet/src/base_node_service/monitor.rs
@@ -19,12 +19,12 @@
 //  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
 //  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 //  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 use std::{sync::Arc, time::Duration};
 
 use chrono::Utc;
 use log::*;
 use minotari_node_wallet_client::BaseNodeWalletClient;
+use tari_common_types::types::FixedHash;
 use tari_comms::protocol::rpc::RpcError;
 use tari_shutdown::ShutdownSignal;
 use tokio::{select, sync::RwLock, time::interval};
@@ -90,7 +90,7 @@ where TWalletConnectivity: WalletConnectivityInterface
     async fn monitor_node(&mut self, mut shutdown_signal: ShutdownSignal) -> Result<(), BaseNodeMonitorError> {
         let mut interval = interval(Duration::from_secs(10));
         interval.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Skip);
-        let mut last_checked_height = 0;
+        let mut last_checked_hash = FixedHash::zero();
 
         loop {
             select! {
@@ -135,11 +135,12 @@ where TWalletConnectivity: WalletConnectivityInterface
                         latency.as_millis()
                     );
 
-                    if best_block_height == last_checked_height {
+                    let tip_hash = chain_metadata.best_block_hash();
+                    if last_checked_hash == *tip_hash {
                         // no new block, continue to the next iteration
                         continue;
                     }
-                    last_checked_height = best_block_height;
+                    last_checked_hash = tip_hash.clone();
 
                     self
                         .update_state(BaseNodeState {
