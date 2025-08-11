362eec4d5a0e0052ad98ef8f29b4f636158aba6c
Maciej Kozuszek <mackozuszek@gmail.com>
2025-08-11 17:12:03 +0200

    Remove dead code

--- comms/core/src/protocol/messaging/error.rs
+++ comms/core/src/protocol/messaging/error.rs
@@ -29,19 +29,8 @@ use crate::{
     connection_manager::PeerConnectionError,
     connectivity::ConnectivityError,
     message::OutboundMessage,
-    peer_manager::PeerManagerError,
     protocol::ProtocolError,
 };
-
-/// Error type for inbound messages.
-#[allow(dead_code)]
-#[derive(Debug, Error)]
-pub enum InboundMessagingError {
-    #[error("PeerManagerError: {0}")]
-    PeerManagerError(#[from] PeerManagerError),
-    #[error("Failed to decode message: {0}")]
-    MessageDecodeError(#[from] prost::DecodeError),
-}
 
 /// Error type for the messaging protocol.
 #[derive(Debug, Error)]
