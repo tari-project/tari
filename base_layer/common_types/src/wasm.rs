d2f54fcc6f0e7b645191876788523322d12d8178
SW van Heerden <swvheerden@gmail.com>
2025-08-22 11:48:42 +0200

    wasm lic

--- base_layer/common_types/src/wasm.rs
+++ base_layer/common_types/src/wasm.rs
@@ -1,4 +1,4 @@
-// Copyright 2025
+// Copyright 2025  The Tari Project
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 // following conditions are met:
 //
