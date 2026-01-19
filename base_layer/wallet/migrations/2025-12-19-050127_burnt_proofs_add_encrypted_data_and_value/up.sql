bec93db86ec2665c550d64779633c1ce53eec5a9
stringhandler <stringhandler@protonmail.com>
2026-01-19 10:53:07 +0200

    waste time fixing a nit

--- base_layer/wallet/migrations/2025-12-19-050127_burnt_proofs_add_encrypted_data_and_value/up.sql
+++ base_layer/wallet/migrations/2025-12-19-050127_burnt_proofs_add_encrypted_data_and_value/up.sql
@@ -4,3 +4,9 @@ alter table burn_proofs
 
 alter table burn_proofs
     add column value bigint null;
+
+ALTER TABLE burn_proofs
+    ADD COLUMN kernel_excess BLOB NULL;
+
+ALTER TABLE burn_proofs
+    ADD COLUMN kernel_excess_sig BLOB NULL;
--- base_layer/wallet/migrations/2026-01-06-120000_burn_proofs_add_kernel_excess_fields/up.sql
+++
@@ -1,6 +0,0 @@
--- Add kernel_excess (excess) and kernel_excess_sig (excess_sig) columns to burn_proofs table
-ALTER TABLE burn_proofs
-    ADD COLUMN kernel_excess BLOB NULL;
-
-ALTER TABLE burn_proofs
-    ADD COLUMN kernel_excess_sig BLOB NULL;
