b95e35f88096cefa1662ad95bf24366f7856906a
SW van Heerden <swvheerden@gmail.com>
2026-01-28 16:18:31 +0200

    fix: user pay for fee and replace by fee (#7662)
    Description
    ---
    Fixes user pay for fee and replace by fee wallet calls
    Fixes base node settings
    Fixes some cucumber tests

--- base_layer/p2p/src/initialization.rs
+++ base_layer/p2p/src/initialization.rs
@@ -569,6 +569,7 @@ impl ServiceInitializer for P2pInitializer {
                 network_wire_byte: self.network.as_wire_byte(),
                 user_agent: self.user_agent.clone(),
             })
+            .with_connection_pool_refresh_interval(config.dht.connectivity.random_pool_refresh_interval)
             .with_peer_validator_config(config.dht.peer_validator_config.clone())
             .with_minimize_connections(if self.config.dht.minimize_connections {
                 Some(self.config.dht.num_neighbouring_nodes + self.config.dht.num_random_nodes)
