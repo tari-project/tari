6eb43105f589eac2d79a4364cc4e77009afda26c
SW van Heerden <swvheerden@gmail.com>
2026-01-28 10:07:05 +0200

    test2

--- integration_tests/build.rs
+++ integration_tests/build.rs
@@ -6,17 +6,21 @@ use std::path::PathBuf;
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    // OUT_DIR = target/release/build/<crate>/out
+    // OUT_DIR =
+    // target/release/build/<integration-test-crate>/out
+    //
     // ancestors:
     // 0 = out
     // 1 = <crate>
     // 2 = build
-    // 3 = release   ← we want this
-    let target_dir = out_dir
+    // 3 = release
+    // 4 = target
+    let deps_dir = out_dir
         .ancestors()
         .nth(3)
-        .expect("Failed to find target directory");
+        .unwrap()
+        .join("deps");
 
-    println!("cargo:rustc-link-search=native={}", target_dir.display());
+    println!("cargo:rustc-link-search=native={}", deps_dir.display());
     println!("cargo:rustc-link-lib=minotari_wallet_ffi");
 }
