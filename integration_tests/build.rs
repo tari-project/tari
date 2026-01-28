e3cb85153598b4ca71b5f633730f17820613b5e6
SW van Heerden <swvheerden@gmail.com>
2026-01-28 10:40:13 +0200

    fmt

--- integration_tests/build.rs
+++ integration_tests/build.rs
@@ -1,7 +1,6 @@
 // Copyright 2022. The Tari Project
 // SPDX-License-Identifier: BSD-3-Clause
-use std::env;
-use std::path::PathBuf;
+use std::{env, path::PathBuf};
 
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
@@ -15,11 +14,7 @@ fn main() {
     // 2 = build
     // 3 = release
     // 4 = target
-    let deps_dir = out_dir
-        .ancestors()
-        .nth(3)
-        .unwrap()
-        .join("deps");
+    let deps_dir = out_dir.ancestors().nth(3).unwrap().join("deps");
 
     println!("cargo:rustc-link-search=native={}", deps_dir.display());
     println!("cargo:rustc-link-lib=minotari_wallet_ffi");
