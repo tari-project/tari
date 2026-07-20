74ca7b38183bebb8d637803e60f08a522e758caf
SW van Heerden <swvheerden@gmail.com>
2026-07-20 14:44:16 +0200

    fix risv builds

--- applications/minotari_app_grpc/build.rs
+++ applications/minotari_app_grpc/build.rs
@@ -8,6 +8,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_client(true)
         .build_server(true)
+        // Required so that older `protoc` versions (e.g. the one shipped for riscv64 linux) accept the
+        // proto3 `optional` keyword instead of failing the build.
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
             &["proto/base_node.proto", "proto/wallet.proto", "proto/p2pool.proto"],
             &["proto"],
